1f3f06284193fa6da12629966e0438f5a1f75bf6
Samy Pessé <samypesse@gmail.com>
2014-03-31 12:00:22 -0700

    Add base for generation

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+    var path = require("path");
+
+    // Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // Init GRUNT configuraton
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "public/static/style.css": "public/stylesheets/main.less"
+                }
+            }
+        }
+    });
+
+    // Build
+    grunt.registerTask('build', [
+        'less'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+};
