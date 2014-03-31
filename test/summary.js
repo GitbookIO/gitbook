3f3bade58e3b258aadab8ecf084004d06e62cede
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-03-31 16:21:04 -0700

    Change summary data format
    Simplify chapter objects, by making them identical to article objects
    besides the .articles attribute

--- test/summary.js
+++ test/summary.js
@@ -23,21 +23,21 @@ describe('Summary parsing', function () {
     });
 
     it('should detect paths and titles', function() {
-        assert(LEXED.chapters[0].chapter.path);
-        assert(LEXED.chapters[1].chapter.path);
-        assert(LEXED.chapters[2].chapter.path);
+        assert(LEXED.chapters[0].path);
+        assert(LEXED.chapters[1].path);
+        assert(LEXED.chapters[2].path);
 
-        assert(LEXED.chapters[0].chapter.title);
-        assert(LEXED.chapters[1].chapter.title);
-        assert(LEXED.chapters[2].chapter.title);
+        assert(LEXED.chapters[0].title);
+        assert(LEXED.chapters[1].title);
+        assert(LEXED.chapters[2].title);
     });
 
     it('should normalize paths from .md to .html', function() {
-        assert.equal(LEXED.chapters[0].chapter.path,'chapter-1/README.html');
-        assert.equal(LEXED.chapters[0].chapter._path,'chapter-1/README.md');
-        assert.equal(LEXED.chapters[1].chapter.path,'chapter-2/README.html');
-        assert.equal(LEXED.chapters[1].chapter._path,'chapter-2/README.md');
-        assert.equal(LEXED.chapters[2].chapter.path,'chapter-3/README.html');
-        assert.equal(LEXED.chapters[2].chapter._path,'chapter-3/README.md');
+        assert.equal(LEXED.chapters[0].path,'chapter-1/README.html');
+        assert.equal(LEXED.chapters[0]._path,'chapter-1/README.md');
+        assert.equal(LEXED.chapters[1].path,'chapter-2/README.html');
+        assert.equal(LEXED.chapters[1]._path,'chapter-2/README.md');
+        assert.equal(LEXED.chapters[2].path,'chapter-3/README.html');
+        assert.equal(LEXED.chapters[2]._path,'chapter-3/README.md');
     });
 });
