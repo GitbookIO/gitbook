5125e04814e0f6bbd5533b5755d9a483f3a4ab73
Samy Pessé <samypesse@gmail.com>
2014-04-04 10:47:44 -0700

    Update tests

--- test/navigation.js
+++ test/navigation.js
@@ -13,57 +13,57 @@ var LEXED = summary(CONTENT);
 describe('Summary navigation', function() {
     it('should provide next & prev entries for a file', function() {
         var nav = navigation(LEXED, [
-            'README.html',
-            'chapter-1/README.html',
-            'chapter-1/ARTICLE1.html',
-            'chapter-1/ARTICLE2.html',
-            'chapter-2/README.html',
+            'README.md',
+            'chapter-1/README.md',
+            'chapter-1/ARTICLE1.md',
+            'chapter-1/ARTICLE2.md',
+            'chapter-2/README.md',
         ]);
 
         // Make sure it found the files we gave it
-        assert(nav['README.html']);
-        assert(nav['chapter-1/README.html']);
-        assert(nav['chapter-1/ARTICLE1.html']);
-        assert(nav['chapter-1/ARTICLE2.html']);
-        assert(nav['chapter-2/README.html']);
+        assert(nav['README.md']);
+        assert(nav['chapter-1/README.md']);
+        assert(nav['chapter-1/ARTICLE1.md']);
+        assert(nav['chapter-1/ARTICLE2.md']);
+        assert(nav['chapter-2/README.md']);
 
 
-        assert.equal(nav['README.html'].prev, null);
-        assert.equal(nav['README.html'].next.path, 'chapter-1/README.html');
+        assert.equal(nav['README.md'].prev, null);
+        assert.equal(nav['README.md'].next.path, 'chapter-1/README.md');
 
-        assert.equal(nav['chapter-1/README.html'].prev.path, 'README.html');
-        assert.equal(nav['chapter-1/README.html'].next.path, 'chapter-1/ARTICLE1.html');
+        assert.equal(nav['chapter-1/README.md'].prev.path, 'README.md');
+        assert.equal(nav['chapter-1/README.md'].next.path, 'chapter-1/ARTICLE1.md');
 
-        assert.equal(nav['chapter-1/ARTICLE1.html'].prev.path, 'chapter-1/README.html');
-        assert.equal(nav['chapter-1/ARTICLE1.html'].next.path, 'chapter-1/ARTICLE2.html');
+        assert.equal(nav['chapter-1/ARTICLE1.md'].prev.path, 'chapter-1/README.md');
+        assert.equal(nav['chapter-1/ARTICLE1.md'].next.path, 'chapter-1/ARTICLE2.md');
 
-        assert.equal(nav['chapter-1/ARTICLE2.html'].prev.path, 'chapter-1/ARTICLE1.html');
-        assert.equal(nav['chapter-1/ARTICLE2.html'].next.path, 'chapter-2/README.html');
+        assert.equal(nav['chapter-1/ARTICLE2.md'].prev.path, 'chapter-1/ARTICLE1.md');
+        assert.equal(nav['chapter-1/ARTICLE2.md'].next.path, 'chapter-2/README.md');
 
-        assert.equal(nav['chapter-2/README.html'].prev.path, 'chapter-1/ARTICLE2.html');
-        assert.equal(nav['chapter-2/README.html'].next.path, 'chapter-3/README.html');
+        assert.equal(nav['chapter-2/README.md'].prev.path, 'chapter-1/ARTICLE2.md');
+        assert.equal(nav['chapter-2/README.md'].next.path, 'chapter-3/README.md');
     });
 
     it('should give full tree, when not limited', function() {
         var nav = navigation(LEXED);
 
-        assert(nav['README.html']);
-        assert(nav['chapter-1/README.html']);
-        assert(nav['chapter-1/ARTICLE1.html']);
-        assert(nav['chapter-1/ARTICLE2.html']);
-        assert(nav['chapter-2/README.html']);
-        assert(nav['chapter-3/README.html']);
+        assert(nav['README.md']);
+        assert(nav['chapter-1/README.md']);
+        assert(nav['chapter-1/ARTICLE1.md']);
+        assert(nav['chapter-1/ARTICLE2.md']);
+        assert(nav['chapter-2/README.md']);
+        assert(nav['chapter-3/README.md']);
     });
 
     it('should detect levels correctly', function() {
         var nav = navigation(LEXED);
 
-        assert.equal(nav['README.html'].level, '0');
-        assert.equal(nav['chapter-1/README.html'].level, '1');
-        assert.equal(nav['chapter-1/ARTICLE1.html'].level, '1.1');
-        assert.equal(nav['chapter-1/ARTICLE2.html'].level, '1.2');
-        assert.equal(nav['chapter-2/README.html'].level, '2');
-        assert.equal(nav['chapter-3/README.html'].level, '3');
+        assert.equal(nav['README.md'].level, '0');
+        assert.equal(nav['chapter-1/README.md'].level, '1');
+        assert.equal(nav['chapter-1/ARTICLE1.md'].level, '1.1');
+        assert.equal(nav['chapter-1/ARTICLE2.md'].level, '1.2');
+        assert.equal(nav['chapter-2/README.md'].level, '2');
+        assert.equal(nav['chapter-3/README.md'].level, '3');
     });
 
     it('should not accept null paths', function() {
