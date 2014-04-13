d9e34fc671b3502e96a933afd3ac7c5a5cafab0f
tcorral <amischol@gmail.com>
2014-04-13 03:09:50 +0200

    Allow scroll in computers that put the navigation over scroll control. When the document is ready it will check the difference between the body-inner and the page-wrapper width to move the layer only this width allowing the user to scroll.

--- theme/javascript/app.js
+++ theme/javascript/app.js
@@ -1,54 +1,58 @@
 require([
-  "jQuery",
-  "utils/storage",
-  "utils/analytic",
-  "utils/sharing",
+    "jQuery",
+    "utils/storage",
+    "utils/analytic",
+    "utils/sharing",
 
-  "core/state",
-  "core/keyboard",
-  "core/exercise",
-  "core/progress",
-  "core/sidebar",
-  "core/search"
-], function($, storage, analytic, sharing, state, keyboard, exercise, progress, sidebar, search){
-  $(document).ready(function() {
-    var $book, bodyInnerWidth, pageWrapperWidth;
-    $book = state.$book;
+    "core/state",
+    "core/keyboard",
+    "core/exercise",
+    "core/quiz",
+    "core/progress",
+    "core/sidebar",
+    "core/search"
+], function($, storage, analytic, sharing, state, keyboard, exercise, quiz, progress, sidebar, search){
+    $(document).ready(function() {
+        var $book, bodyInnerWidth, pageWrapperWidth;
+        $book = state.$book;
 
-    // Init sidebar
-    sidebar.init();
+        // Init sidebar
+        sidebar.init();
 
-    // Load search
-    search.init();
+        // Load search
+        search.init();
 
-    // Init keyboard
-    keyboard.init();
+        // Init keyboard
+        keyboard.init();
 
-    if (state.githubId) {
-      // Initialize storage
-      storage.setBaseKey(state.githubId);
+        if (state.githubId) {
+            // Initialize storage
+            storage.setBaseKey(state.githubId);
 
-      // Star and watch count
-      $.getJSON("https://api.github.com/repos/"+state.githubId)
-        .done(function(repo) {
-          $book.find(".count-star span").text(repo.stargazers_count);
-          $book.find(".count-watch span").text(repo.subscribers_count);
-        });
-    }
+            // Star and watch count
+            $.getJSON("https://api.github.com/repos/"+state.githubId)
+            .done(function(repo) {
+                $book.find(".count-star span").text(repo.stargazers_count);
+                $book.find(".count-watch span").text(repo.subscribers_count);
+            });
+        }
 
-    // Bind exercise
-    exercise.init();
+        // Bind exercises
+        exercise.init();
+        quiz.init();
 
-    // Bind sharing button
-    sharing.init();
+        // Bind sharing button
+        sharing.init();
 
-    // Show progress
-    progress.show();
+        // Show progress
+        progress.show();
 
-    // Focus on content
-    $(".book-body").focus();
-    bodyInnerWidth = parseInt($('.body-inner').css('width'), 10);
-    pageWrapperWidth = parseInt($('.page-wrapper').css('width'), 10);
-    $('.navigation-next').css('margin-right', (bodyInnerWidth - pageWrapperWidth) + 'px');
-  });
+        // Focus on content
+        $(".book-body").focus();
+
+        // Move, if needed, right navigation control to allow scrolling.
+        bodyInnerWidth = parseInt($('.body-inner').css('width'), 10);
+        pageWrapperWidth = parseInt($('.page-wrapper').css('width'), 10);
+        $('.navigation-next').css('margin-right', (bodyInnerWidth - pageWrapperWidth) + 'px');
+    });
 });
