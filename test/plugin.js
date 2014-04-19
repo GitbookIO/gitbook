8be8b825eebc95926e7d7c2596f8b789dba0f799
Samy Pessé <samypesse@gmail.com>
2014-04-19 17:28:44 +0200

    Add base loading of default addons
    Add mixpanel as default addon

--- test/plugin.js
+++ test/plugin.js
@@ -4,10 +4,29 @@ var assert = require('assert');
 
 var Plugin = require('../').generate.Plugin;
 
-describe('Plugin loading', function () {
+describe('Plugin validation', function () {
     var plugin = new Plugin("gitbook-plugin");
 
     it('should be valid', function() {
         assert(plugin.isValid());
+    });
+});
+
+describe('Plugin defaults loading', function () {
+    var ret = true;
+
+    beforeEach(function(done){
+        Plugin.fromList(Plugin.defaults)
+        .then(function(_r) {
+            ret = _r;
+        }, function(err) {
+            ret = null;
+        })
+        .fin(done);
+    });
+    
+
+    it('should load defaults addons', function() {
+        assert(ret != null);
     });
 });
