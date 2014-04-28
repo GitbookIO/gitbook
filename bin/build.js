c30db427b1434bf814e7e72a48092974c7c89ed4
Samy Pessé <samypesse@gmail.com>
2014-04-28 20:33:25 +0200

    Remove some options from command line (moved to book.json)

--- bin/build.js
+++ bin/build.js
@@ -12,12 +12,7 @@ var buildCommand = function(command) {
     return command
     .option('-o, --output <directory>', 'Path to output directory, defaults to ./_book')
     .option('-f, --format <name>', 'Change generation format, defaults to site, availables are: '+_.keys(generators).join(", "))
-    .option('-t, --title <name>', 'Name of the book to generate, default is extracted from readme')
-    .option('-i, --intro <intro>', 'Description of the book to generate, default is extracted from readme')
-    .option('--plugins <plugins>', 'List of plugins to use separated by ","')
     .option('--config <config file>', 'Configuration file to use, defualt to book.json')
-    .option('-g, --github <repo_path>', 'ID of github repo like : username/repo')
-    .option('--githubHost <url>', 'The url of the github host (defaults to https://github.com/');
 };
 
 
@@ -31,12 +26,7 @@ var makeBuildFunc = function(converter) {
             _.extend({}, options || {}, {
                 input: dir,
                 output: outputDir,
-                title: options.title,
-                description: options.intro,
-                github: options.github,
-                githubHost: options.githubHost,
                 generator: options.format,
-                plugins: options.plugins,
                 configFile: options.config
             })
         )
