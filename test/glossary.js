cbe4e9edec3a1751030ad40a28e25df20fde2b08
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-08-15 12:09:36 -0700

    Simplify output of glossary parsing to more simple objects

--- test/glossary.js
+++ test/glossary.js
@@ -11,4 +11,10 @@ describe('Glossary parsing', function () {
     it('should only get heading + paragraph pairs', function() {
         assert.equal(LEXED.length, 5);
     });
+
+    it('should output simple name/description objects', function() {
+        assert.equal(true, !(LEXED.some(function(e) {
+            return !Boolean(e.name && e.description);
+        })));
+    });
 });
