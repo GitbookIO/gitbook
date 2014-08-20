051a0c27e4ede119e188dba118c5f11f2ff0bb12
codepiano <codepiano.li@gmail.com>
2014-08-20 15:44:56 +0800

    add sharing by qrcode
    use current url to generate qrcode,and show it in dropdown.
    code to generate qrcode from https://github.com/davidshimjs/qrcodejs

--- theme/javascript/utils/sharing.js
+++ theme/javascript/utils/sharing.js
@@ -1,8 +1,16 @@
 define([
-    "jQuery"
-], function($) {
+    "jQuery",
+    "utils/qrcode"
+], function($, qrcode) {
     var url = location.href;
     var title = $("title").text();
+    var qrcodeInfo = {
+        width        : 128,
+        height       : 128,
+        colorDark    : "#000000",
+        colorLight   : "#ffffff",
+        correctLevel : 2
+    };
 
     var types = {
         "twitter": function($el) {
@@ -17,21 +25,41 @@ define([
         "weibo": function($el) {
             window.open("http://service.weibo.com/share/share.php?content=utf-8&url="+encodeURIComponent(url)+"&title="+encodeURIComponent(title))
         },
+        "qrcode": function($el) {
+            popQRCode();
+        },
         "instapaper": function($el) {
             window.open("http://www.instapaper.com/text?u="+encodeURIComponent(url));
         }
     };
 
-
     // Bind all sharing button
     var init = function() {
         $(document).on("click", "a[data-sharing],button[data-sharing]", function(e) {
-            if (e) e.preventDefault();
+            if (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            }
             var type = $(this).data("sharing");
 
             types[type]($(this));
         })
     };
+
+    var popQRCode = function() {
+        var qrcodeDropdown = $("#dropdown-qrcode");
+        if (qrcodeDropdown.hasClass("open")) {
+            qrcodeDropdown.removeClass("open");
+        } else {
+            var lastUrl = $("#last_url");
+            if (lastUrl[0] && lastUrl.val() !== url) {
+                qrcodeInfo.text = url;
+                new qrcode.qrcode($("#qrcode")[0], qrcodeInfo);
+            }
+            qrcodeDropdown.addClass("open");
+            lastUrl.val(url);
+        }
+    }
 
     return {
         init: init
