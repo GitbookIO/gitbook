2b2b4f46c2102a85bbc98dc17fe27085d2035c40
Samy Pessé <samypesse@gmail.com>
2015-01-19 20:41:34 +0100

    Complete json generator for multilingual book
    Add tests

--- test/generation.js
+++ test/generation.js
@@ -5,11 +5,21 @@ var assert = require('assert');
 var fs = require('../lib/utils/fs');
 
 describe('Book generation', function () {
-    it('should correctly generate a book with json', function(done) {
+    it('should correctly generate a book to json', function(done) {
     	var OUTPUT_PATH = book1.options.output;
 
         qdone(
         	book1.generate("json")
+	        .fin(function() {
+	        	return fs.remove(OUTPUT_PATH);
+	        }), done);
+    });
+
+    it('should correctly generate a multilingual book to json', function(done) {
+    	var OUTPUT_PATH = book2.options.output;
+
+        qdone(
+        	book2.generate("json")
 	        .fin(function() {
 	        	return fs.remove(OUTPUT_PATH);
 	        }), done);
