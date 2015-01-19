f0ea7bc53b6a586a2edaf25e31aa0f2cad661434
Samy Pessé <samypesse@gmail.com>
2015-01-19 15:28:13 +0100

    Add test for readme parsing

--- test/parsing.js
+++ test/parsing.js
@@ -4,11 +4,16 @@ var assert = require('assert');
 var Book = require('../').Book;
 
 describe('Book parsing', function () {
-    it('should correctly parse it from markdown', function() {
+    it('should correctly parse the readme', function() {
+        assert.equal(book1.options.title, 'My Book');
+    	assert.equal(book1.options.description, 'Test description');
+    });
+
+    it('should correctly parse the summary', function() {
         var LEXED = book1.summary;
 
-        assert.equal(LEXED.chapters[0].path,'intro.md');
-    	assert.equal(LEXED.chapters[1].path,'test.md');
-        assert.equal(LEXED.chapters[2].path,'test2.md');
+        assert.equal(LEXED.chapters[0].path, 'intro.md');
+    	assert.equal(LEXED.chapters[1].path, 'test.md');
+        assert.equal(LEXED.chapters[2].path, 'test2.md');
     });
 });
