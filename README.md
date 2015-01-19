6344928580e521974508d445238c207aecec299b
Samy Pessé <samypesse@gmail.com>
2015-01-19 18:49:02 +0100

    Write file in json generator

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ GitBook
 =======
 
 :warning: This branch contains some test for a version 2.0.
+
+ChangeLog with 1.0.0:
+
+- Hooks `summary` and `glossary` (after and before) have been removed
+- Exercises and Quizzes are no longer parsed in the markdown parser
+- You can now also use the `.markdown` extension for markdown files
+
--- test/generation.js
+++ test/generation.js
@@ -2,8 +2,16 @@ var path = require('path');
 var _ = require('lodash');
 var assert = require('assert');
 
+var fs = require('../lib/utils/fs');
+
 describe('Book generation', function () {
     it('should correctly generate a book with json', function(done) {
-        qdone(book1.generate("json"), done);
+    	var OUTPUT_PATH = book1.options.output;
+
+        qdone(
+        	book1.generate("json")
+	        .fin(function() {
+	        	return fs.remove(OUTPUT_PATH);
+	        }), done);
     });
 });
