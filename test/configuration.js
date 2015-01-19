58b04da60fd819ad851a0ccc826b45bbbafaa0b5
Samy Pessé <samypesse@gmail.com>
2015-01-19 10:15:53 +0100

    Add test for loading configuration from book.js

--- test/configuration.js
+++ test/configuration.js
@@ -5,10 +5,21 @@ var Book = require('../').Book;
 
 describe('Configuration parsing', function () {
     it('should correctly load from json', function(done) {
-        var book = new Book(path.join(__dirname, './fixtures/config'));
+        var book = new Book(path.join(__dirname, './fixtures/configuration/json'));
         book.config.load()
         .then(function() {
         	assert(book.options.title == "Test");
+        })
+        .then(function() {
+        	done()
+        }, done);
+    });
+
+    it('should correctly load from javascript', function(done) {
+        var book = new Book(path.join(__dirname, './fixtures/configuration/js'));
+        book.config.load()
+        .then(function() {
+        	assert(book.options.title == "Test 2");
         })
         .then(function() {
         	done()
