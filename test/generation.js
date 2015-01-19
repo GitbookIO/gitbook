2dbf9b0d4a88a6461ca60f49f3d66ccd42143be3
Samy Pessé <samypesse@gmail.com>
2015-01-19 17:32:59 +0100

    Start adapting json generator

--- test/generation.js
+++ test/generation.js
@@ -2,8 +2,8 @@ var path = require('path');
 var _ = require('lodash');
 var assert = require('assert');
 
-describe('Book parsing', function () {
-    it('should correctly generate a book', function(done) {
-        qdone(book1.generate(), done);
+describe('Book generation', function () {
+    it('should correctly generate a book with json', function(done) {
+        qdone(book1.generate("json"), done);
     });
 });
