ee2420cbcca41f3c33a243084b11fd9946b87ed1
Samy Pessé <samypesse@gmail.com>
2015-01-22 22:57:08 +0100

    Change name of generator "site" to "website"

--- test/website.js
+++ test/website.js
@@ -8,14 +8,14 @@ var fsUtil = require("../lib/utils/fs");
 
 describe('Website Generator', function () {
     it('should correctly generate a book to website', function(done) {
-        testGeneration(books[1], "site", function(output) {
+        testGeneration(books[1], "website", function(output) {
             assert(fs.existsSync(path.join(output, "index.html")));
             assert(fs.existsSync(path.join(output, "search_index.json")));
         }, done);
     });
 
     it('should correctly include styles in website', function(done) {
-        testGeneration(books[0], "site", function(output) {
+        testGeneration(books[0], "website", function(output) {
             assert(fs.existsSync(path.join(output, "styles/website.css")));
 
             var INDEX = fs.readFileSync(path.join(output, "index.html")).toString();
@@ -24,13 +24,13 @@ describe('Website Generator', function () {
     });
 
     it('should correctly include glossary in website', function(done) {
-        testGeneration(books[0], "site", function(output) {
+        testGeneration(books[0], "website", function(output) {
             assert(fs.existsSync(path.join(output, "GLOSSARY.html")));
         }, done);
     });
 
     it('should correctly generate a multilingual book to website', function(done) {
-        testGeneration(books[2], "site", function(output) {
+        testGeneration(books[2], "website", function(output) {
             assert(fs.existsSync(path.join(output, "index.html")));
             assert(fs.existsSync(path.join(output, "gitbook")));
             assert(fs.existsSync(path.join(output, "fr/index.html")));
