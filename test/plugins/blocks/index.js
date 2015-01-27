0e0a23b79484d98695ffa56fa9f885dffef86d36
Samy Pessé <samypesse@gmail.com>
2015-01-27 17:07:25 +0100

    Add blocks from plugins to template engine

--- test/plugins/blocks/index.js
+++ test/plugins/blocks/index.js
@@ -1,14 +1,14 @@
 module.exports = {
     blocks: {
     	"test": {
-    		process: function(body, args, kwargs) {
-    			return "test"+body+"test";
+    		process: function(args) {
+    			return "test"+args.body+"test";
     		}
     	},
     	"test2": {
     		end: "endtest2end",
-    		process: function(body, args, kwargs) {
-    			return "test2"+body+"test2";
+    		process: function(args) {
+    			return "test2"+args.body+"test2";
     		}
     	}
     }
