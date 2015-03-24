b2e684af8f132a617b808bb6f4b91af04425b63e
Samy Pessé <samypesse@gmail.com>
2015-03-24 17:34:47 +0100

    Add test for templating context

---
+++ test/templating.js
@@ -0,0 +1,33 @@
+var pkg = require("../package.json");
+
+describe('Templating', function () {
+    var book;
+
+    before(function() {
+        return books.parse("basic")
+            .then(function(_book) {
+                book = _book;
+            });
+    });
+
+    var testTpl = function(str, args, options) {
+        return book.template.renderString(str, args, options)
+        .then(book.template.postProcess)
+    };
+
+    describe('Context', function() {
+        it('should correctly have access to generator', function() {
+            return testTpl('{{ gitbook.generator }}')
+                .then(function(content) {
+                    content.should.equal("website");
+                });
+        });
+
+        it('should correctly have access to gitbook version', function() {
+            return testTpl('{{ gitbook.version }}')
+                .then(function(content) {
+                    content.should.equal(pkg.version.toString());
+                });
+        });
+    });
+});
