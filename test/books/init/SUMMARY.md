36e2236e6bad2071ae604eccb46331bb7da3b79c
Samy Pesse <samypesse@gmail.com>
2015-05-27 21:05:36 +0200

    Fix #765: in init handle correctly empty entries
    Add tests for it, fix #769

---
+++ test/books/init/SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+* [Hello](hello.md)
+* [Hello 2](hello2.md)
+* Hello 3
+    * [Hello 4](hello3/hello4.md)
---
+++ test/init.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+var should = require('should');
+
+var Book = require('../').Book;
+var LOG_LEVELS = require('../').LOG_LEVELS;
+
+describe('Init Books', function () {
+    var initRoot;
+
+    before(function() {
+        initRoot = path.resolve(__dirname, "books/init");
+        return Book.init(initRoot, {
+            logLevel: LOG_LEVELS.DISABLED,
+        });
+    });
+
+    it('should create all chapters', function() {
+        should(fs.existsSync(path.resolve(initRoot, "hello.md"))).be.ok;
+        should(fs.existsSync(path.resolve(initRoot, "hello2.md"))).be.ok;
+        should(fs.existsSync(path.resolve(initRoot, "hello3/hello4.md"))).be.ok;
+    });
+});
