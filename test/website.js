ba65e3c020faa46eead7376c73af29e5e5839d8b
Samy Pessé <samypesse@gmail.com>
2015-10-06 13:26:32 +0200

    Adapt tests

--- test/website.js
+++ test/website.js
@@ -1,22 +1,22 @@
-describe("Website generator", function () {
-    describe("Basic Book", function() {
+describe('Website generator', function () {
+    describe('Basic Book', function() {
         var book;
 
         before(function() {
-            return books.generate("basic", "website")
+            return books.generate('basic', 'website')
                 .then(function(_book) {
                     book = _book;
                 });
         });
 
-        it("should correctly output an index.html", function() {
-            book.should.have.file("index.html");
+        it('should correctly output an index.html', function() {
+            book.should.have.file('index.html');
         });
 
-        it("should correctly copy assets", function() {
-            book.should.have.file("gitbook");
-            book.should.have.file("gitbook/app.js");
-            book.should.have.file("gitbook/style.css");
+        it('should correctly copy assets', function() {
+            book.should.have.file('gitbook');
+            book.should.have.file('gitbook/app.js');
+            book.should.have.file('gitbook/website.css');
         });
     });
 });
