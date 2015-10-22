481a76319fadef4e8f117dd55fda270c14aa0a25
Samy Pessé <samypesse@gmail.com>
2015-10-22 13:49:28 +0200

    Provide gitbook.state.root to plugins

--- theme/javascript/state.js
+++ theme/javascript/state.js
@@ -1,14 +1,19 @@
 var $ = require('jquery');
+var url = require('url');
+var path = require('path');
 
 var state = {};
 
 state.update = function(dom) {
-    var $book = $(dom.find(".book"));
+    var $book = $(dom.find('.book'));
 
     state.$book = $book;
-    state.level = $book.data("level");
-    state.basePath = $book.data("basepath");
-    state.revision = $book.data("revision");
+    state.level = $book.data('level');
+    state.basePath = $book.data('basepath');
+    state.revision = $book.data('revision');
+
+    // Absolute url to the root of the book
+    state.root = url.resolve(location.origin, path.dirname(path.resolve(location.pathname, state.basePath)));
 };
 
 state.update($);
