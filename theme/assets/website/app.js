013f7f09da56b6a2666332705b593c065e884e3c
Samy Pessé <samypesse@gmail.com>
2015-10-22 16:11:05 +0200

    Add gitbook.state.filepath
    Fix gitbook.state.root

--- theme/assets/website/app.js
+++ theme/assets/website/app.js
@@ -24736,9 +24736,13 @@ state.update = function(dom) {
     state.level = $book.data('level');
     state.basePath = $book.data('basepath');
     state.revision = $book.data('revision');
+    state.filepath = $book.data('filepath');
 
     // Absolute url to the root of the book
-    state.root = url.resolve(location.origin, path.dirname(path.resolve(location.pathname, state.basePath)));
+    state.root = url.resolve(
+        location.protocol+'//'+location.host,
+        path.dirname(path.resolve(location.pathname, state.basePath))
+    );
 };
 
 state.update($);
