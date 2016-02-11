02cff8c72ca742cd06a65d171657eb230eedbc5b
Samy Pessé <samypesse@gmail.com>
2016-02-11 11:53:32 +0100

    Add base for page parsing

--- test/output-json.js
+++ test/output-json.js
@@ -1,11 +1,21 @@
 var mock = require('./mock');
 
 describe('JSON Output', function() {
-    it('should correctly generate a default book', function() {
-        return mock.outputDefaultBook('json')
-        .then(function(output) {
+
+    describe('Sample book', function() {
+        var output;
+
+        before(function() {
+            return mock.outputDefaultBook('json')
+            .then(function(_output) {
+                output = _output;
+            });
+        });
+
+        it('should correctly generate a README.json', function() {
             output.should.have.file('README.json');
         });
+
     });
 });
 
