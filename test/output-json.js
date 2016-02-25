f47a3968b7a78af8091906fe8936cd00581ae05f
Samy Pessé <samypesse@gmail.com>
2016-02-25 08:59:10 +0100

    Add tests for multilingual book and json output

--- test/output-json.js
+++ test/output-json.js
@@ -18,5 +18,32 @@ describe('JSON Output', function() {
         });
 
     });
+
+    describe('Multilingual Book', function() {
+        var output;
+
+        before(function() {
+            return mock.outputBook(JSONOutput, {
+                'LANGS.md': '# Languages\n\n'
+                    + '* [en](./en)\n'
+                    + '* [fr](./fr)\n\n',
+                'en/README.md': '# Hello',
+                'fr/README.md': '# Bonjour'
+
+            })
+            .then(function(_output) {
+                output = _output;
+            });
+        });
+
+        it('should correctly generate a README.json for each language', function() {
+            output.should.have.file('en/README.json');
+            output.should.have.file('fr/README.json');
+        });
+
+        it('should correctly generate a README.json for the whole book', function() {
+            output.should.have.file('README.json');
+        });
+    });
 });
 
