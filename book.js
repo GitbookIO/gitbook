4499ccc501950f472ade2175e742d707752ef39d
Samy Pessé <samypesse@gmail.com>
2016-03-02 10:01:32 +0100

    Explicitly use current version as gitbook version to build

--- book.js
+++ book.js
@@ -1,11 +1,17 @@
 var pkg = require('./package.json');
 
 module.exports = {
+    // Documentation for GitBook is stored under "docs"
     root: './docs',
     title: 'GitBook Documentation',
 
+    // Enforce use of GitBook v3
+    gitbook: pkg.version,
+
+    // Use the "official" theme
     plugins: ['theme-official'],
     theme: 'official',
+
     variables: {
         version: pkg.version
     }
