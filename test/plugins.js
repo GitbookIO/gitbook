7d21c815a5e710de9247db66c8e87f5e94ad2ce3
Jasha Joachimsthal <jasha@onegini.com>
2016-03-24 15:23:28 +0100

    Limit splitting plugin name for git+ssh

--- test/plugins.js
+++ test/plugins.js
@@ -81,6 +81,21 @@ describe('Plugins', function() {
             })
             .should.be.fulfilledWith(1);
         });
+        it('should correctly install dependencies from GitHub via ssh', function() {
+            return mock.setupBook({
+                'book.json': {
+                    plugins: ['ga@git@github.com:GitbookIO/plugin-ga.git#master']
+                }
+            })
+            .then(function(book) {
+                return book.prepareConfig()
+                .then(function() {
+                    var plugins = new PluginsManager(book);
+                    return plugins.install();
+                });
+            })
+            .should.be.fulfilledWith(1);
+        });
     });
 
     describe('Loading', function() {
