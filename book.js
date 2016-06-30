d563abe3df9ec364625d9e8cddb68972a650133c
Soreine <nicolas@gitbook.com>
2016-06-30 15:37:31 +0200

    Reduce size of logo to give room to version number

--- book.js
+++ book.js
@@ -9,7 +9,7 @@ module.exports = {
     gitbook: '3.1.1',
 
     // Use the "official" theme
-    plugins: ['theme-official@2.1.0', '-sharing', '-fontsettings', 'sitemap'],
+    plugins: ['theme-official@2.1.1', '-sharing', '-fontsettings', 'sitemap'],
 
     variables: {
         version: pkg.version
