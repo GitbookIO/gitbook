0614613b8414d62a6150e58f55a53c985a83937c
Soreine <nicolas@gitbook.com>
2016-10-05 21:41:43 +0200

    Starting gitbook-plugin-sharing

--- packages/gitbook-plugin-sharing/src/index.js
+++ packages/gitbook-plugin-sharing/src/index.js
@@ -1,8 +1,34 @@
 const GitBook = require('gitbook-core');
+const { React } = GitBook;
 
 module.exports = GitBook.createPlugin({
-    activate: (dispatch, getState) => {
-
+    activate: (dispatch, getState, { Components }) => {
+        // Dispatch initialization actions
+        dispatch(Components.registerComponent(SharingButton, { role: 'toolbar:buttons:right' }))
+    },
+    deactivate: (dispatch, getState) => {
+        // Dispatch cleanup actions
     },
     reduce: (state, action) => state
 });
+
+let SharingButton = React.createClass({
+    propTypes: {
+        page: GitBook.Shapes.Page
+    },
+
+    onClick() {
+        alert(this.props.page.title)
+    },
+
+    render() {
+        return (
+            <GitBook.Button onClick={this.onClick}>
+                <GitBook.Icon id="facebook"/>
+            </GitBook.Button>
+        )
+    }
+})
+SharingButton = GitBook.connect(SharingButton, function mapStateToProps(state) {
+    return { page: state.page }
+})
