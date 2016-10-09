cf35fef3b380bf59edae0bd05ffb8366fd3bf4dd
Samy Pesse <samypesse@gmail.com>
2016-10-09 17:23:24 +0200

    Use map.getIn for plugin heading

--- packages/gitbook-plugin-headings/src/index.js
+++ packages/gitbook-plugin-headings/src/index.js
@@ -4,7 +4,7 @@ const classNames = require('classnames');
 
 function mapStateToProps({ config }) {
     return {
-        position: config.get('pluginsConfig').get('headings').get('position') || 'left'
+        position: config.getIn(['pluginsConfig', 'headings', 'position'], 'left')
     };
 }
 
