60e3492a924945f72c9ce937f50c262f6e2a2b39
Soreine <nicolas@gitbook.com>
2016-10-11 14:49:22 +0200

    Create HotKeys component

--- packages/gitbook-core/src/index.js
+++ packages/gitbook-core/src/index.js
@@ -12,6 +12,7 @@ const { ImportLink, ImportScript, ImportCSS } = require('./components/Import');
 const HTMLContent = require('./components/HTMLContent');
 const Link = require('./components/Link');
 const Icon = require('./components/Icon');
+const HotKeys = require('./components/HotKeys');
 const Button = require('./components/Button');
 const ButtonGroup = require('./components/ButtonGroup');
 const Dropdown = require('./components/Dropdown');
@@ -51,6 +52,7 @@ module.exports = {
     FlexBox: Box,
     Link,
     Icon,
+    HotKeys,
     Button,
     ButtonGroup,
     Dropdown,
