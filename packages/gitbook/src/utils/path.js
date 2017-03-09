621f0ef0016bda3f65fe4bf01a339ec693cee3ef
Soreine <nicolas@gitbook.com>
2017-03-09 16:09:45 +0100

    gitbook: Extract `isInside` to PathUtils

--- packages/gitbook/src/utils/path.js
+++ packages/gitbook/src/utils/path.js
@@ -1,38 +1,44 @@
-const path = require('path');
+const Path = require('path');
 const error = require('./error');
 
-// Normalize a filename
+/**
+ * Normalize a filename
+ */
 function normalizePath(filename) {
-    return path.normalize(filename);
+    return Path.normalize(filename);
 }
 
-// Return true if file path is inside a folder
+/**
+ * Return true if file path is inside a folder
+ */
 function isInRoot(root, filename) {
-    root = path.normalize(root);
-    filename = path.normalize(filename);
+    root = Path.normalize(root);
+    filename = Path.normalize(filename);
 
     if (root === '.') {
         return true;
     }
-    if (root[root.length - 1] != path.sep) {
-        root = root + path.sep;
+    if (root[root.length - 1] != Path.sep) {
+        root = root + Path.sep;
     }
 
     return (filename.substr(0, root.length) === root);
 }
 
-// Resolve paths in a specific folder
-// Throw error if file is outside this folder
+/**
+ * Resolve paths in a specific folder
+ * Throw error if file is outside this folder
+ */
 function resolveInRoot(root, ...args) {
     const input = args
         .reduce((current, p) => {
             // Handle path relative to book root ("/README.md")
             if (p[0] == '/' || p[0] == '\\') return p.slice(1);
 
-            return current ? path.join(current, p) : path.normalize(p);
+            return current ? Path.join(current, p) : Path.normalize(p);
         }, '');
 
-    const result = path.resolve(root, input);
+    const result = Path.resolve(root, input);
 
     if (!isInRoot(root, result)) {
         throw new error.FileOutOfScopeError({
@@ -44,26 +50,55 @@ function resolveInRoot(root, ...args) {
     return result;
 }
 
-// Chnage extension of a file
+/**
+ * Change extension of a file
+ */
 function setExtension(filename, ext) {
-    return path.join(
-        path.dirname(filename),
-        path.basename(filename, path.extname(filename)) + ext
+    return Path.join(
+        Path.dirname(filename),
+        Path.basename(filename, Path.extname(filename)) + ext
     );
 }
 
-/*
-    Return true if a filename is relative.
-
-    @param {String}
-    @return {Boolean}
-*/
+/**
+ * Return true if a filename is relative.
+ *
+ * @param {String}
+ * @return {Boolean}
+ */
 function isPureRelative(filename) {
     return (filename.indexOf('./') === 0 || filename.indexOf('../') === 0);
 }
 
+/**
+ * Implementation based on https://github.com/domenic/path-is-inside/
+ *
+ * @param {String} path
+ * @param {String} potentialParent
+ * @return {Boolean} True if path match the parent path, or if path is inside parent path
+ */
+function isInside(path = '', potentialParent) {
+    // For inside-directory checking, we want to allow trailing slashes, so normalize.
+    path = stripTrailingSep(path);
+    potentialParent = stripTrailingSep(potentialParent);
+
+    return path.lastIndexOf(potentialParent, 0) === 0 &&
+        (
+            path[potentialParent.length] === Path.sep ||
+            path[potentialParent.length] === undefined
+        );
+}
+
+function stripTrailingSep(path) {
+    if (path[path.length - 1] === Path.sep) {
+        return path.slice(0, -1);
+    }
+    return path;
+}
+
 module.exports = {
     isInRoot,
+    isInside,
     resolveInRoot,
     normalize: normalizePath,
     setExtension,
