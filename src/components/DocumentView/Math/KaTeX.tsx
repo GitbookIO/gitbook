6a62f4252106fd9cab71b34676e4a41d9ed65f8a
Samy Pessé <samypesse@gmail.com>
2023-12-21 15:11:06 +0100

    Lazy load KaTeX CSS (#57)
    * First attempt at lazy loading it
    
    * New approach
    
    * Format
    
    * Remove logs

--- src/components/DocumentView/Math/KaTeX.tsx
+++ src/components/DocumentView/Math/KaTeX.tsx
@@ -1,20 +1,37 @@
 import katex from 'katex';
-import 'katex/dist/katex.min.css';
+import dynamic from 'next/dynamic';
+import React from 'react';
 
 /**
  * Server component to compile the KaTeX formula to HTML.
- * TODO: find way to lazy load KaTeX CSS.
  */
 export function KaTeX(props: { formula: string; inline: boolean; className?: string }) {
     const { formula, inline, className } = props;
 
     const html = katex.renderToString(formula, {
         displayMode: !inline,
+        output: 'htmlAndMathml',
     });
 
-    if (inline) {
-        return <span className={className} dangerouslySetInnerHTML={{ __html: html }} />;
-    } else {
-        return <div className={className} dangerouslySetInnerHTML={{ __html: html }} />;
-    }
+    // While loading the CSS, we fallback to rendering the MathML.
+    const mathml = katex.renderToString(formula, {
+        displayMode: !inline,
+        output: 'mathml',
+    });
+    const fallback = inline ? (
+        <span className={className} dangerouslySetInnerHTML={{ __html: mathml }} />
+    ) : (
+        <div className={className} dangerouslySetInnerHTML={{ __html: mathml }} />
+    );
+
+    const KaTeXCSS = dynamic(() => import('./KaTeXCSS'), { ssr: false, loading: () => fallback });
+    return (
+        <KaTeXCSS>
+            {inline ? (
+                <span className={className} dangerouslySetInnerHTML={{ __html: html }} />
+            ) : (
+                <div className={className} dangerouslySetInnerHTML={{ __html: html }} />
+            )}
+        </KaTeXCSS>
+    );
 }
