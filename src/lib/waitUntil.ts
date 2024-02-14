49a1561801c119d671f13201a30bb7fd618a77c4
Samy Pessé <samypesse@gmail.com>
2024-02-14 12:35:39 +0100

    Update @cloudflare/next-on-pages to 1.9.0 (#156)

--- src/lib/waitUntil.ts
+++ src/lib/waitUntil.ts
@@ -1,21 +1,11 @@
-import { getRequestContext } from '@cloudflare/next-on-pages';
-
-const cloudflareRequestContextSymbol = Symbol.for('__cloudflare-request-context__');
+import { getOptionalRequestContext } from '@cloudflare/next-on-pages';
 
 /**
  * Get a global context object for the current request.
  * This object can be used as a key to store request-specific data in a WeakMap.
  */
 export function getGlobalContext(): object {
-    // See https://github.com/cloudflare/next-on-pages/discussions/596#discussioncomment-8450986
-    // we need a reliable object that will be different per request
-    const cloudflareRequestContext = (
-        globalThis as unknown as {
-            [cloudflareRequestContextSymbol]: object;
-        }
-    )[cloudflareRequestContextSymbol];
-
-    return cloudflareRequestContext ?? globalThis;
+    return getOptionalRequestContext() ?? globalThis;
 }
 
 /**
@@ -23,19 +13,10 @@ export function getGlobalContext(): object {
  * https://developers.cloudflare.com/workers/runtime-apis/handlers/fetch/#contextwaituntil
  */
 export async function waitUntil(promise: Promise<unknown>) {
-    const cloudflare = getCloudflareContext();
+    const cloudflare = getOptionalRequestContext();
     if (cloudflare) {
         cloudflare.ctx.waitUntil(promise);
     } else {
         await promise;
-    }
-}
-
-function getCloudflareContext() {
-    try {
-        const cloudflare = getRequestContext();
-        return cloudflare;
-    } catch (error) {
-        return null;
     }
 }
