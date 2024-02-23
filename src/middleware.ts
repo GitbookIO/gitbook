345a2ea9645b16ff5ce4e3572d22528b3be080b0
Taran Vohra <taranvohra@outlook.com>
2024-02-23 19:44:50 +0530

    log request cookies in the middleware (#184)

--- src/middleware.ts
+++ src/middleware.ts
@@ -77,6 +77,8 @@ export type LookupResult = PublishedContentWithCache & {
 export async function middleware(request: NextRequest) {
     const { url, mode } = getInputURL(request);
 
+    console.log(`handling request with cookies ${request.cookies.toString()}`);
+
     Sentry.setTag('url', url.toString());
     Sentry.setContext('request', {
         method: request.method,
