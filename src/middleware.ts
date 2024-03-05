cf20c5ff3b23e5f0ee0527867933677749e0a3c6
Samy Pessé <samypesse@gmail.com>
2024-03-05 14:46:08 +0000

    Output cache-control only when env is set (#206)

--- src/middleware.ts
+++ src/middleware.ts
@@ -205,10 +205,15 @@ export async function middleware(request: NextRequest) {
     // Add Content Security Policy header
     response.headers.set('content-security-policy', csp);
 
-    if (process.env.NODE_ENV !== 'development' && resolved.cacheMaxAge) {
+    if (resolved.cacheMaxAge) {
         const cacheControl = `public, max-age=60, s-maxage=${resolved.cacheMaxAge}, stale-while-revalidate=60, stale-if-error=0`;
-        response.headers.set('cache-control', cacheControl);
-        response.headers.set('Cloudflare-CDN-Cache-Control', cacheControl);
+
+        if (process.env.GITBOOK_OUTPUT_CACHE === 'true' && process.env.NODE_ENV !== 'development') {
+            response.headers.set('cache-control', cacheControl);
+            response.headers.set('Cloudflare-CDN-Cache-Control', cacheControl);
+        } else {
+            response.headers.set('x-gitbook-cache-control', cacheControl);
+        }
     }
 
     if (resolved.cacheTags && resolved.cacheTags.length > 0) {
