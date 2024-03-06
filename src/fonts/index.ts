46c3ec6699c5fbf9255f2e35bfcb6a432fe36c93
Samy Pessé <samypesse@gmail.com>
2024-03-06 09:59:43 +0000

    Add fallback to the different fonts (#216)

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -18,98 +18,137 @@ import {
 } from 'next/font/google';
 import localFont from 'next/font/local';
 
+/*
+    Fonts are downloaded and loaded by next/font.
+
+    We can't use "preload: true" as otherwise Next will preload all the fonts on the page
+    while spaces only use one font at a time.
+ */
+
 const inter = Inter({
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 export const ibmPlexMono = IBM_Plex_Mono({
     weight: ['400', '600'],
     variable: '--font-mono',
     style: 'normal',
+    display: 'swap',
     preload: false,
+    fallback: ['monospace'],
 });
 
 const firaSans = Fira_Sans_Extra_Condensed({
     weight: ['400', '500', '600', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const ibmPlexSerif = IBM_Plex_Serif({
     weight: ['400', '500', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['serif'],
 });
 
 const lato = Lato({
     weight: ['400', '700', '900'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const merriweather = Merriweather({
     weight: ['400', '700', '900'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['serif'],
 });
 
 const notoSans = Noto_Sans({
     weight: ['400', '500', '700', '800'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const openSans = Open_Sans({
     weight: ['400', '600', '700', '800'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const overpass = Overpass({
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const poppins = Poppins({
     weight: ['400', '500', '600', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const raleway = Raleway({
     weight: ['400', '500', '600', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const roboto = Roboto({
     weight: ['400', '500', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const robotoSlab = Roboto_Slab({
     weight: ['400', '500', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const sourceSansPro = Source_Sans_3({
     weight: ['400', '600', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const ubuntu = Ubuntu({
     weight: ['400', '500', '700'],
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
 });
 
 const abcFavorit = localFont({
     variable: '--font-content',
     preload: false,
+    display: 'swap',
+    fallback: ['system-ui', 'arial'],
     src: [
         {
             path: './ABCFavorit/ABCFavorit-Variable.woff2',
