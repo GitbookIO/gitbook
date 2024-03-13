ee3221885c0438f4b957ee3d2c8daf55e0b387a2
Steven H <shne24@gmail.com>
2024-03-13 16:35:18 +0000

    Remove Sentry trace/transaction (#258)

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,8 +3,16 @@ import * as Sentry from '@sentry/nextjs';
 const dsn = process.env.SENTRY_DSN;
 if (dsn) {
     Sentry.init({
-        dsn,
-        tracesSampleRate: 0.01,
         debug: false,
+        dsn,
+
+        // Disable tracing as it creates additional requests in an env where subrequests are limited.
+        enableTracing: false,
+
+        // Disable transactions  as it creates additional requests in an env where subrequests are limited.
+        // https://docs.sentry.io/platforms/node/configuration/filtering/#using--3
+        beforeSendTransaction: () => {
+            return null;
+        },
     });
 }
