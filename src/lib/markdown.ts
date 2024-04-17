bf450e5cc708a626b81ebf85b6e8f1c0ffb0bc19
Steven H <steven@gitbook.io>
2024-04-17 14:21:07 +0100

    Support GitHub-flavoured Markdown when parsing OpenAPI descriptions. (#2288)

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,5 +1,6 @@
 import rehypeSanitize from 'rehype-sanitize';
 import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
@@ -10,6 +11,7 @@ import { unified } from 'unified';
 export async function parseMarkdown(markdown: string): Promise<string> {
     const file = await unified()
         .use(remarkParse)
+        .use(remarkGfm)
         .use(remarkRehype)
         .use(rehypeSanitize)
         .use(rehypeStringify)
