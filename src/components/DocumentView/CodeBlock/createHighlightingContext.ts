b194aeb9dca588f2290473e28895062f912c4206
Steven Hall <steven@gitbook.io>
2024-04-25 09:28:24 +0100

    Use plain code highlighting on some spaces that are currently crashing.

--- src/components/DocumentView/CodeBlock/createHighlightingContext.ts
+++ src/components/DocumentView/CodeBlock/createHighlightingContext.ts
@@ -1,6 +1,11 @@
 const CODE_HIGHLIGHT_BLOCK_LIMIT = 50;
 
 /**
+ * Spaces where we'll always use plain highlighting.
+ */
+const PLAIN_HIGHLIGHTING_SPACES: string[] = ['V9geAO9ITPi8WOYK5o0r', 'puRmcwVxGFtHph8IjXaf', 'e3jwbMOrr4RhKtZ9C0XL'];
+
+/**
  * Protect against memory issues when highlighting a large number of code blocks.
  * This context only allows 50 code blocks per render to be highlighted.
  * Once highlighting can scale up to a large number of code blocks, it can be removed.
@@ -9,7 +14,11 @@ const CODE_HIGHLIGHT_BLOCK_LIMIT = 50;
  */
 export function createHighlightingContext() {
     let count = 0;
-    return () => {
+    return (spaceId?: string | undefined) => {
+        if (!spaceId || PLAIN_HIGHLIGHTING_SPACES.includes(spaceId)) {
+            return false;
+        }
+
         count += 1;
         return count < CODE_HIGHLIGHT_BLOCK_LIMIT;
     };
