285841f8ead6bb1a314e2eb3d594646734ae729c
Steven Hall <steven@gitbook.io>
2024-05-14 17:20:58 +0100

    Fix regex

--- next.config.js
+++ next.config.js
@@ -43,7 +43,7 @@ module.exports = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: '*.gitbook.io',
+                hostname: '**.gitbook.io',
                 port: '',
             },
         ]
