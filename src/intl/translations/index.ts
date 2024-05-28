d43a183399b54a272d2a4485c992b2ac645919b5
David Burghoff <burghoffdavid@protonmail.com>
2024-05-28 07:57:13 +0200

    import de before en (alphabetical), should fix linter

--- src/intl/translations/index.ts
+++ src/intl/translations/index.ts
@@ -1,10 +1,10 @@
 import { CustomizationLocale } from '@gitbook/api';
 
+import { de } from './de';
 import { en } from './en';
 import { es } from './es';
 import { fr } from './fr';
 import { ja } from './ja';
-import { de } from './de';
 import { TranslationLanguage } from './types';
 import { zh } from './zh';
 
@@ -13,10 +13,10 @@ export * from './types';
 export const languages: {
     [locale in CustomizationLocale]: TranslationLanguage;
 } = {
+    de,
     en,
     fr,
     es,
     zh,
     ja,
-    de,
 };
