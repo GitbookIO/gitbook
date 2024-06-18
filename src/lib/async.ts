edd916b9efcd009ee44c5602e6c5fa7602422028
Johan Preynat <johan.preynat@gmail.com>
2024-06-18 11:01:19 +0200

    Add logs

--- src/lib/async.ts
+++ src/lib/async.ts
@@ -272,6 +272,7 @@ export function singletonMap<Key extends string, Args extends any[], Result>(
         const ctx = await getGlobalContext();
         let current = states.get(ctx);
         if (current) {
+            console.log('got current', key);
             const existing = current.get(key);
             if (existing) {
                 return existing;
@@ -279,6 +280,7 @@ export function singletonMap<Key extends string, Args extends any[], Result>(
         }
 
         if (!current) {
+            console.log('first current', key);
             current = new Map();
             states.set(ctx, current);
         }
