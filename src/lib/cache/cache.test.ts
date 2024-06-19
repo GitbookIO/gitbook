0fdc448bfd5ad87547fcbdbb4a241d0d1d8d9e1e
Johan Preynat <johan.preynat@gmail.com>
2024-06-19 15:35:26 +0200

    Fix cache tests to reflect behavior based on TTL

--- src/lib/cache/cache.test.ts
+++ src/lib/cache/cache.test.ts
@@ -7,21 +7,24 @@ describe('cache', () => {
 
     let fn: CacheFunction<[string], string>;
     let testId = 0;
+    let getTtl: () => number;
 
     beforeEach(() => {
         impl.mockClear();
 
         testId += 1;
+        getTtl = () => 1000;
 
         fn = cache(`cache-${testId}`, async (arg: string, options: CacheFunctionOptions) => {
             await new Promise((resolve) => setTimeout(resolve, 20));
             return {
                 data: impl(arg),
+                ttl: getTtl(),
             };
         });
     });
 
-    it('should only execute once for same argument', async () => {
+    it('should only execute once for same argument if there is a ttl', async () => {
         const result = await Promise.all([fn('a'), fn('a')]);
 
         expect(result).toEqual(['test-a', 'test-a']);
@@ -32,6 +35,21 @@ describe('cache', () => {
         expect(await fn('a')).toEqual('test-a');
 
         expect(impl).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute multiple times for same argument if ttl is 0 or undefined', async () => {
+        getTtl = () => 0;
+
+        const result = await Promise.all([fn('a'), fn('a')]);
+
+        expect(result).toEqual(['test-a', 'test-a']);
+
+        expect(impl).toHaveBeenCalled();
+        expect(impl).toHaveBeenCalledTimes(1);
+
+        expect(await fn('a')).toEqual('test-a');
+
+        expect(impl).toHaveBeenCalledTimes(2);
     });
 
     it('should execute for different arguments', async () => {
