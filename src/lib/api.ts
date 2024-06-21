5c9335b5ffde921e5a0ee4163388ac7981a5140b
Johan Preynat <johan.preynat@gmail.com>
2024-06-21 10:36:21 +0200

    Forward content resolution API response tags to local caches

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -243,6 +243,7 @@ export const getPublishedContentByUrl = cache(
             };
             return {
                 tags: [
+                    ...parsed.tags,
                     getAPICacheTag({
                         tag: 'url',
                         hostname: parsedURL.hostname,
