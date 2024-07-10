dd5dea0bd878997db5b229632e86f93fe3142d45
Steeve Pastorelli <steeve@gitbook.com>
2024-07-10 16:02:57 +0200

    Make sure to normalize url pathname

--- src/components/Header/SpacesDropdownMenuItem.tsx
+++ src/components/Header/SpacesDropdownMenuItem.tsx
@@ -5,16 +5,23 @@ import { useSelectedLayoutSegment } from 'next/navigation';
 
 import { DropdownMenuItem } from './Dropdown';
 
-export function SpacesDropdownMenuItem(props: { variantSpace: Space; currentSpace: Space }) {
-    const { variantSpace, currentSpace } = props;
+function useVariantSpaceHref(variantSpace: Space) {
     const currentPathname = useSelectedLayoutSegment() ?? '';
     const targetUrl = new URL(variantSpace.urls.published ?? variantSpace.urls.app);
     targetUrl.pathname += `/${currentPathname}`;
+    targetUrl.pathname = targetUrl.pathname.replace(/\/{2,}/g, '/').replace(/\/$/, '');
+
+    return targetUrl.toString();
+}
+
+export function SpacesDropdownMenuItem(props: { variantSpace: Space; currentSpace: Space }) {
+    const { variantSpace, currentSpace } = props
+    const variantHref = useVariantSpaceHref(variantSpace);
 
     return (
         <DropdownMenuItem
             key={variantSpace.id}
-            href={targetUrl.toString()}
+            href={variantHref}
             active={variantSpace.id === currentSpace.id}
         >
             {variantSpace.title}
