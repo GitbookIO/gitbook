4fc1ae9ef8717ffb12e90b2dfd835bcd436ce277
Scott Cazan <scottcazan@gmail.com>
2024-07-18 09:45:43 +0200

    Update src/components/Search/server-actions.tsx
    Co-authored-by: Samy Pessé <samypesse@gmail.com>

--- src/components/Search/server-actions.tsx
+++ src/components/Search/server-actions.tsx
@@ -74,7 +74,7 @@ export async function searchSiteContent(args: {
                 siteSpaceIds,
                 cacheBust,
             ),
-            api.getSiteSpaces({
+            siteSpaceIds ? null : api.getSiteSpaces({
                 organizationId: pointer.organizationId,
                 siteId: pointer.siteId,
                 siteShareKey: pointer.siteShareKey,
