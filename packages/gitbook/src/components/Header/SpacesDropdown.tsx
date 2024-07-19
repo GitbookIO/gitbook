b223be0665484cd6bebe183bf1842c2c997dec47
Scott Cazan <scottcazan@gmail.com>
2024-07-19 14:48:43 +0200

    added in title on API so we dont need the extra calls now

--- packages/gitbook/src/components/Header/SpacesDropdown.tsx
+++ packages/gitbook/src/components/Header/SpacesDropdown.tsx
@@ -1,9 +1,6 @@
 import { Collection, Site, Space } from '@gitbook/api';
 
-import { getContentPointer } from '@/app/(space)/fetch';
-import { getCurrentSiteCustomization, getSiteSpaces, getSpaceCustomization } from '@/lib/api';
 import { tcls } from '@/lib/tailwind';
-import { getContentTitle } from '@/lib/utils';
 
 import { Dropdown, DropdownChevron, DropdownMenu } from './Dropdown';
 import { SpacesDropdownMenuItem } from './SpacesDropdownMenuItem';
@@ -15,37 +12,6 @@ export async function SpacesDropdown(props: {
 }) {
     props.parent?.object;
     const { space, spaces } = props;
-    const contentPointer = getContentPointer();
-
-    // fetch customizations based on site vs a legacy space
-    let spaceCustomizations = [];
-    if ('siteId' in contentPointer) {
-        const siteSpaces = await getSiteSpaces({
-            organizationId: contentPointer.organizationId,
-            siteId: contentPointer.siteId,
-        });
-
-        spaceCustomizations = await Promise.all(
-            siteSpaces.map(async ({ id, space }) => [
-                space.id,
-                await getCurrentSiteCustomization({
-                    organizationId: contentPointer.organizationId,
-                    siteId: contentPointer.siteId,
-                    siteSpaceId: id,
-                }),
-            ]),
-        );
-    } else {
-        spaceCustomizations = await Promise.all(
-            spaces.map(async (space) => [space.id, await getSpaceCustomization(space.id)]),
-        );
-    }
-
-    // Map using space IDs as keys for convenience
-    const spaceCustomizationsMap = spaceCustomizations.reduce((accum, layoutKeyVal) => {
-        accum.set(layoutKeyVal[0], layoutKeyVal[1]);
-        return accum;
-    }, new Map());
 
     return (
         <Dropdown
@@ -63,7 +29,7 @@ export async function SpacesDropdown(props: {
                         'text-header-link-500',
                     )}
                 >
-                    {getContentTitle(space, spaceCustomizationsMap.get(space.id) ?? {})}
+                    {space.title}
                     <DropdownChevron />
                 </div>
             )}
@@ -74,10 +40,7 @@ export async function SpacesDropdown(props: {
                         key={`${otherSpace.id}-${index}`}
                         variantSpace={{
                             id: otherSpace.id,
-                            title: getContentTitle(
-                                otherSpace,
-                                spaceCustomizationsMap.get(otherSpace.id) ?? {},
-                            ),
+                            title: otherSpace.title,
                             url: otherSpace.urls.published ?? otherSpace.urls.app,
                         }}
                         active={otherSpace.id === space.id}
