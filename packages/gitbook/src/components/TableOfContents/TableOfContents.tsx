1302f6e79486c85a3a32caecfd7d22a42a32e220
Steeve Pastorelli <steeve@gitbook.com>
2024-08-15 11:21:02 +0200

    review

--- packages/gitbook/src/components/TableOfContents/TableOfContents.tsx
+++ packages/gitbook/src/components/TableOfContents/TableOfContents.tsx
@@ -57,8 +57,27 @@ export function TableOfContents(props: {
         >
             {header ? header : null}
             <TOCScrollContainerProvider
-                withHeaderOffset={withHeaderOffset}
-                withTrademarkEnabled={customization.trademark.enabled}
+                className={tcls(
+                    withHeaderOffset ? 'pt-4' : ['pt-4', 'lg:pt-0'],
+                    'hidden',
+                    'lg:flex',
+                    'flex-grow',
+                    'flex-col',
+                    'overflow-y-auto',
+                    'lg:gutter-stable',
+                    'lg:pr-2',
+                    'group-hover:[&::-webkit-scrollbar]:bg-dark/1',
+                    'group-hover:[&::-webkit-scrollbar-thumb]:bg-dark/3',
+                    '[&::-webkit-scrollbar]:bg-transparent',
+                    '[&::-webkit-scrollbar-thumb]:bg-transparent',
+                    'dark:[&::-webkit-scrollbar]:bg-transparent',
+                    'dark:[&::-webkit-scrollbar-thumb]:bg-transparent',
+                    'dark:group-hover:[&::-webkit-scrollbar]:bg-light/1',
+                    'dark:group-hover:[&::-webkit-scrollbar-thumb]:bg-light/3',
+                    'navigation-open:flex', // can be auto height animated as such https://stackoverflow.com/a/76944290
+                    'lg:-ml-5',
+                    customization.trademark.enabled ? 'lg:pb-20' : 'lg:pb-4',
+                )}
             >
                 <PagesList
                     rootPages={pages}
--- packages/gitbook/src/components/TableOfContents/useScrollToActiveTOCItem.tsx
+++ packages/gitbook/src/components/TableOfContents/useScrollToActiveTOCItem.tsx
@@ -7,37 +7,16 @@ import { tcls } from '@/lib/tailwind';
 const TOCScrollContainerContext = React.createContext<React.RefObject<HTMLDivElement> | null>(null);
 
 export function TOCScrollContainerProvider(
-    props: React.PropsWithChildren<{ withHeaderOffset: boolean; withTrademarkEnabled: boolean }>,
+    props: React.PropsWithChildren<{
+        className: React.HTMLAttributes<HTMLDivElement>['className'];
+    }>,
 ) {
-    const { withHeaderOffset, withTrademarkEnabled, children } = props;
+    const { className, children } = props;
     const scrollContainerRef = React.createRef<HTMLDivElement>();
 
     return (
         <TOCScrollContainerContext.Provider value={scrollContainerRef}>
-            <div
-                ref={scrollContainerRef}
-                className={tcls(
-                    withHeaderOffset ? 'pt-4' : ['pt-4', 'lg:pt-0'],
-                    'hidden',
-                    'lg:flex',
-                    'flex-grow',
-                    'flex-col',
-                    'overflow-y-auto',
-                    'lg:gutter-stable',
-                    'lg:pr-2',
-                    'group-hover:[&::-webkit-scrollbar]:bg-dark/1',
-                    'group-hover:[&::-webkit-scrollbar-thumb]:bg-dark/3',
-                    '[&::-webkit-scrollbar]:bg-transparent',
-                    '[&::-webkit-scrollbar-thumb]:bg-transparent',
-                    'dark:[&::-webkit-scrollbar]:bg-transparent',
-                    'dark:[&::-webkit-scrollbar-thumb]:bg-transparent',
-                    'dark:group-hover:[&::-webkit-scrollbar]:bg-light/1',
-                    'dark:group-hover:[&::-webkit-scrollbar-thumb]:bg-light/3',
-                    'navigation-open:flex', // can be auto height animated as such https://stackoverflow.com/a/76944290
-                    'lg:-ml-5',
-                    withTrademarkEnabled ? 'lg:pb-20' : 'lg:pb-4',
-                )}
-            >
+            <div ref={scrollContainerRef} className={className}>
                 {children}
             </div>
         </TOCScrollContainerContext.Provider>
@@ -58,7 +37,7 @@ export function useScrollToActiveTOCItem(tocItem: {
     const { isActive, linkRef } = tocItem;
 
     const scrollContainerRef = React.useContext(TOCScrollContainerContext);
-    React.useEffect(() => {
+    React.useLayoutEffect(() => {
         if (isActive && linkRef.current && scrollContainerRef?.current) {
             const tocItem = linkRef.current;
             const tocContainer = scrollContainerRef.current;
@@ -75,7 +54,6 @@ export function useScrollToActiveTOCItem(tocItem: {
                 ) {
                     tocContainer.scrollTo({
                         top: tocItemTop - TOC_ITEM_OFFSET,
-                        behavior: 'smooth',
                     });
                 }
             }
