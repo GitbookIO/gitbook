4d476a8cbf5b5ec6b9a61b92be59335c7b6ae618
Brett Jephson <brett@gitbook.io>
2024-08-27 19:03:20 +0100

    Select variables in server url

--- packages/react-openapi/src/OpenAPIServerURLVariable.tsx
+++ packages/react-openapi/src/OpenAPIServerURLVariable.tsx
@@ -12,5 +12,25 @@ export function OpenAPIServerURLVariable(props: {
     variable: OpenAPIV3.ServerVariableObject;
 }) {
     const { variable } = props;
+
+    if (variable.enum && variable.enum.length > 0) {
+        return (<select
+            className={classNames(
+                'openapi-section-select',
+                'openapi-select',
+            )}
+            value={variable.default}
+        >
+            {
+                variable.enum?.map((value: string) => {
+                    return (
+                        <option key={value} value={value}>
+                            {value}
+                        </option>
+                    );
+                }) ?? null}
+        </select>);
+
+    }
     return <span className={classNames('openapi-url-var')}>{variable.default}</span>;
 }
