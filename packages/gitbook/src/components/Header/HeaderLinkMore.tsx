9ab7d40bb6b0a7d1ae63701b5dd16f5b3961cbbf
Samy Pessé <samypesse@gmail.com>
2024-09-06 11:37:32 +0200

    Use ellipsis and fix icon color for more links in the header on small screen

--- packages/gitbook/src/components/Header/HeaderLinkMore.tsx
+++ packages/gitbook/src/components/Header/HeaderLinkMore.tsx
@@ -1,10 +1,17 @@
-import { CustomizationHeaderLink } from '@gitbook/api';
+import {
+    CustomizationHeaderLink,
+    CustomizationHeaderPreset,
+    CustomizationSettings,
+    SiteCustomizationSettings,
+} from '@gitbook/api';
 import React from 'react';
 
 import { ContentRefContext, resolveContentRef } from '@/lib/references';
 
-import { Dropdown, DropdownChevron, DropdownMenu, DropdownMenuItem } from './Dropdown';
+import { Dropdown, DropdownMenu, DropdownMenuItem } from './Dropdown';
 import styles from './headerLinks.module.css';
+import { tcls } from '@/lib/tailwind';
+import { Icon } from '@gitbook/icons';
 
 /**
  * Dropdown menu for header links hidden at small screen size.
@@ -13,13 +20,29 @@ export function HeaderLinkMore(props: {
     label: React.ReactNode;
     links: CustomizationHeaderLink[];
     context: ContentRefContext;
+    customization: CustomizationSettings | SiteCustomizationSettings;
 }) {
-    const { label, links, context } = props;
+    const { label, links, context, customization } = props;
+
+    const isCustomizationDefault =
+        customization.header.preset === CustomizationHeaderPreset.Default;
 
     const renderButton = () => (
         <button className="px-1">
             <span className="sr-only">{label}</span>
-            <DropdownChevron />
+            <Icon
+                icon="ellipsis"
+                className={tcls(
+                    'opacity-6',
+                    'size-3',
+                    'ms-1',
+                    'transition-transform',
+                    'group-hover/dropdown:rotate-180',
+                    !isCustomizationDefault
+                        ? ['text-header-link-500']
+                        : ['text-dark/8', 'dark:text-light/8', 'dark:hover:text-light'],
+                )}
+            />
         </button>
     );
 
