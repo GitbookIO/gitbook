042b850b5df8200dae03e7bfd5794a99501cd915
Brett Jephson <brett@gitbook.io>
2024-09-24 16:24:55 +0100

    RND-2147: automatically scroll TOC (#2478)

---
+++ packages/gitbook/src/components/TableOfContents/TOCScroller.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import React from 'react';
+
+import { ClassValue, tcls } from '@/lib/tailwind';
+
+const TOCScrollContainerRefContext = React.createContext<React.RefObject<HTMLDivElement> | null>(
+    null,
+);
+
+function useTOCScrollContainerRefContext() {
+    const ctx = React.useContext(TOCScrollContainerRefContext);
+    if (!ctx) {
+        throw new Error('Context `TOCScrollContainerRefContext` must be used within Provider');
+    }
+    return ctx;
+}
+
+export function TOCScrollContainer(props: { children: React.ReactNode; className?: ClassValue }) {
+    const { children, className } = props;
+    const scrollContainerRef = React.createRef<HTMLDivElement>();
+
+    return (
+        <TOCScrollContainerRefContext.Provider value={scrollContainerRef}>
+            <div ref={scrollContainerRef} className={tcls(className)}>
+                {children}
+            </div>
+        </TOCScrollContainerRefContext.Provider>
+    );
+}
+
+// Offset to scroll the table of contents item by.
+const TOC_ITEM_OFFSET = 200;
+
+/**
+ * Scrolls the table of contents container to the page item when it becomes active
+ */
+export function useScrollToActiveTOCItem(props: {
+    isActive: boolean;
+    linkRef: React.RefObject<HTMLAnchorElement>;
+}) {
+    const { isActive, linkRef } = props;
+    const scrollContainerRef = useTOCScrollContainerRefContext();
+    const isScrolled = React.useRef(false);
+    React.useLayoutEffect(() => {
+        if (!isActive) {
+            isScrolled.current = false;
+            return;
+        }
+        if (isScrolled.current) {
+            return;
+        }
+        const tocItem = linkRef.current;
+        const tocContainer = scrollContainerRef.current;
+        if (!tocItem || !tocContainer || !isOutOfView(tocItem, tocContainer)) {
+            return;
+        }
+        tocContainer?.scrollTo({
+            top: tocItem.offsetTop - TOC_ITEM_OFFSET,
+        });
+        isScrolled.current = true;
+    }, [isActive, linkRef, scrollContainerRef]);
+}
+
+function isOutOfView(tocItem: HTMLElement, tocContainer: HTMLElement) {
+    const tocItemTop = tocItem.offsetTop;
+    const containerTop = tocContainer.scrollTop;
+    const containerBottom = containerTop + tocContainer.clientHeight;
+    return tocItemTop < containerTop || tocItemTop > containerBottom;
+}
