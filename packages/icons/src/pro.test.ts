6b5e6e92802f2376a4798bbf5ab565232aa6cd90
Samy Pessé <samypesse@gmail.com>
2024-10-14 09:33:16 +0200

    Log warn when not in ci

--- packages/icons/src/pro.test.ts
+++ packages/icons/src/pro.test.ts
@@ -3,8 +3,12 @@ import { validateIconName } from './icons';
 
 it('should have the GitBook custom icon', () => {
     if (!validateIconName('gitbook')) {
-        throw new Error(
-            'The GitBook icon is missing. It indicates that the dependencies were installed without the font-awesome custom package.',
-        );
+        const message =
+            'The GitBook icon is missing. It indicates that the dependencies were installed without the font-awesome custom package.';
+        if (process.env.CI) {
+            throw new Error(message);
+        } else {
+            console.warn(message);
+        }
     }
 });
