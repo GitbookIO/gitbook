c0b03d7edf8d1c09d5ad36afec4deab641fd6756
Samy Pessé <samypesse@gmail.com>
2024-10-26 09:48:01 +0200

    Try with a custom entrypoint

---
+++ packages/gitbook/src/cloudflare-entrypoint.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import nextOnPagesHandler from '@cloudflare/next-on-pages/fetch-handler';
+
+import { withMiddlewareHeadersStorage } from './lib/middleware';
+
+/**
+ * We use a custom entrypoint until we can move to opennext (https://github.com/opennextjs/opennextjs-cloudflare/issues/92).
+ * There is a bug in next-on-pages where headers can't be set on the response in the middleware for RSC requests (https://github.com/cloudflare/next-on-pages/issues/897).
+ */
+export default {
+    async fetch(request, env, ctx) {
+        console.log('start custom fetch');
+        const response = await withMiddlewareHeadersStorage(() =>
+            nextOnPagesHandler.fetch(request, env, ctx),
+        );
+        console.log('end custom fetch');
+
+        return response;
+    },
+} as ExportedHandler<{ ASSETS: Fetcher }>;
