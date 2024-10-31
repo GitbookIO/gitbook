54a797a0b1085e1200cda7db2b24aa5804052e75
Greg Bergé <berge.greg@gmail.com>
2024-10-31 14:02:42 +0100

    Nested list support (#2562)

--- packages/gitbook/src/components/DocumentView/spacing.ts
+++ packages/gitbook/src/components/DocumentView/spacing.ts
@@ -6,46 +6,44 @@ import {
 } from '@gitbook/api';
 import { assertNever } from 'assert-never';
 
-import { ClassValue } from '@/lib/tailwind';
-
 /**
  * Get the line height of a block
  */
 export function getBlockTextStyle(block: DocumentBlock): {
     /** Tailwind class for the text size */
-    textSize: ClassValue;
+    textSize: string;
     /** Tailwind class for the height (h-*) */
-    lineHeight: ClassValue;
+    lineHeight: string;
     /** Tailwind class for the margin top (mt-*) */
-    marginTop?: ClassValue;
+    marginTop?: string;
 } {
     switch (block.type) {
         case 'paragraph':
             return {
-                textSize: ['text-base'],
+                textSize: 'text-base',
                 lineHeight: 'leading-normal',
             };
         case 'heading-1':
             return {
-                textSize: ['text-3xl', 'font-semibold'],
+                textSize: 'text-3xl font-semibold',
                 lineHeight: 'leading-tight',
-                marginTop: '[margin-top:_1em]',
+                marginTop: 'mt-[1em]',
             };
         case 'heading-2':
             return {
-                textSize: ['text-2xl', 'font-semibold'],
+                textSize: 'text-2xl font-semibold',
                 lineHeight: 'leading-snug',
-                marginTop: '[margin-top:_0.75em]',
+                marginTop: 'mt-[0.75em]',
             };
         case 'heading-3':
             return {
-                textSize: ['text-base', 'font-semibold'],
+                textSize: 'text-base font-semibold',
                 lineHeight: 'leading-snug',
-                marginTop: '[margin-top:_0.5em]',
+                marginTop: 'mt-[0.5em]',
             };
         case 'divider':
             return {
-                textSize: [],
+                textSize: '',
                 lineHeight: 'leading-none',
             };
         case 'list-ordered':
@@ -56,7 +54,7 @@ export function getBlockTextStyle(block: DocumentBlock): {
             return getBlockTextStyle(block.nodes[0]);
         default:
             return {
-                textSize: ['text-base'],
+                textSize: 'text-base',
                 lineHeight: 'leading-normal',
             };
     }
