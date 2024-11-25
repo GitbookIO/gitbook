74ce0be5ea2ab5359a464ed0bcdb5c633705387a
Greg Bergé <berge.greg@gmail.com>
2024-11-25 14:09:13 +0100

    Upgrade Next.js

--- packages/gitbook/src/app/(site)/(content)/(.)[[...pathname]]/page.tsx
+++ packages/gitbook/src/app/(site)/(content)/(.)[[...pathname]]/page.tsx
@@ -1,4 +1,4 @@
 // We put this page to enable parallel routing and being able to use a `loading.tsx` component
 // only for in-app navigation.
-export { default } from '../[[...pathname]]/page';
 export * from '../[[...pathname]]/page';
+export { default } from '../[[...pathname]]/page';
