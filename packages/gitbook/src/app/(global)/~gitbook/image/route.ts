c50e23a102ac3f3c93abc41cfd2f807b9a18f893
Samy Pessé <samypesse@gmail.com>
2024-12-02 22:34:54 +0100

    Format

--- packages/gitbook/src/app/(global)/~gitbook/image/route.ts
+++ packages/gitbook/src/app/(global)/~gitbook/image/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest) {
     const signature = request.nextUrl.searchParams.get('sign');
 
     // The current signature algorithm sets version as 2, but we need to support the older version as well
-    const signatureVersion = (request.nextUrl.searchParams.get('sv') as string | undefined);
+    const signatureVersion = request.nextUrl.searchParams.get('sv') as string | undefined;
     if (!urlParam || !signature) {
         return new Response('Missing url/sign parameters', { status: 400 });
     }
