eb9905b40c933ea9ed430fc62de100547850dda0
Greg Bergé <berge.greg@gmail.com>
2025-01-07 16:20:12 +0100

    Fix localStorage security error catching
    Fix GITBOOK-OPEN-1WEA

--- packages/gitbook/src/lib/local-storage.ts
+++ packages/gitbook/src/lib/local-storage.ts
@@ -2,11 +2,10 @@
  * Get an item from local storage safely.
  */
 export function getItem<T>(key: string, defaultValue: T): T {
-    if (typeof localStorage === 'undefined') {
-        return defaultValue;
-    }
-
     try {
+        if (typeof localStorage === 'undefined') {
+            return defaultValue;
+        }
         const stored = localStorage.getItem(key);
         return stored ? (JSON.parse(stored) as T) : defaultValue;
     } catch (error) {
@@ -21,11 +20,10 @@ export function getItem<T>(key: string, defaultValue: T): T {
  * Set an item in local storage safely.
  */
 export function setItem(key: string, value: unknown) {
-    if (typeof localStorage === 'undefined') {
-        return;
-    }
-
     try {
+        if (typeof localStorage === 'undefined') {
+            return;
+        }
         localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
         if (error instanceof Error && error.name === 'SecurityError') {
