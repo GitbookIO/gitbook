2344bab1f8cd58c7e3f98f456470600e5f12c116
Greg Bergé <berge.greg@gmail.com>
2025-01-07 16:31:40 +0100

    Fix cookie tracking security error
    Fix GITBOOK-OPEN-1W7D

--- packages/gitbook/src/components/Insights/cookies.ts
+++ packages/gitbook/src/components/Insights/cookies.ts
@@ -20,13 +20,21 @@ export function setCookiesTracking(enabled: boolean) {
  * Return `undefined` if state is not known.
  */
 export function isCookiesTrackingDisabled() {
-    const state = cookies.get(GRANTED_COOKIE);
+    try {
+        const state = cookies.get(GRANTED_COOKIE);
 
-    if (state === 'yes') {
-        return false;
-    } else if (state === 'no') {
-        return true;
+        if (state === 'yes') {
+            return false;
+        } else if (state === 'no') {
+            return true;
+        }
+
+        return undefined;
+    } catch (error) {
+        // If there is a security error, we consider cookies as disabled
+        if (error instanceof Error && error.name === 'SecurityError') {
+            return true;
+        }
+        throw error;
     }
-
-    return undefined;
 }
