2cda8d4cad6d2354a83f6802c126b82bdcd98015
Greg Bergé <berge.greg@gmail.com>
2025-01-08 16:51:07 +0100

    Add more logs

--- packages/gitbook/src/instrumentation.ts
+++ packages/gitbook/src/instrumentation.ts
@@ -2,11 +2,12 @@ import * as Sentry from '@sentry/nextjs';
 import { Instrumentation } from 'next';
 
 export async function register() {
+    console.log('Instrumentation.register');
     await import('../sentry.edge.config');
 }
 
 export const onRequestError: Instrumentation.onRequestError = async (...args) => {
-    console.log('onRequestError', args);
+    console.log('Instrumentation.onRequestError');
     Sentry.captureRequestError(...args);
     // There is an issue on Cloudflare that requires us to flush the events manually.
     // https://github.com/getsentry/sentry-javascript/issues/14931#issuecomment-2577640023
