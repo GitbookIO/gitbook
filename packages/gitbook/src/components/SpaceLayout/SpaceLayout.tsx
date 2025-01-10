16194c50ba22268af1e9e82ba36db316feac5af7
Zeno Kapitein <zenomilan@me.com>
2025-01-10 14:40:10 +0100

    Vertical orientation for sections list on sites without header (#2714)

--- packages/gitbook/src/components/SpaceLayout/SpaceLayout.tsx
+++ packages/gitbook/src/components/SpaceLayout/SpaceLayout.tsx
@@ -27,6 +27,7 @@ import { shouldTrackEvents } from '@/lib/tracking';
 
 import { SpacesDropdown } from '../Header/SpacesDropdown';
 import { InsightsProvider } from '../Insights';
+import { SiteSectionList } from '../SiteSections';
 
 /**
  * Render the entire content of the space (header, table of contents, footer, and page content).
@@ -153,6 +154,12 @@ export async function SpaceLayout(props: {
                                             </SearchButton>
                                         </React.Suspense>
                                     </div>
+                                )}
+                                {!withTopHeader && withSections && sections && (
+                                    <SiteSectionList
+                                        className={tcls('hidden', 'lg:block')}
+                                        sections={sections}
+                                    />
                                 )}
                                 {withVariants && (
                                     <SpacesDropdown
