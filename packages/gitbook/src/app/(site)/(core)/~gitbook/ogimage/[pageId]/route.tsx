0fb13d5525286fe627c15cb8c626e74fef078a10
Greg Bergé <berge.greg@gmail.com>
2025-01-10 20:46:40 +0100

    Refactor and optimize og image generation

--- packages/gitbook/src/app/(site)/(core)/~gitbook/ogimage/[pageId]/route.tsx
+++ packages/gitbook/src/app/(site)/(core)/~gitbook/ogimage/[pageId]/route.tsx
@@ -7,12 +7,26 @@ import React from 'react';
 
 import { getGitBookContextFromHeaders } from '@/lib/gitbook-context';
 import { getAbsoluteHref } from '@/lib/links';
-import { tcls } from '@/lib/tailwind';
 import { getContentTitle } from '@/lib/utils';
 
 import { PageIdParams, fetchPageData } from '../../../../fetch';
 
 export const runtime = 'edge';
+
+async function loadGoogleFont(font: string, text: string) {
+    const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`;
+    const css = await (await fetch(url)).text();
+    const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+
+    if (resource) {
+        const response = await fetch(resource[1]);
+        if (response.status == 200) {
+            return await response.arrayBuffer();
+        }
+    }
+
+    throw new Error('failed to load font data');
+}
 
 /**
  * Render the OpenGraph image for a space.
@@ -21,39 +35,35 @@ export async function GET(req: NextRequest, { params }: { params: Promise<PageId
     const ctx = getGitBookContextFromHeaders(req.headers);
     const { space, page, customization, site } = await fetchPageData(ctx, await params);
 
+    // If user configured a custom social preview, we redirect to it.
     if (customization.socialPreview.url) {
-        // If user configured a custom social preview, we redirect to it.
         redirect(customization.socialPreview.url);
     }
 
-    async function fetchFont(url: URL): Promise<ArrayBuffer> {
-        const result = await fetch(url.href, { cache: 'no-cache' });
-        if (result.status === 200) {
-            const arrayBuffer = await result.arrayBuffer();
-            return arrayBuffer;
-        }
-        throw new Error('Failed to fetch font');
-    }
+    // Compute all text to load only the necessary fonts
+    const contentTitle = customization.header.logo
+        ? ''
+        : getContentTitle(space, customization, site ?? null);
+    const pageTitle = page
+        ? page.title.length > 64
+            ? page.title.slice(0, 64) + '...'
+            : page.title
+        : 'Not found';
+    const pageDescription =
+        page?.description && page?.title.length <= 64
+            ? page.description.length > 164
+                ? page.description.slice(0, 164) + '...'
+                : page.description
+            : '';
 
     // TODO: Support all fonts available in GitBook
-    // Right now this is impossible since next/font/google does not expose the cached font file
-    // Another option would be to use the Satori prop `loadAdditionalAsset` [example](https://github.com/vercel/satori/blob/main/playground/pages/index.tsx),
-    // but this prop isn't (yet) exposed through `ImageResponse`.
-    const interRegular = fetchFont(
-        new URL('../../../../../../fonts/Inter/Inter-Regular.ttf', import.meta.url),
-    );
-    const interBold = fetchFont(
-        new URL('../../../../../../fonts/Inter/Inter-Bold.ttf', import.meta.url),
-    );
+    const inter = loadGoogleFont('Inter', `${contentTitle}${pageTitle}${pageDescription}`);
 
     const theme = customization.themes.default;
     const useLightTheme = theme === 'light';
 
     // We have no access to CSS variables, so we'll have to hardcode some values
-    const baseColors = {
-        light: '#ffffff',
-        dark: '#111827',
-    };
+    const baseColors = { light: '#ffffff', dark: '#111827' };
 
     let colors = {
         background: baseColors[theme],
@@ -101,49 +111,14 @@ export async function GET(req: NextRequest, { params }: { params: Promise<PageId
             break;
     }
 
-    const favicon = (() => {
-        if ('icon' in customization.favicon)
-            return (
-                <img
-                    src={customization.favicon.icon[theme]}
-                    width={40}
-                    height={40}
-                    tw={tcls('mr-4')}
-                    alt="Icon"
-                />
-            );
-        if ('emoji' in customization.favicon)
-            return (
-                <span tw={tcls('text-4xl', 'mr-4')}>
-                    {String.fromCodePoint(parseInt('0x' + customization.favicon.emoji))}
-                </span>
-            );
-        const src = getAbsoluteHref(
-            ctx,
-            `~gitbook/icon?size=medium&theme=${customization.themes.default}`,
-            true,
-        );
-        return <img src={src} alt="Icon" width={40} height={40} tw={tcls('mr-4')} />;
-    })();
-
     return new ImageResponse(
         (
             <div
-                tw={tcls(
-                    'justify-between',
-                    'p-20',
-                    'relative',
-                    'w-full',
-                    'h-full',
-                    'flex',
-                    'flex-col',
-                    `bg-[${colors.background}]`,
-                    `text-[${colors.body}]`,
-                )}
+                tw={`justify-between p-20 relative w-full h-full flex flex-col bg-[${colors.background}] text-[${colors.body}]`}
             >
                 {/* Gradient */}
                 <div
-                    tw={tcls('absolute', 'inset-0')}
+                    tw="absolute inset-0"
                     style={{
                         backgroundImage: `radial-gradient(ellipse 100% 100% at top right , ${colors.gradient}, ${colors.gradient}00)`,
                         opacity: 0.5,
@@ -151,11 +126,7 @@ export async function GET(req: NextRequest, { params }: { params: Promise<PageId
                 ></div>
 
                 {/* Grid */}
-                <img
-                    tw={tcls('absolute', 'inset-0', 'w-[100vw]', 'h-[100vh]')}
-                    src={gridAsset}
-                    alt="Grid"
-                />
+                <img tw="absolute inset-0 w-[100vw] h-[100vh]" src={gridAsset} alt="Grid" />
 
                 {/* Logo */}
                 {customization.header.logo ? (
@@ -169,39 +140,46 @@ export async function GET(req: NextRequest, { params }: { params: Promise<PageId
                         }
                     />
                 ) : (
-                    <div tw={tcls('flex')}>
-                        {favicon}
-                        <h3 tw={tcls('text-4xl', 'my-0')}>
-                            {getContentTitle(space, customization, site ?? null)}
-                        </h3>
+                    <div tw="flex">
+                        {(() => {
+                            if ('icon' in customization.favicon)
+                                return (
+                                    <img
+                                        src={customization.favicon.icon[theme]}
+                                        width={40}
+                                        height={40}
+                                        tw="mr-4"
+                                        alt="Icon"
+                                    />
+                                );
+                            if ('emoji' in customization.favicon)
+                                return (
+                                    <span tw="text-4xl mr-4">
+                                        {String.fromCodePoint(
+                                            parseInt('0x' + customization.favicon.emoji),
+                                        )}
+                                    </span>
+                                );
+                            const src = getAbsoluteHref(
+                                ctx,
+                                `~gitbook/icon?size=medium&theme=${customization.themes.default}`,
+                                true,
+                            );
+                            return <img src={src} alt="Icon" width={40} height={40} tw="mr-4" />;
+                        })()}
+                        <h3 tw="text-4xl my-0">{contentTitle}</h3>
                     </div>
                 )}
 
                 {/* Title and description */}
-                <div tw={tcls('flex', 'flex-col')}>
+                <div tw="flex flex-col">
                     <h1
-                        tw={tcls(
-                            'text-8xl',
-                            'my-0',
-                            'tracking-tight',
-                            'leading-none',
-                            'text-left',
-                            `text-[${colors.title}]`,
-                            'font-bold',
-                        )}
+                        tw={`text-8xl my-0 tracking-light leading-none text-left text-[${colors.title}] font-bold`}
                     >
-                        {page
-                            ? page.title.length > 64
-                                ? page.title.slice(0, 64) + '...'
-                                : page.title
-                            : 'Not found'}
+                        {pageTitle}
                     </h1>
-                    {page?.description && page?.title.length <= 64 ? (
-                        <h2 tw={tcls('text-4xl', 'mb-0', 'mt-8', 'w-[75%]', 'font-normal')}>
-                            {page.description.length > 164
-                                ? page.description.slice(0, 164) + '...'
-                                : page.description}
-                        </h2>
+                    {pageDescription ? (
+                        <h2 tw="text-4xl mb-0 mt-8 w-[75%] font-normal">{pageDescription}</h2>
                     ) : null}
                 </div>
             </div>
@@ -212,14 +190,7 @@ export async function GET(req: NextRequest, { params }: { params: Promise<PageId
             fonts: [
                 {
                     name: 'Inter',
-                    data: await interRegular,
-                    weight: 400,
-                    style: 'normal',
-                },
-                {
-                    name: 'Inter',
-                    data: await interBold,
-                    weight: 700,
+                    data: await inter,
                     style: 'normal',
                 },
             ],
