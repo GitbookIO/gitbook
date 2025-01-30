de8caac3b7bc670519e4811eca5bb17e2ce8b70b
Greg Bergé <berge.greg@gmail.com>
2025-01-30 14:34:53 +0100

    Document action

--- packages/gitbook/src/components/DocumentView/CodeBlock/highlight-action.ts
+++ packages/gitbook/src/components/DocumentView/CodeBlock/highlight-action.ts
@@ -4,6 +4,11 @@ import { DocumentBlockCode } from '@gitbook/api';
 
 import { highlight, RenderedInline } from './highlight';
 
+/**
+ * Server action to highlight a code block.
+ * By using a server action, we can avoid loading the highlighter on the client-side
+ * and increasing the bundle size.
+ */
 export async function highlightAction(block: DocumentBlockCode, inlines: RenderedInline[]) {
     return highlight(block, inlines);
 }
