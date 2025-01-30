40b2c03a41d403731015c9ae2a219e442a32e6ee
Greg Bergé <berge.greg@gmail.com>
2025-01-30 17:05:51 +0100

    Update test

--- packages/react-openapi/src/parser/index.test.ts
+++ packages/react-openapi/src/parser/index.test.ts
@@ -1,11 +1,14 @@
-import { it } from 'bun:test';
+import { expect, it } from 'bun:test';
 import { readFile } from 'node:fs/promises';
-import { parseOpenAPIV3 } from './v3';
+import { parseOpenAPI } from '.';
 
 it('should parse and not give a recursive structure', async () => {
-    await parseOpenAPIV3({
+    const schema = await parseOpenAPI({
         value: await readFile(new URL('./fixtures/spec-example.json', import.meta.url), 'utf-8'),
         url: 'https://example.com',
         parseMarkdown: async (input) => input,
     });
+
+    JSON.stringify(schema);
+    expect(schema.openapi).toBe('3.0.0');
 });
