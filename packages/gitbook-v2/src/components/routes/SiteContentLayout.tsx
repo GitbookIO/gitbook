c3cb1c033b188097c0e3e053268dbc30ad422405
Samy Pessé <samypesse@gmail.com>
2025-01-31 12:03:44 +0100

    Fix urls

--- packages/gitbook-v2/src/components/routes/SiteContentLayout.tsx
+++ packages/gitbook-v2/src/components/routes/SiteContentLayout.tsx
@@ -11,12 +11,12 @@ export async function SiteContentLayout({
     children: React.ReactNode;
 }) {
     const { api } = context;
-    const { data: site } = await api.orgs.getSiteById(context.organizationId, context.siteId);
+    const { data: publishedSite } = await api.orgs.getPublishedContentSite(context.organizationId, context.siteId);
 
     return (
         <html lang="en">
             <body>
-                <h1>{site.title}</h1>
+                <h1>{publishedSite.site.title}</h1>
                 {children}
             </body>
         </html>
--- packages/gitbook-v2/src/components/routes/SiteContentPage.tsx
+++ packages/gitbook-v2/src/components/routes/SiteContentPage.tsx
@@ -2,12 +2,11 @@ import { GitBookSiteContext } from '@/lib/context';
 
 export async function SiteContentPage({ context }: { context: GitBookSiteContext }) {
     const { api } = context;
-    const { data: site } = await api.orgs.getSiteById(context.organizationId, context.siteId);
+    const { data } = await api.orgs.getPublishedContentSite(context.organizationId, context.siteId);
 
     return (
         <div>
-            <h1>{site.title}</h1>
-            <p>Found site {site.id}</p>
+            <p>Found site {data.site.id}</p>
         </div>
     );
 }
