6fc1bceba587a8cd5d7fe869a5210a5d51550a5a
Valentino Hudhra <2587839+valentin0h@users.noreply.github.com>
2025-02-19 17:05:33 +0100

    Apply suggestions from code review
    Co-authored-by: Zeno Kapitein <zenomilan@me.com>

--- packages/colors/src/colors.ts
+++ packages/colors/src/colors.ts
@@ -4,12 +4,12 @@
 export const DEFAULT_PRIMARY_COLOR = '#346DDB';
 
 /**
- *
+ * The darkest color that exists in GitBook, used as the relative minimum of every generated color scale.
  */
 export const DARK_BASE = '#1D1D1D';
 
 /**
- *
+ * The lightest color that exists in GitBook, used as the relative maximum of every generated color scale.
  */
 export const LIGHT_BASE = '#FFFFFF';
 
@@ -19,21 +19,21 @@ export const LIGHT_BASE = '#FFFFFF';
 export const DEFAULT_TINT_COLOR = '#787878';
 
 /**
- *
+ * Used for informational messages and neutral alerts.
  */
 export const DEFAULT_HINT_INFO_COLOR = '#787878';
 
 /**
- *
+ * Used for showing important information or non-critical warnings.
  */
 export const DEFAULT_HINT_WARNING_COLOR = '#FE9A00';
 
 /**
- *
+ * Used for destructive actions or raising attention to critical information.
  */
 export const DEFAULT_HINT_DANGER_COLOR = '#FB2C36';
 
 /**
- *
+ * Used for showing positive actions or achievements.
  */
 export const DEFAULT_HINT_SUCCESS_COLOR = '#00C950';
