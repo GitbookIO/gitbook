f997216b7c1b6414062679d4b975f55c72aef16b
Samy Pessé <samypesse@gmail.com>
2025-02-25 00:57:55 +0100

    Fix icons

--- packages/gitbook-v2/next.config.mjs
+++ packages/gitbook-v2/next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
         // We can't use dynamicIO because it doesn't accept reading params in the root layout
         // dynamicIO: true,
     },
+
+    env: {
+        BUILD_VERSION: (process.env.GITHUB_SHA ?? '').slice(0, 7),
+        GITBOOK_ASSETS_PREFIX: process.env.GITBOOK_ASSETS_PREFIX,
+        GITBOOK_ICONS_URL: process.env.GITBOOK_ICONS_URL,
+        GITBOOK_ICONS_TOKEN: process.env.GITBOOK_ICONS_TOKEN,
+        NEXT_SERVER_ACTIONS_ENCRYPTION_KEY: process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY,
+    }
 };
 
 export default nextConfig;
