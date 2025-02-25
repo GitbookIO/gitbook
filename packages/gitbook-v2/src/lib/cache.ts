72119f2c81a622ffcbb5afbe1156ff18930d16e1
Samy Pessé <samypesse@gmail.com>
2025-02-25 19:49:23 +0100

    Align cache tag

--- packages/gitbook-v2/src/lib/cache.ts
+++ packages/gitbook-v2/src/lib/cache.ts
@@ -2,33 +2,33 @@
  * Get the cache tag for a site.
  */
 export function getSiteCacheTag(siteId: string) {
-    return `sites/${siteId}`;
+    return `site:${siteId}`;
 }
 
 /**
  * Get the cache tag for a hostname.
  */
 export function getHostnameCacheTag(hostname: string) {
-    return `hostnames/${hostname}`;
+    return `url:${hostname}`;
 }
 
 /**
  * Get the cache tag for an OpenAPI spec.
  */
 export function getOpenAPISpecCacheTag(organizationId: string, slug: string) {
-    return `organizations/${organizationId}/openapis/${slug}`;
+    return `organization:${organizationId}:openapi:${slug}`;
 }
 
 /**
  * Get the cache tag for a space.
  */
 export function getSpaceCacheTag(spaceId: string) {
-    return `spaces/${spaceId}`;
+    return `space:${spaceId}`;
 }
 
 /**
  * Get the cache tag for a change request.
  */
 export function getChangeRequestCacheTag(spaceId: string, changeRequestId: string) {
-    return `spaces/${spaceId}/change-requests/${changeRequestId}`;
+    return `space:${spaceId}:change-request:${changeRequestId}`;
 }
