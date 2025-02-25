9169c8e81a4b8e61053d9aad39f4c6e7371d7384
Samy Pessé <samypesse@gmail.com>
2025-02-25 16:42:51 +0100

    Add generateViewport and generateMetadata for pages

--- packages/gitbook-v2/src/app/dynamic/[mode]/[...url]/page.tsx
+++ packages/gitbook-v2/src/app/dynamic/[mode]/[...url]/page.tsx
@@ -1,8 +1,35 @@
 import { getDynamicSiteContext, RouteParams } from '@v2/app/utils';
-import { SitePage } from '@/components/SitePage';
+import {
+    generateSitePageMetadata,
+    generateSitePageViewport,
+    SitePage,
+} from '@/components/SitePage';
+import { Metadata, Viewport } from 'next';
+
+type PageProps = {
+    params: Promise<RouteParams>;
+    searchParams: Promise<{ fallback?: string }>;
+};
 
 export default async function Page({ params }: { params: Promise<RouteParams> }) {
     const { context, pathname } = await getDynamicSiteContext(await params);
 
     return <SitePage context={context} pageParams={{ pathname }} redirectOnFallback={true} />;
+}
+
+export async function generateViewport(props: PageProps): Promise<Viewport> {
+    const { context } = await getDynamicSiteContext(await props.params);
+    return generateSitePageViewport(context);
+}
+
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+    const { context, pathname } = await getDynamicSiteContext(await props.params);
+    const searchParams = await props.searchParams;
+
+    return generateSitePageMetadata({
+        context,
+        pageParams: { pathname },
+        redirectOnFallback: true,
+        fallback: !!searchParams.fallback,
+    });
 }
--- packages/gitbook-v2/src/app/static/[mode]/[...url]/page.tsx
+++ packages/gitbook-v2/src/app/static/[mode]/[...url]/page.tsx
@@ -1,16 +1,39 @@
 import { unstable_cacheTag as cacheTag } from 'next/cache';
 import { getSiteCacheTag } from '@v2/lib/cache';
 import { getStaticSiteContext, RouteParams } from '@v2/app/utils';
-import { SitePage } from '@/components/SitePage';
+import {
+    generateSitePageMetadata,
+    generateSitePageViewport,
+    SitePage,
+} from '@/components/SitePage';
+import { Metadata, Viewport } from 'next';
 
 export const dynamic = 'force-static';
 
-export default async function Page({ params }: { params: Promise<RouteParams> }) {
+type PageProps = {
+    params: Promise<RouteParams>;
+};
+
+export default async function Page(props: PageProps) {
     'use cache';
 
-    const { context, pathname } = await getStaticSiteContext(await params);
+    const { context, pathname } = await getStaticSiteContext(await props.params);
 
     cacheTag(getSiteCacheTag(context.site.id));
 
     return <SitePage context={context} pageParams={{ pathname }} redirectOnFallback={true} />;
+}
+
+export async function generateViewport(props: PageProps): Promise<Viewport> {
+    const { context } = await getStaticSiteContext(await props.params);
+    return generateSitePageViewport(context);
+}
+
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+    const { context, pathname } = await getStaticSiteContext(await props.params);
+    return generateSitePageMetadata({
+        context,
+        pageParams: { pathname },
+        redirectOnFallback: true,
+    });
 }
