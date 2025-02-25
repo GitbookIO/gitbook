9edd3550910fd1ef13ec8474efeb5c109f96aa3b
Samy Pessé <samypesse@gmail.com>
2025-02-25 15:07:12 +0100

    Test

--- packages/gitbook-v2/src/app/dynamic/[mode]/[...url]/not-found.tsx
+++ packages/gitbook-v2/src/app/dynamic/[mode]/[...url]/not-found.tsx
@@ -3,10 +3,11 @@ import { getStaticSiteContext } from '@v2/app/utils';
 import { getURLFromMiddleware, getURLModeFromMiddleware } from '@v2/lib/middleware';
 
 export default async function NotFound() {
-    const { context } = await getStaticSiteContext({
-        url: await getURLFromMiddleware(),
-        mode: await getURLModeFromMiddleware(),
-    });
+    return <div>Not Found dynamic (TODO)</div>;
+    // const { context } = await getStaticSiteContext({
+    //     url: await getURLFromMiddleware(),
+    //     mode: await getURLModeFromMiddleware(),
+    // });
 
-    return <SitePageNotFound context={context} />;
+    // return <SitePageNotFound context={context} />;
 }
