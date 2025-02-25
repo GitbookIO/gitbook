8125226d3e21c63cc857c77f44c8143387e8d6aa
Samy Pessé <samypesse@gmail.com>
2025-02-25 11:58:10 +0100

    Add todo for not-found

--- packages/gitbook-v2/src/app/dynamic/[mode]/[...url]/not-found.tsx
+++ packages/gitbook-v2/src/app/dynamic/[mode]/[...url]/not-found.tsx
@@ -2,6 +2,8 @@ import { SitePageNotFound } from '@/components/SitePage';
 import { getDynamicSiteContext, RouteParams } from '@v2/app/utils';
 
 export default async function NotFound({ params }: { params: Promise<RouteParams> }) {
+    // TODO: how to make it work as not-found doesn't get any props?
+
     const { context } = await getDynamicSiteContext(await params);
 
     return <SitePageNotFound context={context} />;
--- packages/gitbook-v2/src/app/static/[mode]/[...url]/not-found.tsx
+++ packages/gitbook-v2/src/app/static/[mode]/[...url]/not-found.tsx
@@ -2,6 +2,8 @@ import { SitePageNotFound } from '@/components/SitePage';
 import { getStaticSiteContext, RouteParams } from '@v2/app/utils';
 
 export default async function NotFound({ params }: { params: Promise<RouteParams> }) {
+    // TODO: how to make it work as not-found doesn't get any props?
+
     const { context } = await getStaticSiteContext(await params);
 
     return <SitePageNotFound context={context} />;
