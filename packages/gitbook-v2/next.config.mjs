f052352ce433a6937411c4c2686351021207221d
Samy Pessé <samypesse@gmail.com>
2025-02-26 10:08:13 +0100

    Prepare new host for production of v2

--- packages/gitbook-v2/next.config.mjs
+++ packages/gitbook-v2/next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
 
     env: {
         BUILD_VERSION: (process.env.GITHUB_SHA ?? '').slice(0, 7),
+        GITBOOK_URL: process.env.GITBOOK_URL,
         GITBOOK_ASSETS_PREFIX: process.env.GITBOOK_ASSETS_PREFIX,
         GITBOOK_ICONS_URL: process.env.GITBOOK_ICONS_URL,
         GITBOOK_ICONS_TOKEN: process.env.GITBOOK_ICONS_TOKEN,
