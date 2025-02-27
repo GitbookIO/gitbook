503093da36ed37079ecbdca64236944e5767efb5
taranvohra <taranvohra@outlook.com>
2025-02-28 00:57:34 +0530

    Fix getProxyModeBasePath

--- packages/gitbook/src/lib/paths.ts
+++ packages/gitbook/src/lib/paths.ts
@@ -18,3 +18,14 @@ export function removeTrailingSlash(path: string): string {
 export function removeLeadingSlash(path: string): string {
     return path.replace(/^\/+/, '');
 }
+
+/**
+ * Normalize a pathname to make it start with a slash
+ */
+export function normalizePathname(pathname: string): string {
+    if (!pathname.startsWith('/')) {
+        pathname = `/${pathname}`;
+    }
+
+    return pathname;
+}
