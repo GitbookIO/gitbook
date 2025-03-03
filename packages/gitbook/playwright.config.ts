92de73e290905340cfe29100d274669a10d3b0d8
Samy Pessé <samypesse@gmail.com>
2025-03-03 09:52:51 +0100

    Add back disable of vercel toolbar

--- packages/gitbook/playwright.config.ts
+++ packages/gitbook/playwright.config.ts
@@ -19,5 +19,8 @@ export default defineConfig({
         baseURL: process.env.BASE_URL || 'http://localhost:3000',
         trace: 'on-first-retry',
         screenshot: 'only-on-failure',
+        extraHTTPHeaders: {
+            'x-vercel-skip-toolbar': '1',
+        },
     },
 });
