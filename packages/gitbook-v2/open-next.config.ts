80237c34f94740b77671c478aa5b7c915156e13b
Samy Pessé <samypesse@gmail.com>
2025-03-03 16:54:16 +0100

    Allow all origins (including sub-domains) for server actions

--- packages/gitbook-v2/open-next.config.ts
+++ packages/gitbook-v2/open-next.config.ts
@@ -28,7 +28,7 @@ const config = {
 
     experimental: {
         serverActions: {
-            allowedOrigins: ['*'],
+            allowedOrigins: ['*', '*.*', '*.*.*', '*.*.*.*', '*.*.*.*.*'],
         },
     },
 };
