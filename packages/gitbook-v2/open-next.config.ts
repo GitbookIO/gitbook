22db3180eda172241307673f19c489b852064b41
Samy Pessé <samypesse@gmail.com>
2025-03-06 19:11:31 +0100

    Configure KV

--- packages/gitbook-v2/open-next.config.ts
+++ packages/gitbook-v2/open-next.config.ts
@@ -1,30 +1,8 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import kvIncrementalCache from '@opennextjs/cloudflare/kv-cache';
+import memoryQueue from '@opennextjs/cloudflare/memory-queue';
 
-import cache from '@opennextjs/cloudflare/kvCache';
-
-const config = {
-    default: {
-        override: {
-            wrapper: 'cloudflare-node',
-            converter: 'edge',
-            incrementalCache: async () => cache,
-            tagCache: 'dummy',
-            queue: 'dummy',
-        },
-    },
-
-    middleware: {
-        external: true,
-        override: {
-            wrapper: 'cloudflare-edge',
-            converter: 'edge',
-            proxyExternalRequest: 'fetch',
-        },
-    },
-
-    dangerous: {
-        enableCacheInterception: false,
-    },
-};
-
-export default config;
+export default defineCloudflareConfig({
+    incrementalCache: kvIncrementalCache,
+    queue: memoryQueue,
+});
