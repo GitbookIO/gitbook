f7a5164dcaea04c2c8a2f309aeb9e377d5e56ebd
Samy Pessé <samypesse@gmail.com>
2025-03-06 23:33:33 +0100

    Rename worker and configure D1

--- packages/gitbook-v2/open-next.config.ts
+++ packages/gitbook-v2/open-next.config.ts
@@ -1,8 +1,10 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import d1TagCache from '@opennextjs/cloudflare/d1-tag-cache';
 import kvIncrementalCache from '@opennextjs/cloudflare/kv-cache';
 import memoryQueue from '@opennextjs/cloudflare/memory-queue';
 
 export default defineCloudflareConfig({
     incrementalCache: kvIncrementalCache,
     queue: memoryQueue,
+    tagCache: d1TagCache,
 });
