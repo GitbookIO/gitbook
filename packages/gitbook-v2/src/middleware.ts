11e6f22919ba11fdb8684c5540cea0bb335bb730
taranvohra <taranvohra@outlook.com>
2025-03-07 19:19:26 +0530

    log all header

--- packages/gitbook-v2/src/middleware.ts
+++ packages/gitbook-v2/src/middleware.ts
@@ -171,6 +171,10 @@ function serveErrorResponse(error: Error) {
  *      URL is taken from the pathname.
  */
 function getSiteURLFromRequest(request: NextRequest): URLWithMode | null {
+    request.headers.forEach((value, key) => {
+        console.log('header', { key, value });
+    });
+
     const xGitbookUrl = request.headers.get('x-gitbook-url');
     if (xGitbookUrl) {
         return {
@@ -200,7 +204,6 @@ function getSiteURLFromRequest(request: NextRequest): URLWithMode | null {
     }
 
     const xForwardedHost = request.headers.get('x-forwarded-host');
-    console.log('x-forwarded-host', xForwardedHost);
     // The x-forwarded-host is set by Vercel for all requests
     // so we ignore it if the hostname is the same as the instance one.
     if (xForwardedHost) {
