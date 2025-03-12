b011ea0af8d1eff1134a8c9351990a954605eb46
Samy Pessé <samypesse@gmail.com>
2025-03-13 00:25:48 +0100

    Simplify deferred codeblock highlighting and fix streaming (#2977)

--- packages/gitbook/src/components/DocumentView/CodeBlock/ClientCodeBlock.tsx
+++ packages/gitbook/src/components/DocumentView/CodeBlock/ClientCodeBlock.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import type { DocumentBlockCode } from '@gitbook/api';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import { useInViewportListener } from '@/components/hooks/useInViewportListener';
 import { useScrollListener } from '@/components/hooks/useScrollListener';
-import { useDebounceCallback, useEventCallback } from 'usehooks-ts';
+import { useDebounceCallback } from 'usehooks-ts';
 import type { BlockProps } from '../Block';
 import { CodeBlockRenderer } from './CodeBlockRenderer';
 import type { HighlightLine, RenderedInline } from './highlight';
@@ -22,53 +22,82 @@ type ClientBlockProps = Pick<BlockProps<DocumentBlockCode>, 'block' | 'style'> &
 export function ClientCodeBlock(props: ClientBlockProps) {
     const { block, style, inlines } = props;
     const blockRef = useRef<HTMLDivElement>(null);
-    const processedRef = useRef(false);
-    const isInViewportRef = useRef<boolean | null>(null);
-    const [lines, setLines] = useState<HighlightLine[]>(() => plainHighlight(block, []));
+    const isInViewportRef = useRef(false);
+    const [isInViewport, setIsInViewport] = useState(false);
+    const plainLines = useMemo(() => plainHighlight(block, []), [block]);
+    const [lines, setLines] = useState<null | HighlightLine[]>(null);
 
     // Preload the highlighter when the block is mounted.
     useEffect(() => {
         import('./highlight').then(({ preloadHighlight }) => preloadHighlight(block));
     }, [block]);
 
-    const runHighlight = useEventCallback(() => {
-        if (processedRef.current) {
-            return;
-        }
-        if (typeof window !== 'undefined') {
-            import('./highlight').then(({ highlight }) => {
-                highlight(block, inlines).then((lines) => {
-                    setLines(lines);
-                    processedRef.current = true;
-                });
-            });
-        }
-    });
-    const debouncedRunHighlight = useDebounceCallback(runHighlight, 1000);
+    // When user scrolls, we need to wait for the scroll to finish before running the highlight
+    const isScrollingRef = useRef(false);
+    const onFinishScrolling = useDebounceCallback(() => {
+        isScrollingRef.current = false;
 
+        // If the block is in the viewport after the scroll, we need to run the highlight
+        if (isInViewportRef.current) {
+            setIsInViewport(true);
+        }
+    }, 100);
+    useScrollListener(
+        () => {
+            isScrollingRef.current = true;
+            onFinishScrolling();
+        },
+        useRef(typeof window !== 'undefined' ? window : null)
+    );
+
+    // Detect when the block is in viewport
     useInViewportListener(
         blockRef,
         (isInViewport, disconnect) => {
-            // Disconnect once in viewport
-            if (isInViewport) {
-                disconnect();
-                // If it's initially in viewport, we need to run the highlight
-                if (isInViewportRef.current === null) {
-                    runHighlight();
-                }
-            }
             isInViewportRef.current = isInViewport;
+            if (isScrollingRef.current) {
+                // If the user is scrolling, we don't want to run the highlight
+                // because it will be run when the scroll is finished
+                return;
+            }
+
+            if (isInViewport) {
+                // Disconnect once in viewport
+                disconnect();
+                setIsInViewport(true);
+            }
         },
         { rootMargin: '200px' }
     );
 
-    const handleScroll = useDebounceCallback(() => {
-        if (isInViewportRef.current) {
-            debouncedRunHighlight();
+    // When the block is visible or updated, we need to re-run the highlight
+    useEffect(() => {
+        if (isInViewport) {
+            // If the block is in viewport, we need to run the highlight
+            let cancelled = false;
+
+            if (typeof window !== 'undefined') {
+                import('./highlight').then(({ highlight }) => {
+                    highlight(block, inlines).then((lines) => {
+                        if (cancelled) {
+                            return;
+                        }
+
+                        setLines(lines);
+                    });
+                });
+            }
+
+            return () => {
+                cancelled = true;
+            };
         }
-    }, 80);
 
-    useScrollListener(handleScroll, useRef(typeof window !== 'undefined' ? window : null));
+        // Otherwise if the block is not in viewport, we reset to the plain lines
+        setLines(null);
+    }, [isInViewport, block, inlines]);
 
-    return <CodeBlockRenderer ref={blockRef} block={block} style={style} lines={lines} />;
+    return (
+        <CodeBlockRenderer ref={blockRef} block={block} style={style} lines={lines ?? plainLines} />
+    );
 }
