c72367adb691b7993cf79097b0e40493ce806ad2
Zeno Kapitein <zeno@gitbook.io>
2025-03-20 13:55:33 +0100

    Add close button, better support bold&gradient themes

--- packages/gitbook/src/components/Announcement/AnnouncementBanner.tsx
+++ packages/gitbook/src/components/Announcement/AnnouncementBanner.tsx
@@ -5,6 +5,7 @@ import { tcls } from '@/lib/tailwind';
 import type { CustomizationAnnouncement } from '@gitbook/api';
 import { Icon, type IconName } from '@gitbook/icons';
 import Link from 'next/link';
+import React from 'react';
 import { CONTAINER_STYLE } from '../layout';
 import { linkStyles } from '../primitives';
 
@@ -13,76 +14,93 @@ export function AnnouncementBanner(props: {
     contentRef: ResolvedContentRef | null;
 }) {
     const { announcement, contentRef } = props;
+    const [visible, setVisible] = React.useState(true);
 
     const hasLink = announcement.link && contentRef?.href;
 
     const Tag = hasLink ? Link : 'div';
     const style = BANNER_STYLES[announcement.style];
-    // const linkIcon = announcement.link?.to.kind == 'url' : 'chevron-right';
 
-    return (
-        <div className={tcls('mt-4', 'mb-2', CONTAINER_STYLE)}>
-            <Tag
-                href={contentRef?.href ?? ''}
-                className={tcls(
-                    'flex items-center justify-center gap-3 rounded-md straight-corners:rounded-none px-4 py-3 text-neutral-strong text-sm transition-colors',
-                    style.background,
-                    hasLink && style.hover
-                )}
-            >
-                <Icon
-                    icon={style.icon as IconName}
-                    className={`size-4 shrink-0 ${style.iconColor}`}
-                />
-                <div>
-                    {announcement.message}
-                    {hasLink ? (
-                        announcement.link?.title ? (
-                            // When there's a link, the whole banner is always clickable.
-                            // Since we can't nest links inside links, we make this span *look* like a link.
-                            <span className={tcls(linkStyles, style.link, 'ml-1')}>
-                                {announcement.link.title}
-                                {contentRef?.icon ?? (
-                                    <Icon icon="chevron-right" className="ml-1 inline size-2" />
-                                )}
-                            </span>
-                        ) : (
-                            (contentRef?.icon ?? <Icon icon="chevron-right" className="size-3" />)
-                        )
-                    ) : null}
-                </div>
-            </Tag>
+    return visible === true ? (
+        <div className="theme-bold:bg-header-background pt-4 pb-2">
+            <div className={tcls('relative', CONTAINER_STYLE)}>
+                <Tag
+                    href={contentRef?.href ?? ''}
+                    className={tcls(
+                        'flex w-full items-start justify-center gap-3 overflow-hidden rounded-md straight-corners:rounded-none py-3 pr-12 pl-4 text-neutral-strong text-sm theme-bold:ring-1 theme-gradient:ring-1 ring-inset transition-colors',
+                        style.container,
+                        hasLink && style.hover
+                    )}
+                >
+                    <Icon
+                        icon={style.icon as IconName}
+                        className={`mt-0.5 size-4 shrink-0 ${style.iconColor}`}
+                    />
+                    <div>
+                        {announcement.message}
+                        {hasLink ? (
+                            announcement.link?.title ? (
+                                // When there's a link, the whole banner is always clickable.
+                                // Since we can't nest links inside links, we make this span *look* like a link.
+                                <span className={tcls(linkStyles, style.link, 'ml-1')}>
+                                    {announcement.link.title}
+                                    {contentRef?.icon ?? (
+                                        <Icon icon="chevron-right" className="ml-1 inline size-2" />
+                                    )}
+                                </span>
+                            ) : (
+                                (contentRef?.icon ?? (
+                                    <Icon icon="chevron-right" className="size-3" />
+                                ))
+                            )
+                        ) : null}
+                    </div>
+                </Tag>
+                {announcement.style !== 'danger' ? (
+                    <button
+                        className={`absolute top-0 right-4 mt-2 mr-2 rounded straight-corners:rounded-none p-1.5 transition-all hover:ring-1 sm:right-6 md:right-8 ${style.close}`}
+                        type="button"
+                        onClick={() => setVisible(false)}
+                    >
+                        <Icon icon="close" className="size-4" />
+                    </button>
+                ) : null}
+            </div>
         </div>
-    );
+    ) : null;
 }
 
 const BANNER_STYLES = {
     info: {
-        background: 'bg-info',
-        hover: 'hover:bg-info-hover',
+        container: 'bg-info ring-info-subtle',
+        hover: 'hover:bg-info-hover active:bg-info-active',
         icon: 'circle-info',
         iconColor: 'text-info-subtle',
+        close: 'hover:bg-tint-base hover:ring-info-subtle',
         link: '',
     },
     warning: {
-        background: 'bg-warning decoration-warning/6',
+        container: 'bg-warning decoration-warning/6 ring-warning-subtle',
         hover: 'hover:bg-warning-hover',
         icon: 'circle-exclamation',
         iconColor: 'text-warning-subtle',
+        close: 'hover:bg-tint-base hover:ring-warning-subtle',
         link: 'links-default:text-warning links-default:hover:text-warning-strong links-default:decoration-warning/6 links-accent:decoration-warning',
     },
     danger: {
-        background: 'bg-danger',
+        container: 'bg-danger decoration-danger/6 ring-danger-subtle',
         hover: 'hover:bg-danger-hover',
         icon: 'triangle-exclamation',
         iconColor: 'text-danger-subtle',
+        close: 'hover:bg-tint-base hover:ring-danger-subtle',
         link: 'links-default:text-danger links-default:hover:text-danger-strong links-default:decoration-danger/6 links-accent:decoration-danger',
     },
     success: {
-        background: 'bg-success',
+        container: 'bg-success decoration-success/6 ring-success-subtle',
         hover: 'hover:bg-success-hover',
         icon: 'circle-check',
         iconColor: 'text-success-subtle',
+        close: 'hover:bg-tint-base hover:ring-success-subtle',
         link: 'links-default:text-success links-default:hover:text-success-strong links-default:decoration-success/6 links-accent:decoration-success',
     },
 };
