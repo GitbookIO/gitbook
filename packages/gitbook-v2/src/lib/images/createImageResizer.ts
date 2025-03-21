acd17922669e3c73c8596316237ee5495207cb3e
Samy Pessé <samypesse@gmail.com>
2025-03-21 16:57:26 +0100

    Resize image at the site level

--- packages/gitbook-v2/src/lib/images/createImageResizer.ts
+++ packages/gitbook-v2/src/lib/images/createImageResizer.ts
@@ -62,7 +62,7 @@ export function createImageResizer({
                     url: urlInput,
                 });
 
-                const url = linker.toAbsoluteURL(linker.toPathInSpace('/~gitbook/image'));
+                const url = linker.toAbsoluteURL(linker.toPathInSite('/~gitbook/image'));
                 const searchParams = new URLSearchParams();
                 searchParams.set('url', getImageAPIUrl(urlInput));
 
