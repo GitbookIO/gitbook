aa06079fef28ed49da91c8bf95a1dd74a86f5936
Valentino Hudhra <v.hudhra@gmail.com>
2025-03-26 11:51:26 +0100

    review 1

--- packages/gitbook/src/components/Announcement/AnnouncementDismissedScript.tsx
+++ packages/gitbook/src/components/Announcement/AnnouncementDismissedScript.tsx
@@ -5,47 +5,13 @@ import {
     ANNOUNCEMENT_DAYS_TILL_RESET,
     ANNOUNCEMENT_STORAGE_KEY,
 } from './constants';
+import { checkStorageForDismissedScript } from './script';
 
 /**
  * Inject a script to read the local storage state for the announcement banner and apply the appropriate CSS class to the <html> element as early as possible.
  * Bypasses react state to prevent flickering.
  */
 export function AnnouncementDismissedScript() {
-    /**
-     * Read the local storage state for the announcement banner and apply the appropriate CSS class to the <html> element.
-     */
-    function checkStorageForDismissedScript(
-        storageKey: string,
-        daysTillReset: number,
-        cssClass: string
-    ) {
-        let showBanner = true;
-
-        try {
-            const announcementStateStr = window.localStorage.getItem(storageKey);
-            const announcementState = announcementStateStr
-                ? JSON.parse(announcementStateStr)
-                : undefined;
-
-            if (announcementState && !announcementState.visible) {
-                const dismissedAt = announcementState.at;
-                const nowTime = new Date().getTime();
-
-                // Check if enough days have passed since dismissal
-                const daysSinceDismissal = Math.floor(
-                    (nowTime - dismissedAt) / (1000 * 60 * 60 * 24)
-                );
-                if (daysSinceDismissal < daysTillReset) {
-                    showBanner = false;
-                }
-            }
-        } catch {}
-
-        if (!showBanner) {
-            document.documentElement.classList.add(cssClass);
-        }
-    }
-
     const scriptArgs = JSON.stringify([
         ANNOUNCEMENT_STORAGE_KEY,
         ANNOUNCEMENT_DAYS_TILL_RESET,
