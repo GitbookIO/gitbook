f2b992baef1ca8c45f44968305335b202d5da9a1
Zeno Kapitein <zeno@gitbook.io>
2025-03-26 11:13:19 +0100

    Stop variant selector from growing too large

--- packages/gitbook/src/components/Header/SpacesDropdown.tsx
+++ packages/gitbook/src/components/Header/SpacesDropdown.tsx
@@ -63,7 +63,7 @@ export function SpacesDropdown(props: {
                         className
                     )}
                 >
-                    <span className={tcls('truncate', 'grow')}>{siteSpace.title}</span>
+                    <span className={tcls('line-clamp-1', 'grow')}>{siteSpace.title}</span>
                     <DropdownChevron />
                 </div>
             )}
