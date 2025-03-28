6f1588449979eb08997c967c71ca7fb913249519
Samy Pessé <samypesse@gmail.com>
2025-03-28 11:44:54 +0100

    Update packages/gitbook-v2/src/lib/images/getImageResizingContextId.ts
    Co-authored-by: Taran Vohra <taranvohra@outlook.com>

--- packages/gitbook-v2/src/lib/images/getImageResizingContextId.ts
+++ packages/gitbook-v2/src/lib/images/getImageResizingContextId.ts
@@ -5,7 +5,7 @@
 export function getImageResizingContextId(url: URL): string {
     if (url.host === 'proxy.gitbook.site' || url.host === 'proxy.gitbook-staging.site') {
         // For proxy requests, we extract the site ID from the pathname
-        // e.g. https://proxy.gitbook.com/site/siteId/...
+        // e.g. https://proxy.gitbook.site/site/siteId/...
         const pathname = url.pathname.slice(1).split('/');
         return pathname.slice(0, 2).join('/');
     }
