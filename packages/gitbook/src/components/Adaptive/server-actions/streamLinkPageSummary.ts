c93d60a20bbceef1ceea0204089807877860bd28
Zeno Kapitein <zeno@gitbook.io>
2025-04-04 10:04:55 +0200

    Start using visitedPages

--- packages/gitbook/src/components/Adaptive/server-actions/streamLinkPageSummary.ts
+++ packages/gitbook/src/components/Adaptive/server-actions/streamLinkPageSummary.ts
@@ -1,4 +1,5 @@
 'use server';
+import { filterOutNullable } from '@/lib/typescript';
 import { getV1BaseContext } from '@/lib/v1';
 import { isV2 } from '@/lib/v2';
 import { AIMessageRole } from '@gitbook/api';
@@ -17,6 +18,7 @@ export async function* streamLinkPageSummary({
     targetPageId,
     linkPreview,
     linkTitle,
+    visitedPages,
 }: {
     currentSpaceId: string;
     currentPageId: string;
@@ -29,7 +31,7 @@ export async function* streamLinkPageSummary({
     const baseContext = isV2() ? await getServerActionBaseContext() : await getV1BaseContext();
     const siteURLData = await getSiteURLDataFromMiddleware();
 
-    const [{ stream }] = await Promise.all([
+    const [{ stream, response }] = await Promise.all([
         streamGenerateObject(
             baseContext,
             {
@@ -44,57 +46,75 @@ export async function* streamLinkPageSummary({
                 messages: [
                     {
                         role: AIMessageRole.Developer,
-                        content: `# Role
+                        content: `# 1. Role
 You are a documentation navigator. Your job is to help the user read documentation more efficiently. Your aim is to prevent the user from having to read the target page by giving them all the information they need to know.
 
-# Task
+# 2. Task
 Using both the current page context and the target page content, produce a page highlight that:
 - Highlights the key facts from the target page.
 - Relates strongly to the topic the user is currently reading about.
 - Is very succinct and direct, using only one or two short sentences (each sentence using no more than one comma).
-- Remains strictly factual, without referring to “the page”.
+- Remains strictly factual, without referring to "the page".
 
-# Instructions
-1.	Identify the key paragraph surrounding the link's text (e.g., “change request”) from the current page.
+# 3. Instructions
+1.	Identify the key paragraph surrounding the link's text (e.g., "change request") from the current page.
 2.	Extract and combine relevant information from the target page to address the link's context.
 3.	Combine in one or two short sentences that are direct and brief.
 
-# Examples
+# 4. Examples
 ## Example 1
-- Link context: “This feature is only available on the Ultimate plan.”
-- Link preview: “Pricing: Learn about our different pricing tiers.”
-- Response: “The Ultimate plan costs $25 per month. A Pro plan is available too.”
+- Link context: "This feature is only available on the Ultimate plan."
+- Link preview: "Pricing: Learn about our different pricing tiers."
+- Response: "The Ultimate plan costs $25 per month. A Pro plan is available too."
 
 ## Example 2
-- Link context: “You can use keyboard shortcuts to get to the Search menu faster.”
-- Link preview: “Keyboard shortcuts: A quick reference guide to all the keyboard shortcuts available.”
-- Response: “To open the Search menu, use the keyboard shortcut ⌘K or Ctrl+K.”
+- Link context: "You can use keyboard shortcuts to get to the Search menu faster."
+- Link preview: "Keyboard shortcuts: A quick reference guide to all the keyboard shortcuts available."
+- Response: "To open the Search menu, use the keyboard shortcut ⌘K or Ctrl+K."
 
 ## Example 3
-- Link context: “This feature can only be enabled by an admin.”
-- Link preview: “Roles: An overview of the different roles on the platform.”
-- Response: “The admin role is reserved for the creator of the organisation.”`,
+- Link context: "This feature can only be enabled by an admin."
+- Link preview: "Roles: An overview of the different roles on the platform."
+- Response: "The admin role is reserved for the creator of the organisation."`,
                     },
                     {
                         role: AIMessageRole.Developer,
-                        content: `# Context
-## Current page
+                        content: `# 5. Current page
 The content of the current page is:`,
                         attachments: [
                             {
-                                type: 'page',
+                                type: 'page' as const,
                                 spaceId: currentSpaceId,
                                 pageId: currentPageId,
                             },
                         ],
                     },
+                    ...(visitedPages
+                        ? [
+                              {
+                                  role: AIMessageRole.Developer,
+                                  content: '# 6. Previous pages',
+                              },
+                              ...visitedPages.map(({ spaceId, pageId }) => ({
+                                  role: AIMessageRole.Developer,
+                                  content: `## Page ${pageId}`,
+                                  attachments: [
+                                      {
+                                          type: 'page' as const,
+                                          spaceId,
+                                          pageId,
+                                      },
+                                  ],
+                              })),
+                          ]
+                        : []),
                     {
                         role: AIMessageRole.Developer,
-                        content: `## Target page
+                        content: `# 7. Target page
 The content of the target page is:`,
                         attachments: [
                             {
-                                type: 'page',
+                                type: 'page' as const,
                                 spaceId: targetSpaceId,
                                 pageId: targetPageId,
                             },
@@ -102,7 +122,7 @@ The content of the target page is:`,
                     },
                     {
                         role: AIMessageRole.Developer,
-                        content: `## Link preview
+                        content: `# 8. Link preview
 The content of the link preview is:
 > ${linkPreview}`,
                     },
@@ -110,11 +130,13 @@ The content of the link preview is:
                         role: AIMessageRole.User,
                         content: `I'm considering reading the link titled "${linkTitle}" to page ID ${targetPageId}. Give the most relevant information from this page. Relate it to my current page and in particular the paragraph I'm currently reading. Be very concise.`,
                     },
-                ],
+                ].filter(filterOutNullable),
             }
         ),
         fetchServerActionSiteContext(baseContext),
     ]);
+
+    console.log((await response).responseId);
 
     for await (const value of stream) {
         const highlight = value.highlight;
