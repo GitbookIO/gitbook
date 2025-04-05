02339d02ed9180e299f4d0545c784c77fedb0d66
Samy Pessé <samypesse@gmail.com>
2025-04-06 00:19:56 +0200

    Test without secret

--- packages/gitbook-v2/src/lib/routes.ts
+++ packages/gitbook-v2/src/lib/routes.ts
@@ -1,6 +1,4 @@
-import crypto from 'node:crypto';
 import { NextResponse } from 'next/server';
-import { GITBOOK_SECRET } from './env';
 
 /**
  * Verify the signature of the request and call the function with the body.
@@ -13,23 +11,23 @@ export async function withVerifySignature<T>(
         const rawBody = await request.text();
         const body = JSON.parse(rawBody) as T;
 
-        if (GITBOOK_SECRET) {
-            // Retrieve the signature header from the request
-            const incomingSignature = request.headers.get('x-gitbook-signature');
-            if (!incomingSignature) {
-                return NextResponse.json({ error: 'Missing signature header' }, { status: 400 });
-            }
+        // if (GITBOOK_SECRET) {
+        //     // Retrieve the signature header from the request
+        //     const incomingSignature = request.headers.get('x-gitbook-signature');
+        //     if (!incomingSignature) {
+        //         return NextResponse.json({ error: 'Missing signature header' }, { status: 400 });
+        //     }
 
-            const computedSignature = crypto
-                .createHmac('sha256', GITBOOK_SECRET)
-                .update(rawBody)
-                .digest('hex');
+        //     const computedSignature = crypto
+        //         .createHmac('sha256', GITBOOK_SECRET)
+        //         .update(rawBody)
+        //         .digest('hex');
 
-            // Compare incoming signature to computed signature
-            if (incomingSignature !== computedSignature) {
-                return NextResponse.json({ error: 'Invalid signature' }, { status: 401 });
-            }
-        }
+        //     // Compare incoming signature to computed signature
+        //     if (incomingSignature !== computedSignature) {
+        //         return NextResponse.json({ error: 'Invalid signature' }, { status: 401 });
+        //     }
+        // }
 
         return await fn(body);
     } catch (_error) {
