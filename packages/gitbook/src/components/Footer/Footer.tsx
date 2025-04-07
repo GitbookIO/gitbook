80ada6b85ccdc60216eb6e9e6acbc48be8d37d12
Zeno Kapitein <zeno@gitbook.io>
2025-04-07 12:44:56 +0200

    Initial tweaks

--- packages/gitbook/src/components/Footer/Footer.tsx
+++ packages/gitbook/src/components/Footer/Footer.tsx
@@ -10,10 +10,14 @@ import { CONTAINER_STYLE } from '../layout';
 import { FooterLinksGroup } from './FooterLinksGroup';
 
 const FOOTER_COLUMNS = 4;
+const MAX_COLUMNS_FOR_CONDENSED_LAYOUT = 3; // Prevent unnecessary whitespace when there's not a lot of footer content
 
 export function Footer(props: { context: GitBookSiteContext }) {
     const { context } = props;
     const { customization } = context;
+
+    customization.footer.logo = undefined;
+    // customization.footer.groups = [...customization.footer.groups, ...customization.footer.groups];
 
     return (
         <footer
@@ -30,10 +34,10 @@ export function Footer(props: { context: GitBookSiteContext }) {
         >
             <div className="scroll-nojump">
                 <div className={tcls(CONTAINER_STYLE, 'px-4', 'mx-auto', 'flex', 'gap-12')}>
-                    <div className="hidden page-no-toc:hidden basis-72 lg:block" />
                     <div
                         className={tcls(
                             'py-8',
+                            'lg:py-12',
                             'gap-12',
                             'flex',
                             'flex-wrap',
@@ -42,61 +46,77 @@ export function Footer(props: { context: GitBookSiteContext }) {
                             'flex-1',
                             'max-w-3xl',
                             'page-full-width:max-w-none',
+                            'lg:max-w-none',
                             'mx-auto'
                         )}
                     >
                         {/* Footer Logo */}
-                        {customization.footer.logo ? (
-                            <Image
-                                alt="Logo"
-                                resize={context.imageResizer}
-                                sources={{
-                                    light: {
-                                        src: customization.footer.logo.light,
-                                    },
-                                    dark: customization.footer.logo.dark
-                                        ? {
-                                              src: customization.footer.logo.dark,
-                                          }
-                                        : null,
-                                }}
-                                priority="lazy"
-                                style={[
-                                    'w-auto',
-                                    'max-w-40',
-                                    'lg:max-w-64',
-                                    'max-h-12',
-                                    'object-contain',
-                                    'object-left',
-                                    'rounded',
-                                    'straight-corners:rounded-sm',
-                                    'order-1',
-                                ]}
-                                sizes={[
-                                    {
-                                        width: 320,
-                                    },
-                                ]}
-                            />
-                        ) : null}
+                        <div
+                            className={tcls(
+                                'order-first basis-72',
+                                customization.footer.logo ? 'flex' : 'hidden',
+                                customization.footer.logo &&
+                                    customization.footer.groups.length >
+                                        MAX_COLUMNS_FOR_CONDENSED_LAYOUT
+                                    ? 'xl:flex'
+                                    : 'lg:flex'
+                            )}
+                        >
+                            {customization.footer.logo ? (
+                                <Image
+                                    alt="Logo"
+                                    resize={context.imageResizer}
+                                    sources={{
+                                        light: {
+                                            src: customization.footer.logo.light,
+                                        },
+                                        dark: customization.footer.logo.dark
+                                            ? {
+                                                  src: customization.footer.logo.dark,
+                                              }
+                                            : null,
+                                    }}
+                                    priority="lazy"
+                                    style={[
+                                        'w-auto',
+                                        'max-w-40',
+                                        'lg:max-w-64',
+                                        'max-h-10',
+                                        'lg:max-h-12',
+                                        'object-contain',
+                                        'object-left',
+                                        'rounded',
+                                        'straight-corners:rounded-sm',
+                                    ]}
+                                    sizes={[
+                                        {
+                                            width: 320,
+                                        },
+                                    ]}
+                                />
+                            ) : null}
+                        </div>
 
                         {/* Mode Switcher */}
-                        {customization.themes.toggeable ? (
-                            <div className="order-2 ml-auto flex items-center justify-end xl:hidden">
+                        <div className="order-3 ml-auto flex items-center justify-end empty:max-lg:hidden xl:hidden">
+                            {customization.themes.toggeable ? (
                                 <React.Suspense fallback={null}>
                                     <ThemeToggler />
                                 </React.Suspense>
-                            </div>
-                        ) : null}
+                            ) : null}
+                        </div>
 
                         {/* Navigation Groups (split into equal columns) */}
                         {customization.footer.groups?.length > 0 ? (
                             <div
                                 className={tcls(
-                                    'order-3 mx-auto flex w-full grow flex-col gap-10 sm:flex-row sm:gap-6',
+                                    'order-3 mx-auto flex w-full max-w-3xl grow flex-col gap-10 sm:flex-row sm:gap-6 xl:w-auto',
+                                    customization.footer.groups.length <=
+                                        MAX_COLUMNS_FOR_CONDENSED_LAYOUT && 'lg:order-2 lg:w-auto',
                                     !customization.footer.logo &&
-                                        customization.footer.groups.length < 2 &&
-                                        'self-center sm:order-1 sm:w-auto sm:max-w-3xl sm:flex-1 sm:items-start sm:text-start'
+                                        customization.footer.groups.length <=
+                                            MAX_COLUMNS_FOR_CONDENSED_LAYOUT &&
+                                        'self-center sm:order-2 sm:w-auto sm:max-w-3xl sm:flex-1 sm:items-start sm:text-start'
                                 )}
                             >
                                 {partition(customization.footer.groups, FOOTER_COLUMNS).map(
@@ -118,6 +138,8 @@ export function Footer(props: { context: GitBookSiteContext }) {
                             </div>
                         ) : null}
 
+                        <div className="order-3 hidden xl:block xl:basis-56" />
+
                         {/* Legal */}
                         <div
                             className={tcls(
@@ -131,7 +153,6 @@ export function Footer(props: { context: GitBookSiteContext }) {
                             ) : null}
                         </div>
                     </div>
-                    <div className="hidden page-no-toc:hidden lg:block xl:basis-56" />
                 </div>
             </div>
         </footer>
