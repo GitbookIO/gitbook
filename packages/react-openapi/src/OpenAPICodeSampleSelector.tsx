cbd768a0955d94a8fa027c69ea8148c61498d840
Nolann B. <100787331+nolannbiron@users.noreply.github.com>
2025-04-08 09:35:22 +0200

    Improve OpenAPI codesample (#3090)

---
+++ packages/react-openapi/src/OpenAPICodeSampleSelector.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { useCallback } from 'react';
+import type { Key } from 'react-aria';
+import { useStore } from 'zustand';
+import { OpenAPIPath } from './OpenAPIPath';
+import { OpenAPISelect, OpenAPISelectItem } from './OpenAPISelect';
+import { StaticSection } from './StaticSection';
+import { getOrCreateStoreByKey } from './getOrCreateStoreByKey';
+import type { OpenAPIOperationData } from './types';
+
+function useCodeSampleState(initialKey: Key = 'default') {
+    const store = useStore(getOrCreateStoreByKey('codesample', initialKey));
+    return {
+        key: store.key,
+        setKey: useCallback((key: Key) => store.setKey(key), [store.setKey]),
+    };
+}
+
+type CodeSampleItem = OpenAPISelectItem & {
+    body: React.ReactNode;
+    footer?: React.ReactNode;
+};
+
+export function OpenAPICodeSampleHeader(props: {
+    items: CodeSampleItem[];
+    data: OpenAPIOperationData;
+}) {
+    const { data, items } = props;
+
+    const state = useCodeSampleState(items[0]?.key ?? '');
+    const selected = items.find((item) => item.key === state.key) || items[0];
+
+    return (
+        <>
+            <OpenAPIPath canCopy={false} withServer={false} data={data} />
+            {items.length > 1 ? (
+                <OpenAPISelect
+                    selectedKey={selected?.key}
+                    onSelectionChange={(key) => {
+                        state.setKey(key);
+                    }}
+                    items={items}
+                    placement="bottom end"
+                >
+                    {items.map((item) => (
+                        <OpenAPISelectItem key={item.key} id={item.key} value={item}>
+                            {item.label}
+                        </OpenAPISelectItem>
+                    ))}
+                </OpenAPISelect>
+            ) : items[0] ? (
+                <span className="openapi-codesample-label">{items[0].label}</span>
+            ) : null}
+        </>
+    );
+}
+
+export function OpenAPICodeSampleBody(props: {
+    items: CodeSampleItem[];
+    data: OpenAPIOperationData;
+}) {
+    const { items, data } = props;
+    if (!items[0]) {
+        throw new Error('No items provided');
+    }
+
+    const state = useCodeSampleState(items[0]?.key);
+
+    const selected = items.find((item) => item.key === state.key) || items[0];
+
+    if (!selected) {
+        return null;
+    }
+
+    return (
+        <StaticSection
+            header={<OpenAPICodeSampleHeader data={data} items={items} />}
+            className="openapi-codesample"
+        >
+            <div id={selected.key as string} className="openapi-codesample-panel">
+                {selected.body ? selected.body : null}
+                {selected.footer ? selected.footer : null}
+            </div>
+        </StaticSection>
+    );
+}
---
+++ packages/react-openapi/src/OpenAPISelect.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import clsx from 'clsx';
+import {
+    Button,
+    type Key,
+    ListBox,
+    ListBoxItem,
+    type ListBoxItemProps,
+    Popover,
+    type PopoverProps,
+    Select,
+    type SelectProps,
+    SelectValue,
+} from 'react-aria-components';
+
+export type OpenAPISelectItem = {
+    key: Key;
+    label: string;
+};
+
+interface OpenAPISelectProps<T extends OpenAPISelectItem> extends Omit<SelectProps<T>, 'children'> {
+    items: T[];
+    children: React.ReactNode | ((item: T) => React.ReactNode);
+    selectedKey?: Key;
+    onChange?: (key: string | number) => void;
+    placement?: PopoverProps['placement'];
+}
+
+export function OpenAPISelect<T extends OpenAPISelectItem>(props: OpenAPISelectProps<T>) {
+    const { items, children, className, placement } = props;
+
+    return (
+        <Select {...props} className={clsx('openapi-select', className)}>
+            <Button>
+                <SelectValue />
+                <span aria-hidden="true">
+                    <svg
+                        className="gb-icon"
+                        style={{
+                            maskImage:
+                                "url('https://ka-p.fontawesome.com/releases/v6.6.0/svgs/regular/chevron-down.svg?v=2&token=a463935e93')",
+                            maskRepeat: 'no-repeat',
+                            maskPosition: 'center center',
+                        }}
+                    />
+                </span>
+            </Button>
+            <Popover placement={placement} className="openapi-select-popover">
+                <ListBox className="openapi-select-listbox" items={items}>
+                    {children}
+                </ListBox>
+            </Popover>
+        </Select>
+    );
+}
+
+export function OpenAPISelectItem(props: ListBoxItemProps) {
+    return (
+        <ListBoxItem
+            {...props}
+            className={({ isFocused, isSelected }) =>
+                clsx('openapi-select-item', {
+                    'openapi-select-item-focused': isFocused,
+                    'openapi-select-item-selected': isSelected,
+                })
+            }
+        />
+    );
+}
