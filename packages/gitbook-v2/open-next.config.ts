eb8f452ab55636060f531989c0bc5ef1995be9a8
Samy Pessé <samypesse@gmail.com>
2025-04-08 12:11:27 +0200

    Use R2 incremental cache

--- packages/gitbook-v2/open-next.config.ts
+++ packages/gitbook-v2/open-next.config.ts
@@ -1,10 +1,11 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-import kvIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
+import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache';
 import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
 import doShardedTagCache from '@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache';
 
 export default defineCloudflareConfig({
-    incrementalCache: kvIncrementalCache,
+    incrementalCache: withRegionalCache(r2IncrementalCache, { mode: 'short-lived' }),
     tagCache: doShardedTagCache({
         baseShardSize: 12,
         regionalCache: true,
