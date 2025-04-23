32edd37f4d7494120f1a7eced782faa312c962b3
Steven Hall <steven@gitbook.io>
2025-04-23 16:25:50 +0100

    Revert "Increase logging around caching behaviour causing page crashes. (#3182)"
    This reverts commit 97b7c79bc6a69ca6739b6e593e6f3b5b2f0c3c82.

--- packages/gitbook-v2/src/lib/context.ts
+++ packages/gitbook-v2/src/lib/context.ts
@@ -220,8 +220,6 @@ export async function fetchSiteContextByIds(
 ): Promise<GitBookSiteContext> {
     const { dataFetcher } = baseContext;
 
-    const DEBUG = ids.site === 'site_cu2ih';
-
     const [{ site: orgSite, structure: siteStructure, customizations, scripts }, spaceContext] =
         await Promise.all([
             throwIfDataError(
@@ -233,22 +231,6 @@ export async function fetchSiteContextByIds(
             ),
             fetchSpaceContextByIds(baseContext, ids),
         ]);
-
-    DEBUG && console.log('ids', JSON.stringify(ids));
-    DEBUG &&
-        console.log(
-            'siteStructure',
-            siteStructure
-                ? JSON.stringify({
-                      type: siteStructure.type,
-                      structure: siteStructure.structure.map((s) => {
-                          // @ts-ignore
-                          const { siteSpaces, urls, ...rest } = s;
-                          return rest;
-                      }),
-                  })
-                : 'null'
-        );
 
     // override the title with the customization title
     // TODO: remove this hack once we have a proper way to handle site customizations
@@ -425,7 +407,7 @@ export function checkIsRootSiteContext(context: GitBookSiteContext): boolean {
 function parseSiteSectionsAndGroups(structure: SiteStructure, siteSectionId: string) {
     const sectionsAndGroups = getSiteStructureSections(structure, { ignoreGroups: false });
     const section = parseCurrentSection(structure, siteSectionId);
-    assert(section, `couldn't find section "${siteSectionId}" in site structure`);
+    assert(section, 'A section must be defined when there are multiple sections');
     return { list: sectionsAndGroups, current: section } satisfies SiteSections;
 }
 
