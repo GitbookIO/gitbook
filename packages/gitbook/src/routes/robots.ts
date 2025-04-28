81b4a4db535de78a70a944878b039ac0b4d4fb38
Johan Preynat <johan.preynat@gmail.com>
2025-04-28 12:07:34 +0200

    Allow /~gitbook/image and /~gitbook/icon despite querystring parameters for SEO in robots.txt (#3191)

--- packages/gitbook/src/routes/robots.ts
+++ packages/gitbook/src/routes/robots.ts
@@ -8,20 +8,23 @@ export async function serveRobotsTxt(context: GitBookSiteContext) {
     const { linker } = context;
 
     const isRoot = checkIsRootSiteContext(context);
-    const lines = [
-        'User-agent: *',
-        // Disallow dynamic routes / search queries
-        'Disallow: /*?',
-        ...((await isSiteIndexable(context))
-            ? [
-                  'Allow: /',
-                  `Sitemap: ${linker.toAbsoluteURL(linker.toPathInSpace(isRoot ? '/sitemap.xml' : '/sitemap-pages.xml'))}`,
-              ]
-            : ['Disallow: /']),
-    ];
-    const content = lines.join('\n');
+    const isIndexable = await isSiteIndexable(context);
 
-    return new Response(content, {
+    const lines = isIndexable
+        ? [
+              'User-agent: *',
+              // Allow image resizing and icon generation routes for favicons and search results
+              'Allow: /~gitbook/image?*',
+              'Allow: /~gitbook/icon?*',
+              // Disallow other dynamic routes / search queries
+              'Disallow: /*?',
+              'Allow: /',
+              `Sitemap: ${linker.toAbsoluteURL(linker.toPathInSpace(isRoot ? '/sitemap.xml' : '/sitemap-pages.xml'))}`,
+          ]
+        : ['User-agent: *', 'Disallow: /'];
+
+    const robotsTxt = lines.join('\n');
+    return new Response(robotsTxt, {
         headers: {
             'Content-Type': 'text/plain',
         },
