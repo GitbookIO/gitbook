00ed430cea6438179168ad61e905ecb51df513df
Samy Pessé <samypesse@gmail.com>
2025-05-06 09:51:50 +0200

    Proper fix

--- packages/gitbook/src/components/DocumentView/ReusableContent.tsx
+++ packages/gitbook/src/components/DocumentView/ReusableContent.tsx
@@ -2,7 +2,7 @@ import type { DocumentBlockReusableContent } from '@gitbook/api';
 
 import { resolveContentRef } from '@/lib/references';
 
-import { type GitBookSpaceContext, fetchSpaceContextByIds } from '@v2/lib/context';
+import type { GitBookSpaceContext } from '@v2/lib/context';
 import { getDataOrNull } from '@v2/lib/data';
 import type { BlockProps } from './Block';
 import { UnwrappedBlocks } from './Blocks';
@@ -46,15 +46,21 @@ export async function ReusableContent(props: BlockProps<DocumentBlockReusableCon
     // Create a new context for reusable content block, including
     // the data fetcher with the token from the block meta and the correct
     // space and revision pointers.
-    const reusableContentContext: GitBookSpaceContext = await fetchSpaceContextByIds(
-        { ...context.contentContext, dataFetcher },
-        {
-            space: resolved.reusableContent.space.id,
-            revision: resolved.reusableContent.revision,
-            changeRequest: undefined,
-            shareKey: undefined,
-        }
-    );
+    const reusableContentContext: GitBookSpaceContext =
+        context.contentContext.space.id === resolved.reusableContent.space.id
+            ? context.contentContext
+            : {
+                  ...context.contentContext,
+                  dataFetcher,
+                  space: resolved.reusableContent.space,
+                  revisionId: resolved.reusableContent.revision,
+                  // When the reusable content is in a different space, we don't resolve relative links to pages
+                  // as this space might not be part of the current site.
+                  // In the future, we might expand the logic to look up the space from the list of all spaces in the site
+                  // and adapt the relative links to point to the correct variant.
+                  pages: [],
+                  shareKey: undefined,
+              };
 
     return (
         <UnwrappedBlocks
