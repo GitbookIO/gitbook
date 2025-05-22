411c8ed424f2c806b6c1149eac465383800d6a46
Brett Jephson <brett@gitbook.io>
2025-05-22 14:30:38 +0100

    chore: format

--- packages/gitbook/src/components/DocumentView/Columns/Columns.tsx
+++ packages/gitbook/src/components/DocumentView/Columns/Columns.tsx
@@ -1,29 +1,30 @@
-import type { DocumentBlockColumns } from '@gitbook/api';
-import { BlockProps } from "../Block";
-import { Blocks } from '../Blocks';
 import { tcls } from '@/lib/tailwind';
+import type { DocumentBlockColumns } from '@gitbook/api';
+import type { BlockProps } from '../Block';
+import { Blocks } from '../Blocks';
 
 export function Columns(props: BlockProps<DocumentBlockColumns>) {
-     const { block, style, ancestorBlocks, document, context } = props;
+    const { block, style, ancestorBlocks, document, context } = props;
 
-    return <div className={tcls("flex gap-4", style)}>
-        {block.nodes.map((columnBlock) => (<Column>
-            <Blocks 
-                key={columnBlock.key} 
-                nodes={columnBlock.nodes} 
-                document={document}
-                ancestorBlocks={[...ancestorBlocks, block, columnBlock]}
-                context={context}
-                blockStyle='flip-heading-hash'
-                style='w-full space-y-4' />
-        </Column>))}
-    </div>;
-}
-
-export function Column(props: { children?: React.ReactNode; }) {
     return (
-        <div className="flex-column flex-1">
-            {props.children}
+        <div className={tcls('flex gap-4', style)}>
+            {block.nodes.map((columnBlock) => (
+                <Column>
+                    <Blocks
+                        key={columnBlock.key}
+                        nodes={columnBlock.nodes}
+                        document={document}
+                        ancestorBlocks={[...ancestorBlocks, block, columnBlock]}
+                        context={context}
+                        blockStyle="flip-heading-hash"
+                        style="w-full space-y-4"
+                    />
+                </Column>
+            ))}
         </div>
     );
+}
+
+export function Column(props: { children?: React.ReactNode }) {
+    return <div className="flex-1 flex-column">{props.children}</div>;
 }
