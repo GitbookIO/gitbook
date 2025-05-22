4c3cc1f24c3989d20420324080ac9bf72b9adc48
Nicolas Dorseuil <nicolas@gitbook.io>
2025-05-22 19:03:38 +0200

    fix script

--- packages/gitbook-v2/openNext/customWorkers/script/updateWrangler.js
+++ packages/gitbook-v2/openNext/customWorkers/script/updateWrangler.js
@@ -10,8 +10,8 @@ const args = process.argv.slice(2);
 // The versionId is in the format xxx-xxx-xxx-xxx, we need the first part to reconstruct the preview URL
 const versionId = args[0];
 
-// The preview URL is in the format https://<versionId>-gitbook-open-v2-preview.gitbook.workers.dev
-const previewUrl = `https://${versionId.split('-')[0]}-gitbook-open-v2-preview.gitbook.workers.dev`;
+// The preview URL is in the format https://<versionId>-gitbook-open-v2-server-preview.gitbook.workers.dev
+const previewUrl = `https://${versionId.split('-')[0]}-gitbook-open-v2-server-preview.gitbook.workers.dev`;
 
 const updatedFile = file.replace(/"PREVIEW_URL": "TO_REPLACE"/, `"PREVIEW_URL": "${previewUrl}"`);
 
