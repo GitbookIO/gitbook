d74c78af6fb041162667a6de3fe5a8ac098bb9be
Steven Hall <steven@gitbook.io>
2025-05-24 11:19:36 +0100

    Add revalidatePaths

--- packages/gitbook-v2/src/app/~gitbook/revalidate/route.ts
+++ packages/gitbook-v2/src/app/~gitbook/revalidate/route.ts
@@ -1,15 +1,16 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
 import { withVerifySignature } from '@v2/lib/routes';
-import { revalidateTag } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 
 interface JsonBody {
     tags: string[];
+    paths?: string[];
 }
 
 /**
  * Revalidate cached data based on tags.
- * The body should be a JSON with { tags: string[] }
+ * The body should be a JSON with { tags: string[], paths?: string[] }
  */
 export async function POST(req: NextRequest) {
     return withVerifySignature<JsonBody>(req, async (body) => {
@@ -22,10 +23,25 @@ export async function POST(req: NextRequest) {
             );
         }
 
+        if (body.paths && !Array.isArray(body.paths)) {
+            return NextResponse.json(
+                {
+                    error: 'paths must be an array',
+                },
+                { status: 400 }
+            );
+        }
+
         body.tags.forEach((tag) => {
             // biome-ignore lint/suspicious/noConsole: we want to log here
             console.log(`Revalidating tag: ${tag}`);
             revalidateTag(tag);
+        });
+
+        body.paths?.forEach((path) => {
+            // biome-ignore lint/suspicious/noConsole: we want to log here
+            console.log(`Revalidating path: ${path}`);
+            revalidatePath(path);
         });
 
         return NextResponse.json({
