042011ac1a8a91cdbaf8701610b0228fb20f0c21
Nicolas Dorseuil <nicolas@gitbook.io>
2025-05-27 13:41:31 +0200

    temp disable tag and queue in the server

--- packages/gitbook-v2/openNext/queue/server.ts
+++ packages/gitbook-v2/openNext/queue/server.ts
@@ -1,14 +1,17 @@
 import type { Queue } from '@opennextjs/aws/types/overrides.js';
-import { getCloudflareContext } from '@opennextjs/cloudflare';
+// import { getCloudflareContext } from '@opennextjs/cloudflare';
 
+// biome-ignore lint/correctness/noUnusedVariables: <explanation>
 interface Env {
     MIDDLEWARE_REFERENCE?: Pick<Queue, 'send'>;
 }
 
 export default {
     name: 'GitbookISRQueue',
+    // biome-ignore lint/correctness/noUnusedVariables: <explanation>
     send: async (msg) => {
-        const { env, ctx } = getCloudflareContext();
-        ctx.waitUntil((env as Env).MIDDLEWARE_REFERENCE?.send(msg) ?? Promise.resolve());
+        //TODO: Re add this
+        // const { env, ctx } = getCloudflareContext();
+        // ctx.waitUntil((env as Env).MIDDLEWARE_REFERENCE?.send(msg) ?? Promise.resolve());
     },
 } satisfies Queue;
--- packages/gitbook-v2/openNext/tagCache/server.ts
+++ packages/gitbook-v2/openNext/tagCache/server.ts
@@ -1,40 +1,43 @@
 import type { NextModeTagCache } from '@opennextjs/aws/types/overrides.js';
-import { getCloudflareContext } from '@opennextjs/cloudflare';
 
-interface Env {
-    MIDDLEWARE_REFERENCE?: Pick<
-        NextModeTagCache,
-        'getLastRevalidated' | 'hasBeenRevalidated' | 'writeTags' | 'getPathsByTags'
-    >;
-}
+// interface Env {
+//     MIDDLEWARE_REFERENCE?: Pick<
+//         NextModeTagCache,
+//         'getLastRevalidated' | 'hasBeenRevalidated' | 'writeTags' | 'getPathsByTags'
+//     >;
+// }
 
+//TODO: Reenable all of this.
 export default {
     name: 'GitbookTagCache',
     mode: 'nextMode',
-    getLastRevalidated: async (tags: string[]) => {
-        try {
-            const { env } = getCloudflareContext();
-            return (env as Env).MIDDLEWARE_REFERENCE?.getLastRevalidated(tags) ?? 0;
-        } catch (error) {
-            console.error('Server - Error getting last revalidated tags:', error);
-            return 0; // Fallback to 0 if there's an error
-        }
+    getLastRevalidated: async (_tags: string[]) => {
+        return 0;
+        // try {
+        //     const { env } = getCloudflareContext();
+        //     return (env as Env).MIDDLEWARE_REFERENCE?.getLastRevalidated(tags) ?? 0;
+        // } catch (error) {
+        //     console.error('Server - Error getting last revalidated tags:', error);
+        //     return 0; // Fallback to 0 if there's an error
+        // }
     },
-    hasBeenRevalidated: async (tags: string[]) => {
-        try {
-            const { env } = getCloudflareContext();
-            return (env as Env).MIDDLEWARE_REFERENCE?.hasBeenRevalidated(tags) ?? false;
-        } catch (error) {
-            console.error('Server - Error checking if tags have been revalidated:', error);
-            return false; // Fallback to false if there's an error
-        }
+    hasBeenRevalidated: async (_tags: string[]) => {
+        // try {
+        //     const { env } = getCloudflareContext();
+        //     return (env as Env).MIDDLEWARE_REFERENCE?.hasBeenRevalidated(tags) ?? false;
+        // } catch (error) {
+        //     console.error('Server - Error checking if tags have been revalidated:', error);
+        //     return false; // Fallback to false if there's an error
+        // }
+        return false;
     },
-    writeTags: async (tags: string[]) => {
-        try {
-            const { env } = getCloudflareContext();
-            (env as Env).MIDDLEWARE_REFERENCE?.writeTags(tags);
-        } catch (error) {
-            console.error('Server - Error writing tags:', error);
-        }
+    writeTags: async (_tags: string[]) => {
+        // try {
+        //     const { env } = getCloudflareContext();
+        //     (env as Env).MIDDLEWARE_REFERENCE?.writeTags(tags);
+        // } catch (error) {
+        //     console.error('Server - Error writing tags:', error);
+        // }
+        return;
     },
 } satisfies NextModeTagCache;
