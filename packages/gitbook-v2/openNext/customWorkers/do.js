e392a445202866d17857ff6162a6a58eb2123da2
Nicolas Dorseuil <nicolas@gitbook.io>
2025-05-29 18:36:57 +0200

    debug tag cache

--- packages/gitbook-v2/openNext/customWorkers/do.js
+++ packages/gitbook-v2/openNext/customWorkers/do.js
@@ -19,9 +19,65 @@ export class R2WriteBuffer extends DurableObject {
     }
 }
 
-export { DOQueueHandler } from '../../.open-next/.build/durable-objects/queue.js';
+// TODO: replace once i figured out why `revalidateTag` is not working in preview
+export class DOShardedTagCache extends DurableObject {
+    sql;
 
-export { DOShardedTagCache } from '../../.open-next/.build/durable-objects/sharded-tag-cache.js';
+    constructor(state, env) {
+        super(state, env);
+        this.sql = state.storage.sql;
+        state.blockConcurrencyWhile(async () => {
+            this.sql.exec(
+                'CREATE TABLE IF NOT EXISTS revalidations (tag TEXT PRIMARY KEY, revalidatedAt INTEGER)'
+            );
+        });
+    }
+
+    async getLastRevalidated(tags) {
+        try {
+            const result = this.sql
+                .exec(
+                    `SELECT MAX(revalidatedAt) AS time FROM revalidations WHERE tag IN (${tags.map(() => '?').join(', ')})`,
+                    ...tags
+                )
+                .toArray();
+            console.log('getLastRevalidated', tags, result);
+            if (result.length === 0) return 0;
+            // We only care about the most recent revalidation
+            return result[0]?.time ?? 0;
+        } catch (e) {
+            console.error(e);
+            // By default we don't want to crash here, so we return 0
+            return 0;
+        }
+    }
+
+    async hasBeenRevalidated(tags, lastModified) {
+        const result = this.sql
+            .exec(
+                `SELECT 1 FROM revalidations WHERE tag IN (${tags.map(() => '?').join(', ')}) AND revalidatedAt > ? LIMIT 1`,
+                ...tags,
+                lastModified ?? Date.now()
+            )
+            .toArray();
+
+        console.log('hasBeenRevalidated', tags, lastModified, result);
+        return result.length > 0;
+    }
+
+    async writeTags(tags, lastModified) {
+        console.log('writeTags', tags, lastModified);
+        tags.forEach((tag) => {
+            this.sql.exec(
+                'INSERT OR REPLACE INTO revalidations (tag, revalidatedAt) VALUES (?, ?)',
+                tag,
+                lastModified
+            );
+        });
+    }
+}
+
+export { DOQueueHandler } from '../../.open-next/.build/durable-objects/queue.js';
 
 export default {
     async fetch() {
