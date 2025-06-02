0f867e533a65532730e014810a161753e81bbf9d
conico974 <nicodorseuil@yahoo.fr>
2025-06-02 17:40:04 +0200

    bump OpenNext to latest (#3276)
    Co-authored-by: Nicolas Dorseuil <nicolas@gitbook.io>

--- packages/gitbook-v2/openNext/queue/middleware.ts
+++ packages/gitbook-v2/openNext/queue/middleware.ts
@@ -2,20 +2,13 @@ import { trace } from '@/lib/tracing';
 import type { Queue } from '@opennextjs/aws/types/overrides.js';
 import { getCloudflareContext } from '@opennextjs/cloudflare';
 import doQueue from '@opennextjs/cloudflare/overrides/queue/do-queue';
-import memoryQueue from '@opennextjs/cloudflare/overrides/queue/memory-queue';
-
-interface Env {
-    STAGE?: string;
-}
 
 export default {
     name: 'GitbookISRQueue',
     send: async (msg) => {
         return trace({ operation: 'gitbookISRQueueSend', name: msg.MessageBody.url }, async () => {
-            const { ctx, env } = getCloudflareContext();
-            const hasDurableObject =
-                (env as Env).STAGE !== 'dev' && (env as Env).STAGE !== 'preview';
-            ctx.waitUntil(hasDurableObject ? memoryQueue.send(msg) : doQueue.send(msg));
+            const { ctx } = getCloudflareContext();
+            ctx.waitUntil(doQueue.send(msg));
         });
     },
 } satisfies Queue;
