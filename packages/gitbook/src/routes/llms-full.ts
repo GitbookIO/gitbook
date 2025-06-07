e5bb89a3a7773c7a591bf249e1a6b7a83294500e
Samy Pessé <samypesse@gmail.com>
2025-06-07 23:08:33 +0200

    Stream it

--- packages/gitbook/src/routes/llms-full.ts
+++ packages/gitbook/src/routes/llms-full.ts
@@ -7,131 +7,141 @@ import type { RevisionPageDocument, SiteSection, SiteSpace } from '@gitbook/api'
 import { type GitBookSiteContext, checkIsRootSiteContext } from '@v2/lib/context';
 import { throwIfDataError } from '@v2/lib/data';
 import assertNever from 'assert-never';
-import type { Link, Paragraph, Root, RootContent } from 'mdast';
+import type { Link, Paragraph, Root } from 'mdast';
 import { fromMarkdown } from 'mdast-util-from-markdown';
 import { frontmatterFromMarkdown } from 'mdast-util-frontmatter';
 import { gfmFromMarkdown, gfmToMarkdown } from 'mdast-util-gfm';
 import { toMarkdown } from 'mdast-util-to-markdown';
 import { frontmatter } from 'micromark-extension-frontmatter';
 import { gfm } from 'micromark-extension-gfm';
+import { pMapIterable } from 'p-map';
 import { remove } from 'unist-util-remove';
 import { visit } from 'unist-util-visit';
 
+// We limit the concurrency to 100 to avoid reaching limit with concurrent requests
+// or file descriptor limits.
+const MAX_CONCURRENCY = 100;
+
 /**
  * Generate a llms-full.txt file for the site.
+ * As the result can be large, we stream it as we generate it.
  */
 export async function serveLLMsFullTxt(context: GitBookSiteContext) {
     if (!checkIsRootSiteContext(context)) {
         return new Response('llms.txt is only served from the root of the site', { status: 404 });
     }
 
-    const tree: Root = {
-        type: 'root',
-        children: await getNodesFromSiteStructure(context),
-    };
-
-    return new Response(toMarkdown(tree, { extensions: [gfmToMarkdown()] }), {
-        headers: {
-            'Content-Type': 'text/markdown; charset=utf-8',
-        },
-    });
+    return new Response(
+        new ReadableStream({
+            async pull(controller) {
+                await streamMarkdownFromSiteStructure(context, controller);
+                controller.close();
+            },
+        }),
+        {
+            headers: {
+                'Content-Type': 'text/markdown; charset=utf-8',
+            },
+        }
+    );
 }
 
 /**
- * Get MDAST nodes from site structure.
+ * Stream markdown from site structure.
  */
-async function getNodesFromSiteStructure(context: GitBookSiteContext): Promise<RootContent[]> {
+async function streamMarkdownFromSiteStructure(
+    context: GitBookSiteContext,
+    stream: ReadableStreamDefaultController<string>
+): Promise<void> {
     switch (context.structure.type) {
         case 'sections':
-            return getNodesFromSections(
+            return streamMarkdownFromSections(
                 context,
+                stream,
                 getSiteStructureSections(context.structure, { ignoreGroups: true })
             );
         case 'siteSpaces':
-            return getNodesFromSiteSpaces(context, context.structure.structure, '');
+            return streamMarkdownFromSiteSpaces(context, stream, context.structure.structure, '');
         default:
             assertNever(context.structure);
     }
 }
 
 /**
- * Get MDAST nodes from site sections.
+ * Stream markdown from site sections.
  */
-async function getNodesFromSections(
+async function streamMarkdownFromSections(
     context: GitBookSiteContext,
+    stream: ReadableStreamDefaultController<string>,
     siteSections: SiteSection[]
-): Promise<RootContent[]> {
-    const all = await Promise.all(
-        siteSections.map(async (siteSection): Promise<RootContent[]> => {
-            const siteSpaceNodes = await getNodesFromSiteSpaces(
-                context,
-                siteSection.siteSpaces,
-                siteSection.path
-            );
-            return siteSpaceNodes;
-        })
-    );
-    return all.flat();
+): Promise<void> {
+    for (const siteSection of siteSections) {
+        await streamMarkdownFromSiteSpaces(
+            context,
+            stream,
+            siteSection.siteSpaces,
+            siteSection.path
+        );
+    }
 }
 
 /**
- * Get MDAST nodes from site spaces.
+ * Stream markdown from site spaces.
  */
-async function getNodesFromSiteSpaces(
+async function streamMarkdownFromSiteSpaces(
     context: GitBookSiteContext,
+    stream: ReadableStreamDefaultController<string>,
     siteSpaces: SiteSpace[],
     basePath: string
-): Promise<RootContent[]> {
+): Promise<void> {
     const { dataFetcher } = context;
 
-    const all = await Promise.all(
-        siteSpaces.map(async (siteSpace): Promise<RootContent[]> => {
-            const siteSpaceUrl = siteSpace.urls.published;
-            if (!siteSpaceUrl) {
-                return [];
-            }
-            const rootPages = await throwIfDataError(
-                dataFetcher.getRevisionPages({
-                    spaceId: siteSpace.space.id,
-                    revisionId: siteSpace.space.revision,
-                    metadata: false,
-                })
-            );
-            const pages = getIndexablePages(rootPages)
-                // We currently limit the number of pages to 500 to avoid generating a too large markdown output
-                // and because of limits with the server on how many requests / files can be opened.
-                .slice(0, 500);
-            const nodes = (
-                await Promise.all(
-                    pages.map(async ({ page }): Promise<RootContent[]> => {
-                        if (page.type !== 'document') {
-                            return [];
-                        }
+    for (const siteSpace of siteSpaces) {
+        const siteSpaceUrl = siteSpace.urls.published;
+        if (!siteSpaceUrl) {
+            continue;
+        }
+        const rootPages = await throwIfDataError(
+            dataFetcher.getRevisionPages({
+                spaceId: siteSpace.space.id,
+                revisionId: siteSpace.space.revision,
+                metadata: false,
+            })
+        );
+        const pages = getIndexablePages(rootPages);
 
-                        return getNodesFromPage(
-                            context,
-                            siteSpace,
-                            page,
-                            joinPath(basePath, siteSpace.path)
-                        );
-                    })
-                )
-            ).flat();
-            return nodes;
-        })
-    );
-    return all.flat();
+        for await (const markdown of pMapIterable(
+            pages,
+            async ({ page }) => {
+                if (page.type !== 'document') {
+                    return '';
+                }
+
+                return getMarkdownForPage(
+                    context,
+                    siteSpace,
+                    page,
+                    joinPath(basePath, siteSpace.path)
+                );
+            },
+            {
+                concurrency: MAX_CONCURRENCY,
+            }
+        )) {
+            stream.enqueue(markdown);
+        }
+    }
 }
 
 /**
- * Get MDAST nodes from a page.
+ * Get markdown from a page.
  */
-async function getNodesFromPage(
+async function getMarkdownForPage(
     context: GitBookSiteContext,
     siteSpace: SiteSpace,
     page: RevisionPageDocument,
     basePath: string
-): Promise<RootContent[]> {
+): Promise<string> {
     const { dataFetcher } = context;
 
     const pageMarkdown = await throwIfDataError(
@@ -163,7 +173,8 @@ async function getNodesFromPage(
     // Rewrite relative links to absolute links
     transformLinks(context, tree, { currentPagePath: page.path, basePath });
 
-    return tree.children;
+    const markdown = toMarkdown(tree, { extensions: [gfmToMarkdown()] });
+    return `${markdown}\n\n`;
 }
 
 /**
