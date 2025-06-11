77574252f31ebed6ce5218ebc319647dd64923fb
Steeve Pastorelli <steeve@gitbook.com>
2025-06-11 14:10:19 +0200

    Also add launchdarkly-site.gitbook.education

--- packages/gitbook-v2/src/middleware.ts
+++ packages/gitbook-v2/src/middleware.ts
@@ -42,6 +42,7 @@ const ADAPTIVE_CONTENT_HOSTS = [
     'adaptive-docs.gitbook-staging.com',
     'enriched-content-playground.gitbook-staging.io',
     'docs.testgitbook.com',
+    'launchdarkly-site.gitbook.education',
 ];
 
 export async function middleware(request: NextRequest) {
