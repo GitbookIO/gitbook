9660b5344a10bea40d26af3b57e6649236a57fb2
Steven Hall <steven@gitbook.io>
2025-06-17 12:22:33 +0100

    Remove the AI summary

--- packages/gitbook/src/components/DocumentView/InlineLinkTooltipClient.tsx
+++ packages/gitbook/src/components/DocumentView/InlineLinkTooltipClient.tsx
@@ -6,7 +6,6 @@ import { tcls } from '@/lib/tailwind';
 import { Icon } from '@gitbook/icons';
 import * as Tooltip from '@radix-ui/react-tooltip';
 import { Fragment } from 'react';
-import { AIPageLinkSummary } from '../Adaptive';
 import { Button, StyledLink } from '../primitives';
 
 export function InlineLinkTooltipClient(props: {
@@ -104,16 +103,6 @@ export function InlineLinkTooltipClient(props: {
                                     <p className="mt-1 text-sm text-tint">{target.subText}</p>
                                 ) : null}
                             </div>
-
-                            {aiSummary ? (
-                                <div className="border-tint-subtle border-t bg-tint p-4">
-                                    <AIPageLinkSummary
-                                        targetPageId={aiSummary.pageId}
-                                        targetSpaceId={aiSummary.spaceId}
-                                        showTrademark
-                                    />
-                                </div>
-                            ) : null}
                         </div>
                         <Tooltip.Arrow
                             className={
