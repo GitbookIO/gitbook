ca6964dec02eb14388600f476dc271c56225419d
Steven Hall <steven@gitbook.io>
2025-06-17 13:10:48 +0100

    Bring tooltip changes back

--- packages/gitbook/src/components/DocumentView/InlineLinkTooltipClient.tsx
+++ packages/gitbook/src/components/DocumentView/InlineLinkTooltipClient.tsx
@@ -6,6 +6,7 @@ import { tcls } from '@/lib/tailwind';
 import { Icon } from '@gitbook/icons';
 import * as Tooltip from '@radix-ui/react-tooltip';
 import { Fragment } from 'react';
+import { AIPageLinkSummary } from '../Adaptive';
 import { Button, StyledLink } from '../primitives';
 
 export function InlineLinkTooltipClient(props: {
@@ -103,6 +104,16 @@ export function InlineLinkTooltipClient(props: {
                                     <p className="mt-1 text-sm text-tint">{target.subText}</p>
                                 ) : null}
                             </div>
+
+                            {aiSummary ? (
+                                <div className="border-tint-subtle border-t bg-tint p-4">
+                                    <AIPageLinkSummary
+                                        targetPageId={aiSummary.pageId}
+                                        targetSpaceId={aiSummary.spaceId}
+                                        showTrademark
+                                    />
+                                </div>
+                            ) : null}
                         </div>
                         <Tooltip.Arrow
                             className={
