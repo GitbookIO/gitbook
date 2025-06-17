0dfb8c7ab43cd63d1159e99c0ac4b9f9d9554323
Steven Hall <steven@gitbook.io>
2025-06-17 13:32:49 +0100

    Refactor

--- packages/gitbook/src/components/DocumentView/InlineLinkTooltip.tsx
+++ packages/gitbook/src/components/DocumentView/InlineLinkTooltip.tsx
@@ -5,8 +5,13 @@ import type React from 'react';
 import { getSpaceLanguage } from '@/intl/server';
 import { tString } from '@/intl/translate';
 import { languages } from '@/intl/translations';
+import { tcls } from '@/lib/tailwind';
 import { Icon } from '@gitbook/icons';
+import * as Tooltip from '@radix-ui/react-tooltip';
 import type { GitBookAnyContext } from '@v2/lib/context';
+import { Fragment } from 'react';
+import { AIPageLinkSummary } from '../Adaptive';
+import { Button, StyledLink } from '../primitives';
 import { InlineLinkTooltipClient } from './InlineLinkTooltipClient';
 
 export function InlineLinkTooltip(props: {
@@ -65,20 +70,94 @@ export function InlineLinkTooltip(props: {
     })();
 
     return (
-        <InlineLinkTooltipClient
-            target={{
-                href: resolved.href,
-                text: resolved.text,
-                subText: resolved.subText,
-                icon: resolved.icon,
-            }}
-            breadcrumbs={breadcrumbs}
-            isSamePage={isSamePage}
-            isExternal={isExternal}
-            language={language}
-            aiSummary={aiSummary}
-        >
-            {children}
+        <InlineLinkTooltipClient trigger={children}>
+            <Tooltip.Content className="z-40 w-screen max-w-md animate-present px-4 sm:w-auto">
+                <div className="overflow-hidden rounded-md straight-corners:rounded-none shadow-lg shadow-tint-12/4 ring-1 ring-tint-subtle dark:shadow-tint-1 ">
+                    <div className="bg-tint-base p-4">
+                        <div className="flex items-start gap-4">
+                            <div className="flex flex-col">
+                                {breadcrumbs && breadcrumbs.length > 0 ? (
+                                    <div className="mb-1 flex grow flex-wrap items-center gap-x-2 gap-y-0.5 font-semibold text-tint text-xs uppercase leading-tight tracking-wide">
+                                        {breadcrumbs.map((crumb, index) => {
+                                            const Tag = crumb.href ? StyledLink : 'div';
+
+                                            return (
+                                                <Fragment key={crumb.label}>
+                                                    {index !== 0 ? (
+                                                        <Icon
+                                                            icon="chevron-right"
+                                                            className="size-3 text-tint-subtle"
+                                                        />
+                                                    ) : null}
+                                                    <Tag
+                                                        className={tcls(
+                                                            'flex gap-1',
+                                                            crumb.href &&
+                                                                'links-default:text-tint no-underline hover:underline contrast-more:underline contrast-more:decoration-current'
+                                                        )}
+                                                        href={crumb.href ?? '#'}
+                                                    >
+                                                        {crumb.icon ? (
+                                                            <span className="mt-0.5 text-tint-subtle empty:hidden">
+                                                                {crumb.icon}
+                                                            </span>
+                                                        ) : null}
+                                                        {crumb.label}
+                                                    </Tag>
+                                                </Fragment>
+                                            );
+                                        })}
+                                    </div>
+                                ) : null}
+                                <div
+                                    className={tcls(
+                                        'flex gap-2 leading-snug',
+                                        isExternal && 'text-sm [overflow-wrap:anywhere]'
+                                    )}
+                                >
+                                    {resolved.icon ? (
+                                        <div className="mt-1 text-tint-subtle empty:hidden">
+                                            {resolved.icon}
+                                        </div>
+                                    ) : null}
+                                    <h5 className="font-semibold">{resolved.text}</h5>
+                                </div>
+                            </div>
+                            {!isSamePage && resolved.href ? (
+                                <Button
+                                    className={tcls(
+                                        '-mx-2 -my-2 ml-auto',
+                                        breadcrumbs?.length === 0 ? 'place-self-center' : null
+                                    )}
+                                    variant="blank"
+                                    href={resolved.href}
+                                    target="_blank"
+                                    label={tString(language, 'open_in_new_tab')}
+                                    size="small"
+                                    icon="arrow-up-right-from-square"
+                                    iconOnly={true}
+                                />
+                            ) : null}
+                        </div>
+                        {resolved.subText ? (
+                            <p className="mt-1 text-sm text-tint">{resolved.subText}</p>
+                        ) : null}
+                    </div>
+
+                    {aiSummary ? (
+                        <div className="border-tint-subtle border-t bg-tint p-4">
+                            <AIPageLinkSummary
+                                targetPageId={aiSummary.pageId}
+                                targetSpaceId={aiSummary.spaceId}
+                                showTrademark
+                            />
+                        </div>
+                    ) : null}
+                </div>
+                <Tooltip.Arrow
+                    className={typeof aiSummary !== 'undefined' ? 'fill-tint-3' : 'fill-tint-1'}
+                />
+            </Tooltip.Content>
         </InlineLinkTooltipClient>
     );
 }
--- packages/gitbook/src/components/DocumentView/InlineLinkTooltipClient.tsx
+++ packages/gitbook/src/components/DocumentView/InlineLinkTooltipClient.tsx
@@ -1,127 +1,17 @@
 'use client';
-
-import { tString } from '@/intl/translate';
-import type { TranslationLanguage } from '@/intl/translations';
-import { tcls } from '@/lib/tailwind';
-import { Icon } from '@gitbook/icons';
 import * as Tooltip from '@radix-ui/react-tooltip';
-import { Fragment } from 'react';
-import { AIPageLinkSummary } from '../Adaptive';
-import { Button, StyledLink } from '../primitives';
 
 export function InlineLinkTooltipClient(props: {
-    isSamePage: boolean;
-    isExternal: boolean;
-    aiSummary?: { pageId: string; spaceId: string };
-    breadcrumbs: Array<{ href?: string; label: string; icon?: React.ReactNode }>;
-    target: {
-        href: string;
-        text: string;
-        subText?: string;
-        icon?: React.ReactNode;
-    };
-    language: TranslationLanguage;
+    trigger: React.ReactNode;
     children: React.ReactNode;
 }) {
-    const { isSamePage, isExternal, aiSummary, language, target, breadcrumbs, children } = props;
+    const { trigger, children } = props;
 
     return (
         <Tooltip.Provider delayDuration={200}>
             <Tooltip.Root>
-                <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
-                <Tooltip.Portal>
-                    <Tooltip.Content className="z-40 w-screen max-w-md animate-present px-4 sm:w-auto">
-                        <div className="overflow-hidden rounded-md straight-corners:rounded-none shadow-lg shadow-tint-12/4 ring-1 ring-tint-subtle dark:shadow-tint-1 ">
-                            <div className="bg-tint-base p-4">
-                                <div className="flex items-start gap-4">
-                                    <div className="flex flex-col">
-                                        {breadcrumbs && breadcrumbs.length > 0 ? (
-                                            <div className="mb-1 flex grow flex-wrap items-center gap-x-2 gap-y-0.5 font-semibold text-tint text-xs uppercase leading-tight tracking-wide">
-                                                {breadcrumbs.map((crumb, index) => {
-                                                    const Tag = crumb.href ? StyledLink : 'div';
-
-                                                    return (
-                                                        <Fragment key={crumb.label}>
-                                                            {index !== 0 ? (
-                                                                <Icon
-                                                                    icon="chevron-right"
-                                                                    className="size-3 text-tint-subtle"
-                                                                />
-                                                            ) : null}
-                                                            <Tag
-                                                                className={tcls(
-                                                                    'flex gap-1',
-                                                                    crumb.href &&
-                                                                        'links-default:text-tint no-underline hover:underline contrast-more:underline contrast-more:decoration-current'
-                                                                )}
-                                                                href={crumb.href ?? '#'}
-                                                            >
-                                                                {crumb.icon ? (
-                                                                    <span className="mt-0.5 text-tint-subtle empty:hidden">
-                                                                        {crumb.icon}
-                                                                    </span>
-                                                                ) : null}
-                                                                {crumb.label}
-                                                            </Tag>
-                                                        </Fragment>
-                                                    );
-                                                })}
-                                            </div>
-                                        ) : null}
-                                        <div
-                                            className={tcls(
-                                                'flex gap-2 leading-snug',
-                                                isExternal && 'text-sm [overflow-wrap:anywhere]'
-                                            )}
-                                        >
-                                            {target.icon ? (
-                                                <div className="mt-1 text-tint-subtle empty:hidden">
-                                                    {target.icon}
-                                                </div>
-                                            ) : null}
-                                            <h5 className="font-semibold">{target.text}</h5>
-                                        </div>
-                                    </div>
-                                    {!isSamePage && target.href ? (
-                                        <Button
-                                            className={tcls(
-                                                '-mx-2 -my-2 ml-auto',
-                                                breadcrumbs?.length === 0
-                                                    ? 'place-self-center'
-                                                    : null
-                                            )}
-                                            variant="blank"
-                                            href={target.href}
-                                            target="_blank"
-                                            label={tString(language, 'open_in_new_tab')}
-                                            size="small"
-                                            icon="arrow-up-right-from-square"
-                                            iconOnly={true}
-                                        />
-                                    ) : null}
-                                </div>
-                                {target.subText ? (
-                                    <p className="mt-1 text-sm text-tint">{target.subText}</p>
-                                ) : null}
-                            </div>
-
-                            {aiSummary ? (
-                                <div className="border-tint-subtle border-t bg-tint p-4">
-                                    <AIPageLinkSummary
-                                        targetPageId={aiSummary.pageId}
-                                        targetSpaceId={aiSummary.spaceId}
-                                        showTrademark
-                                    />
-                                </div>
-                            ) : null}
-                        </div>
-                        <Tooltip.Arrow
-                            className={
-                                typeof aiSummary !== 'undefined' ? 'fill-tint-3' : 'fill-tint-1'
-                            }
-                        />
-                    </Tooltip.Content>
-                </Tooltip.Portal>
+                <Tooltip.Trigger asChild>{trigger}</Tooltip.Trigger>
+                <Tooltip.Portal>{children}</Tooltip.Portal>
             </Tooltip.Root>
         </Tooltip.Provider>
     );
