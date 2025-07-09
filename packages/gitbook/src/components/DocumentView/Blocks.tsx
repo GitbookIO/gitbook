11e9f108d95ab171c08f93a90939d404598108fa
Zeno Kapitein <zeno@gitbook.io>
2025-07-09 16:13:21 +0200

    Update Blocks.tsx

--- packages/gitbook/src/components/DocumentView/Blocks.tsx
+++ packages/gitbook/src/components/DocumentView/Blocks.tsx
@@ -85,7 +85,7 @@ export function UnwrappedBlocks<TBlock extends DocumentBlock>(props: UnwrappedBl
                 key={node.key || `${node.type}-${index}`}
                 block={node}
                 style={[
-                    'site-width-default:mx-auto w-full decoration-primary/6',
+                    'mx-auto page-width-wide:mx-0 w-full decoration-primary/6',
                     node.data && 'fullWidth' in node.data && node.data.fullWidth
                         ? 'max-w-screen-2xl'
                         : 'max-w-3xl',
