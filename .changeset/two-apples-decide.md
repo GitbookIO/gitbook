3fa1bfc84618c6cce084f08b8137d07a8dc44b46
Greg Bergé <berge.greg@gmail.com>
2025-08-05 09:59:12 +0200

    Less strict OpenAPI parser
    Be less strict in the parsing of OpenAPI
    
    Now return errors (so we can collect them to be displayed to user)

---
+++ .changeset/two-apples-decide.md
@@ -0,0 +1,5 @@
+---
+"@gitbook/openapi-parser": major
+---
+
+Less strict OpenAPI parser, now returns { errors, filesystem }
--- packages/openapi-parser/src/v2.test.ts
+++ packages/openapi-parser/src/v2.test.ts
@@ -5,12 +5,12 @@ const specV2 = await Bun.file(new URL('./fixtures/spec-v2.json', import.meta.url
 
 describe('#convertOpenAPIV2ToOpenAPIV3', () => {
     it('converts an OpenAPIV2 in V3', async () => {
-        const schema = await convertOpenAPIV2ToOpenAPIV3({
+        const result = await convertOpenAPIV2ToOpenAPIV3({
             value: specV2,
             rootURL: null,
         });
         // Ensure the structure returned is not recursive (not dereferenced).
-        JSON.stringify(schema);
-        expect(schema[0]?.specification.openapi).toBe('3.1.1');
+        JSON.stringify(result.filesystem);
+        expect(result.filesystem[0]?.specification.openapi).toBe('3.1.1');
     });
 });
