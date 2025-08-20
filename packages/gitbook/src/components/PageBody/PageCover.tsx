8fa053eaac9b2552a30cd64dac774e221bddf476
Zeno Kapitein <zeno@gitbook.io>
2025-08-20 17:42:27 +0200

    Fix pageaside height on pages with hero cover

--- packages/gitbook/src/components/PageBody/PageCover.tsx
+++ packages/gitbook/src/components/PageBody/PageCover.tsx
@@ -76,6 +76,8 @@ export async function PageCover(props: {
 
     return (
         <div
+            id="page-cover"
+            data-full={as === 'full' ? 'true' : 'false'}
             className={tcls(
                 'overflow-hidden',
                 // Negative margin to balance the container padding
