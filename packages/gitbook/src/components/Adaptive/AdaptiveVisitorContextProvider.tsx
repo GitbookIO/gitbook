2c3703f6b92cd5f13d326f4557acc5c05a3dbbc1
Steeve Pastorelli <steeve@gitbook.com>
2025-08-27 17:23:46 +0200

    Add an adaptive provider to provide context to site and openapi blocks

---
+++ packages/gitbook/src/components/Adaptive/AdaptiveVisitorContextProvider.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import type { GitBookSiteContext } from '@/lib/context';
+import { ExpressionRuntime, parseTemplate } from '@gitbook/expr';
+import {
+    OpenAPIOperationContextProvider,
+    OpenAPITryItPrefillContextProvider,
+} from '@gitbook/react-openapi';
+import type React from 'react';
+import { createContext, useCallback, useContext, useEffect, useRef, useState } from 'react';
+
+type VisitorClaimsData = {
+    visitor: {
+        claims: Record<string, unknown> & {
+            unsigned: Record<string, unknown>;
+        };
+    };
+};
+
+const AdaptiveVisitorContext = createContext<{ visitorClaims: VisitorClaimsData } | null>(null);
+
+/**
+ * Provide context to adapt site based on visitor claims.
+ */
+export function AdaptiveVisitorContextProvider(
+    props: React.PropsWithChildren<{
+        getVisitorClaimsUrl: string;
+        contextId: GitBookSiteContext['contextId'];
+    }>
+) {
+    const { getVisitorClaimsUrl, contextId, children } = props;
+
+    const runtime = useRef<ExpressionRuntime>(new ExpressionRuntime());
+    const [visitorClaimsData, setVisitorClaimsData] = useState<VisitorClaimsData | null>(null);
+
+    useEffect(() => {
+        // Only fetch visitor claims if contextId is defined (adaptive content site).
+        if (!contextId) {
+            setVisitorClaimsData(null);
+            return;
+        }
+
+        async function fetchVisitorData() {
+            try {
+                const res = await fetch(getVisitorClaimsUrl);
+                if (!res.ok) {
+                    setVisitorClaimsData(null);
+                    return;
+                }
+                const data = await res.json<VisitorClaimsData>();
+                setVisitorClaimsData(data);
+            } catch (err) {
+                console.error('Error fetching visitor claims data', err);
+                setVisitorClaimsData(null);
+            }
+        }
+        fetchVisitorData();
+    }, [getVisitorClaimsUrl, contextId]);
+
+    const resolveTryItPrefillExpression = useCallback(
+        (expr: string) => {
+            if (!visitorClaimsData) {
+                return undefined;
+            }
+            const parts = parseTemplate(expr);
+            if (!parts.length) {
+                return undefined;
+            }
+            return runtime.current.evaluateTemplate(expr, visitorClaimsData);
+        },
+        [visitorClaimsData]
+    );
+
+    return (
+        <AdaptiveVisitorContext.Provider
+            value={visitorClaimsData ? { visitorClaims: visitorClaimsData } : null}
+        >
+            <OpenAPIOperationContextProvider>
+                <OpenAPITryItPrefillContextProvider
+                    resolveTryItPrefillExpression={resolveTryItPrefillExpression}
+                >
+                    {children}
+                </OpenAPITryItPrefillContextProvider>
+            </OpenAPIOperationContextProvider>
+        </AdaptiveVisitorContext.Provider>
+    );
+}
+
+export function useAdaptiveVisitorContext() {
+    return useContext(AdaptiveVisitorContext);
+}
