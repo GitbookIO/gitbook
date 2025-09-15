d66d86e0474393c94c7b8419edf18376d0cd8b2e
Samy Pessé <samypesse@gmail.com>
2025-09-15 14:21:31 +0200

    Format

--- packages/gitbook/src/routes/robots.ts
+++ packages/gitbook/src/routes/robots.ts
@@ -5,24 +5,24 @@ import { isSiteIndexable } from '@/lib/seo';
  * Generate a robots.txt for a site.
  */
 export async function serveRobotsTxt(context: GitBookSiteContext) {
-  const { linker } = context;
-  const isRoot = checkIsRootSiteContext(context);
-  const isIndexable = await isSiteIndexable(context);
-  const sitemapPath = linker.toPathInSpace(isRoot ? '/sitemap.xml' : '/sitemap-pages.xml');
-  const sitemapUrl = linker.toAbsoluteURL(sitemapPath);
-  const lines = isIndexable
-    ? [
-        'User-agent: *',
-        // Disallow only internal search
-        'Disallow: /*?*q=*',
-        'Disallow: /*?*ask=*',
-        // Allow dynamic assets (may include ?)
-        'Allow: /~gitbook/image?*',
-        'Allow: /~gitbook/icon?*', 
-        'Allow: /favicon.ico', 
-        'Allow: /', 
-        `Sitemap: ${sitemapUrl}`,
-      ]
-    : ['User-agent: *', 'Disallow: /'];
-  return new Response(lines.join('\n'), { headers: { 'Content-Type': 'text/plain' } });
+    const { linker } = context;
+    const isRoot = checkIsRootSiteContext(context);
+    const isIndexable = await isSiteIndexable(context);
+    const sitemapPath = linker.toPathInSpace(isRoot ? '/sitemap.xml' : '/sitemap-pages.xml');
+    const sitemapUrl = linker.toAbsoluteURL(sitemapPath);
+    const lines = isIndexable
+        ? [
+              'User-agent: *',
+              // Disallow only internal search
+              'Disallow: /*?*q=*',
+              'Disallow: /*?*ask=*',
+              // Allow dynamic assets (may include ?)
+              'Allow: /~gitbook/image?*',
+              'Allow: /~gitbook/icon?*',
+              'Allow: /favicon.ico',
+              'Allow: /',
+              `Sitemap: ${sitemapUrl}`,
+          ]
+        : ['User-agent: *', 'Disallow: /'];
+    return new Response(lines.join('\n'), { headers: { 'Content-Type': 'text/plain' } });
 }
