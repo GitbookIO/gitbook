e714a564014ed04d5be60e330e448f8000a73e8d
Zeno Kapitein <zeno@gitbook.io>
2025-09-23 12:52:29 +0200

    Update NavigationLoader.tsx

--- packages/gitbook/src/components/primitives/NavigationLoader.tsx
+++ packages/gitbook/src/components/primitives/NavigationLoader.tsx
@@ -8,8 +8,8 @@ export const NavigationLoader = () => {
     return (
         <div
             className={tcls(
-                'pointer-events-none fixed inset-x-0 top-0 z-50 h-0.5 items-center justify-center overflow-hidden',
-                isNavigating ? 'flex' : 'hidden animate-fade-out-slow'
+                'pointer-events-none fixed inset-x-0 top-0 z-50 h-0.5 overflow-hidden',
+                isNavigating ? 'block' : 'hidden animate-fade-out-slow'
             )}
         >
             <div className={tcls('h-full w-full origin-left animate-crawl bg-primary-solid')} />
