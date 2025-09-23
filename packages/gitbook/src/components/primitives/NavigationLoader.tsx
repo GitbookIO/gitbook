b42b0b391059cee0afe8a8464c96eddbcf3f39db
conico974 <nicodorseuil@yahoo.fr>
2025-09-23 15:00:27 +0200

    Add navigation loader and rename context provider (#3642)
    Co-authored-by: Nicolas Dorseuil <nicolas@gitbook.io>
    Co-authored-by: Zeno Kapitein <zeno@gitbook.io>

---
+++ packages/gitbook/src/components/primitives/NavigationLoader.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { tcls } from '@/lib/tailwind';
+import { useIsNavigating } from '../hooks';
+
+export const NavigationLoader = () => {
+    const isNavigating = useIsNavigating();
+
+    return (
+        <div
+            className={tcls(
+                'pointer-events-none fixed inset-x-0 top-0 z-50 h-0.5 overflow-hidden',
+                isNavigating ? 'block' : 'hidden animate-fade-out-slow'
+            )}
+        >
+            <div className={tcls('h-full w-full origin-left animate-crawl bg-primary-solid')} />
+        </div>
+    );
+};
