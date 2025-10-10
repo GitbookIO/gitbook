6123c6febe517d78bd67223e0f7c93b189a32c36
Greg Bergé <berge.greg@gmail.com>
2025-10-10 14:40:53 +0200

    Use ts-expect-error

--- packages/gitbook/src/components/DocumentView/InlineIcon.tsx
+++ packages/gitbook/src/components/DocumentView/InlineIcon.tsx
@@ -8,11 +8,11 @@ import { textColorToStyle } from './utils/colors';
 export async function InlineIcon(props: InlineProps<DocumentInlineIcon>) {
     const { inline } = props;
     const icon = inline.data.icon as IconName;
-    // @TODO remove the useless casting once API is updated
-    const color =
-        'color' in inline.data && inline.data.color
-            ? (inline.data.color as DocumentMarkColor['data']['text'])
-            : undefined;
+    // @ts-expect-error remove this comment once API is updated
+    const color = inline.data.color
+        ? // @ts-expect-error remove "as DocumentMarkColor['data']['text']" once API is updated
+          (inline.data.color as DocumentMarkColor['data']['text'])
+        : undefined;
 
     return (
         <Icon
