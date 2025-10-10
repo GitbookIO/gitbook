79af4dcd8e37cfe7409b5826e74f2cb7666c0195
Greg Bergé <berge.greg@gmail.com>
2025-10-10 13:22:40 +0200

    Support inline icon colors

--- packages/gitbook/src/components/DocumentView/InlineIcon.tsx
+++ packages/gitbook/src/components/DocumentView/InlineIcon.tsx
@@ -1,10 +1,23 @@
-import type { DocumentInlineIcon } from '@gitbook/api';
+import type { DocumentInlineIcon, DocumentMarkColor } from '@gitbook/api';
 
+import { tcls } from '@/lib/tailwind';
 import { Icon, type IconName } from '@gitbook/icons';
 import type { InlineProps } from './Inline';
+import { textColorToStyle } from './utils/colors';
 
 export async function InlineIcon(props: InlineProps<DocumentInlineIcon>) {
     const { inline } = props;
+    const icon = inline.data.icon as IconName;
+    // @TODO remove the useless casting once API is updated
+    const color =
+        'color' in inline.data && inline.data.color
+            ? (inline.data.color as DocumentMarkColor['data']['text'])
+            : undefined;
 
-    return <Icon icon={inline.data.icon as IconName} className="inline size-[1em]" />;
+    return (
+        <Icon
+            icon={icon}
+            className={tcls('inline size-[1em]', color ? textColorToStyle[color] : null)}
+        />
+    );
 }
