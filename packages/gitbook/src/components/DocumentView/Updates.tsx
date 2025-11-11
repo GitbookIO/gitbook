2ab79d2c1189c4cc96054136fb54b5e7b4ab112f
Viktor Renkema <vrenkema@gmail.com>
2025-11-11 16:34:03 +0100

    Support rendering Update blocks

---
+++ packages/gitbook/src/components/DocumentView/Updates.tsx
@@ -0,0 +1,16 @@
+import type { DocumentBlockUpdates } from '@gitbook/api';
+import type { BlockProps } from './Block';
+import { Blocks } from './Blocks';
+
+export function Updates(props: BlockProps<DocumentBlockUpdates>) {
+    const { block, style, ancestorBlocks, ...contextProps } = props;
+
+    return (
+        <Blocks
+            {...contextProps}
+            nodes={block.nodes}
+            ancestorBlocks={[...ancestorBlocks, block]}
+            style={[style, 'flex flex-col gap-20']}
+        />
+    );
+}
