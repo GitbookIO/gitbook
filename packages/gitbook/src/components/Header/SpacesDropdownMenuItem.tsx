f6c28c64f718e7c4f4402ece186d0eae27808f19
taranvohra <taranvohra@outlook.com>
2025-11-13 01:38:59 +0530

    fix variant space comparison

--- packages/gitbook/src/components/Header/SpacesDropdownMenuItem.tsx
+++ packages/gitbook/src/components/Header/SpacesDropdownMenuItem.tsx
@@ -1,29 +1,33 @@
 'use client';
 
-import type { Space } from '@gitbook/api';
-
 import { joinPath } from '@/lib/paths';
 import { useCurrentPageMetadata, useCurrentPagePath } from '../hooks';
 import { DropdownMenuItem } from '../primitives/DropdownMenu';
 
 interface VariantSpace {
-    id: Space['id'];
-    title: Space['title'];
+    id: string;
+    title: string;
     url: string;
     isActive: boolean;
+    spaceId: string;
 }
 
-// When switching to a different variant space, we reconstruct the URL by swapping the space path.
+/**
+ * Return the href for a variant space, taking into account the current page path and metadata.
+ */
 function useVariantSpaceHref(variantSpace: VariantSpace, currentSpacePath: string, active = false) {
     const currentPathname = useCurrentPagePath();
     const { metaLinks } = useCurrentPageMetadata();
 
-    const variantHasAlternate = metaLinks?.alternates.find(
-        (alt) => alt.space?.id === variantSpace.id
+    // We first check if there is an alternate link for the variant space in the current page metadata.
+    const pageHasAlternateForVariant = metaLinks?.alternates.find(
+        (alt) => alt.space?.id === variantSpace.spaceId
     );
-    if (variantHasAlternate) {
-        return variantHasAlternate.href;
+    if (pageHasAlternateForVariant) {
+        return pageHasAlternateForVariant.href;
     }
+
+    // If there is no alternate link, we reconstruct the URL by swapping the space path.
 
     // We need to ensure that the variant space URL is not the same as the current space path.
     // If it is, we return only the variant space URL to redirect to the root of the variant space.
