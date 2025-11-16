f4284b4406e2bee37ab3576e3de81681ad4925ac
Samy Pessé <samypesse@gmail.com>
2025-11-16 01:31:36 +0100

    Remove log

--- scripts/publish-if-new.sh
+++ scripts/publish-if-new.sh
@@ -10,7 +10,6 @@ set -euo pipefail
 
 echo "Node auth token: ${NODE_AUTH_TOKEN}"
 echo "NPM config userconfig: ${NPM_CONFIG_USERCONFIG}"
-cat ${NPM_CONFIG_USERCONFIG}
 
 NAME=$(node -p "require('./package.json').name")
 VERSION=$(node -p "require('./package.json').version")
