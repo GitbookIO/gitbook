9c69cc413f6b4c63d56c9d4a8f3f7f15c6c9214e
Zeno Kapitein <zeno@gitbook.io>
2025-12-05 13:25:26 +0100

    Add customizable suggested questions (#3789)

--- packages/gitbook/src/components/AIChat/AIChat.tsx
+++ packages/gitbook/src/components/AIChat/AIChat.tsx
@@ -101,7 +101,11 @@ export function AIChat() {
                         </EmbeddableFrameButtons>
                     </EmbeddableFrameHeader>
                     <EmbeddableFrameBody>
-                        <AIChatBody chatController={chatController} chat={chat} />
+                        <AIChatBody
+                            chatController={chatController}
+                            chat={chat}
+                            suggestions={config.suggestions}
+                        />
                     </EmbeddableFrameBody>
                 </EmbeddableFrameMain>
             </EmbeddableFrame>
