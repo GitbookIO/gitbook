5a714f571321f3e6c180889dfc57928629a7f9fe
Zeno Kapitein <zeno@gitbook.io>
2025-12-12 14:08:52 +0100

    Update button sizing

--- packages/gitbook/src/components/DocumentView/InlineButton.tsx
+++ packages/gitbook/src/components/DocumentView/InlineButton.tsx
@@ -22,6 +22,7 @@ export async function InlineButton(props: InlineProps<api.DocumentInlineButton>)
                 label={inline.data.label}
                 // TODO: use a variant specifically for user-defined buttons.
                 variant={inline.data.kind}
+                size="medium"
                 className="leading-normal"
                 disabled={href === undefined}
                 icon={inline.data.icon as IconName | undefined}
