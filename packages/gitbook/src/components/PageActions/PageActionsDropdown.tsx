8a38cc67adb5ce103a0939f190d8f31450fdc3af
Zeno Kapitein <zeno@gitbook.io>
2026-01-13 18:11:09 +0100

    Apply suggestion from @clairechabas
    Co-authored-by: Claire Chabas <claire.chabas@gmail.com>

--- packages/gitbook/src/components/PageActions/PageActionsDropdown.tsx
+++ packages/gitbook/src/components/PageActions/PageActionsDropdown.tsx
@@ -51,7 +51,7 @@ export function PageActionsDropdown(props: PageActionsDropdownProps) {
     return defaultAction || dropdownActions.length > 0 ? (
         <ButtonGroup ref={ref} className={props.className}>
             {defaultAction}
-            {dropdownActions.length > (defaultAction ? 1 : 0) ? ( // Show dropdown if there's no default action OR more than one action.
+            {!defaultAction || dropdownActions.length >  1 ? (
                 <DropdownMenu
                     align="end"
                     className="!min-w-60 max-w-max"
