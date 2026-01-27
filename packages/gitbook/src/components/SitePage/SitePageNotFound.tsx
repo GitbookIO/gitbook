bd0605562c153544f7d591c18ad459f6fa1a0e39
Nicolas Dorseuil <nicolas@gitbook.io>
2026-01-27 18:21:09 +0100

    Enhance SearchContainer to wait for body load before opening assistant and update SitePageNotFound to handle ask parameter for redirection

--- packages/gitbook/src/components/SitePage/SitePageNotFound.tsx
+++ packages/gitbook/src/components/SitePage/SitePageNotFound.tsx
@@ -8,6 +8,7 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 import { useSpaceBasePath } from '../SpaceLayout/SpaceLayoutContext';
 import { CurrentPageProvider } from '../hooks';
+import { SuspenseLoadedHint } from '../primitives';
 
 /**
  * Component that displays a "page not found" message.
@@ -20,11 +21,18 @@ export function SitePageNotFound() {
 
     // If ?fallback=true, redirect to the root page.
     const fallback = searchParams?.get('fallback');
+    const ask = searchParams?.get('ask');
     useEffect(() => {
         if (fallback) {
             router.replace(basePath);
+        }else if (ask) {
+            // This is a workaround to prevent an infinite loop when the ask parameter is present.
+            // If it is present while we are on a 404 page, Next.js will fetch the RSC of the page again,
+            // trigerring the page to rerender and the assistant to start again, causing the infinite loop.
+            // To prevent this, when we detect the ask parameter, we redirect to the base path where the error does not occur.
+            router.replace(`${basePath}?${searchParams?.toString()}`);
         }
-    }, [basePath, fallback, router]);
+    }, [basePath, fallback, ask, router, searchParams]);
 
     return (
         <CurrentPageProvider page={null}>
@@ -45,6 +53,7 @@ export function SitePageNotFound() {
                     </h2>
                     <p className={tcls('text-base', 'mb-4')}>{t(language, 'notfound')}</p>
                 </div>
+                <SuspenseLoadedHint />
 
                 {/* Track the page not found as a page view */}
                 <TrackPageViewEvent displayContext={SiteInsightsDisplayContext.Site} />
