49685c9525dfdee15590f47e64cd5ce543e7b572
Samy Pessé <samypesse@gmail.com>
2026-01-28 09:16:39 +0100

    Use public package of fontawesome

--- packages/icons/bin/kit.js
+++ packages/icons/bin/kit.js
@@ -5,17 +5,10 @@ import url from 'node:url';
  * Get the path to the kit, depending on the Pro or Free version.
  */
 export function getKitPath() {
-    let source = path.dirname(
-        url.fileURLToPath(import.meta.resolve('@fortawesome/fontawesome-free/package.json'))
+    return path.resolve(
+        path.dirname(
+            url.fileURLToPath(import.meta.resolve('@gitbook/fontawesome-pro/package.json'))
+        ),
+        'icons'
     );
-    try {
-        source = path.resolve(
-            path.dirname(
-                url.fileURLToPath(import.meta.resolve('@gitbook/fontawesome-pro/package.json'))
-            ),
-            'icons'
-        );
-    } catch (_error) {}
-
-    return source;
 }
