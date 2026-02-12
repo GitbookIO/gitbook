22f574da9c7be2c6be960d19f6419b110930f918
Zeno Kapitein <zeno@gitbook.io>
2026-02-12 14:06:28 +0100

    Fix followup & suggested questions overflow (#3995)

--- packages/gitbook/src/components/AIChat/AiChatFollowupSuggestions.tsx
+++ packages/gitbook/src/components/AIChat/AiChatFollowupSuggestions.tsx
@@ -17,18 +17,19 @@ export function AIChatFollowupSuggestions(props: {
     return (
         <div className="flex grow flex-col">
             <div
-                className="sticky bottom-0 mt-auto flex flex-col items-start gap-2"
+                className="sticky bottom-0 mt-auto flex max-w-full flex-col items-start gap-2"
                 data-testid="ai-chat-followup-suggestions"
             >
                 {chat.followUpSuggestions.map((suggestion, index) => (
                     <Button
+                        truncate={false}
                         data-testid="ai-chat-followup-suggestion"
                         key={index}
                         onClick={() => {
                             chatController.postMessage({ message: suggestion });
                         }}
                         label={suggestion}
-                        className="starting:h-0 max-w-full origin-left animate-blur-in-slow whitespace-normal border-none bg-primary-11/1 starting:py-0 text-left transition-all transition-discrete duration-500 *:whitespace-normal hover:bg-primary-hover"
+                        className="starting:h-0 max-w-full origin-left animate-blur-in-slow border-none bg-primary-11/1 starting:py-0 text-left transition-all transition-discrete duration-500 hover:bg-primary-hover"
                         size="small"
                         variant="blank"
                         style={{
