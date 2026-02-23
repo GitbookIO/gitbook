ec363694001db64797b750797bf73dcb7f239302
Claire Chabas <claire.chabas@gmail.com>
2026-02-23 18:13:36 +0000

    Prevent inline code to break to multiple lines

--- packages/gitbook/src/components/DocumentView/Text.tsx
+++ packages/gitbook/src/components/DocumentView/Text.tsx
@@ -101,7 +101,9 @@ function Code(props: MarkedLeafProps<DocumentMarkCode>) {
                 // Text size is proportional to the font-size of the parent element
                 'text-[.875em]',
                 // We ensure that the code is not making the parent bigger, especially in headings
-                'leading-[calc(max(1.20em,1.25rem))]'
+                'leading-[calc(max(1.20em,1.25rem))]',
+                // Ensure that inline code does not break into multiple lines
+                'inline-flex whitespace-nowrap break-normal'
             )}
         >
             {props.children}
