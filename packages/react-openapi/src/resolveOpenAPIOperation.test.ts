c86b70f965220e8766f1a4f9374516f458ebeb4b
Nolann Biron <biron.nolann@gmail.com>
2026-03-04 18:32:46 +0100

    Support x-enable-proxy at operation level

--- packages/react-openapi/src/resolveOpenAPIOperation.test.ts
+++ packages/react-openapi/src/resolveOpenAPIOperation.test.ts
@@ -259,6 +259,50 @@ describe('#resolveOpenAPIOperation', () => {
 
             expect(resolved?.['x-enable-proxy']).toBeUndefined();
         });
+
+        it('should be available at the operation level', async () => {
+            const filesystem = await loadFixture({
+                openapi: '3.1.0',
+                info: { title: 'Test', version: '1.0' },
+                paths: {
+                    '/test': {
+                        get: {
+                            'x-enable-proxy': true,
+                            responses: { '200': { description: 'OK' } },
+                        },
+                    },
+                },
+            });
+            const resolved = await resolveOpenAPIOperation(filesystem, {
+                method: 'get',
+                path: '/test',
+            });
+
+            expect(resolved?.operation['x-enable-proxy']).toBe(true);
+        });
+
+        it('should allow operation-level to override spec-level', async () => {
+            const filesystem = await loadFixture({
+                openapi: '3.1.0',
+                info: { title: 'Test', version: '1.0' },
+                'x-enable-proxy': true,
+                paths: {
+                    '/test': {
+                        get: {
+                            'x-enable-proxy': false,
+                            responses: { '200': { description: 'OK' } },
+                        },
+                    },
+                },
+            });
+            const resolved = await resolveOpenAPIOperation(filesystem, {
+                method: 'get',
+                path: '/test',
+            });
+
+            expect(resolved?.['x-enable-proxy']).toBe(true);
+            expect(resolved?.operation['x-enable-proxy']).toBe(false);
+        });
     });
 
     describe('server precedence', () => {
