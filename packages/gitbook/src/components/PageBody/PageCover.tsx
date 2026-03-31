656510fb55741aa757c919b432d71b48e69f4fd1
Zeno Kapitein <zeno@gitbook.io>
2026-03-31 19:22:20 +0200

    Update PageCover.tsx

--- packages/gitbook/src/components/PageBody/PageCover.tsx
+++ packages/gitbook/src/components/PageBody/PageCover.tsx
@@ -94,15 +94,14 @@ export async function PageCover(props: {
                           'md:-mx-8',
 
                           // If there's no sidebar, extend the cover
-                          'page-no-toc:lg:-ml-92',
-                          //   'not-layout-full:2xl:circular-corners:rounded-b-3xl',
-                          //   'not-layout-full:2xl:rounded-corners:rounded-b-xl',
+                          'layout-default:page-no-toc:lg:-ml-92',
 
-                          // In layout-wide, extend cover to account for outline sidebar on the right
-                          'layout-wide:2xl:-mr-[min(calc((100vw-90rem)/2+2rem),18rem)]',
+                          'layout-wide:2xl:-mr-72',
+                          'layout-wide:page-no-toc:2xl:-ml-72',
+                          'layout-wide:has-sidebar:page-no-toc:lg:-ml-156',
 
-                          // In layout-full, extend cover to full screen width
-                          'layout-wide:no-sidebar:xl:-mx-[max(calc((100vw-90rem)/2+2rem),2rem)]',
+                          'layout-wide:3xl:circular-corners:rounded-b-3xl',
+                          'layout-wide:3xl:rounded-corners:rounded-b-xl',
 
                           // Round the bottom left corner once the sidebar is shown next to it
                           //   'not-layout-full:lg:rounded-corners:rounded-bl-xl',
