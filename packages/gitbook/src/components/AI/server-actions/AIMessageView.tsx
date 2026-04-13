6696d8e3cd0d605aac5e66f43163bdc4ceca8585
Samy Pessé <samypesse@gmail.com>
2026-04-13 09:23:39 +0200

    Format

--- packages/gitbook/src/components/AI/server-actions/AIMessageView.tsx
+++ packages/gitbook/src/components/AI/server-actions/AIMessageView.tsx
@@ -23,10 +23,7 @@ export function AIMessageView(
                 return (
                     <div
                         key={index}
-                        className={tcls(
-                            'flex flex-col gap-2',
-                            hasContent ? 'has-content' : ''
-                        )}
+                        className={tcls('flex flex-col gap-2', hasContent ? 'has-content' : '')}
                     >
                         {step.content ? (
                             <DocumentView
