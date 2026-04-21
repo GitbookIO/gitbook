e6f45999df7421bdd085e0419672e377602072f8
Samy Pessé <samypesse@gmail.com>
2026-04-21 17:26:33 +0200

    Oups

--- packages/gitbook/src/app/sites/static/[mode]/[siteURL]/[siteData]/~gitbook/markdown-ask/[pagePath]/route.ts
+++ packages/gitbook/src/app/sites/static/[mode]/[siteURL]/[siteData]/~gitbook/markdown-ask/[pagePath]/route.ts
@@ -4,10 +4,10 @@ import type { NextRequest } from 'next/server';
 
 export async function GET(rawRequest: NextRequest, { params }: { params: Promise<RouteParams> }) {
     const { context } = await getStaticSiteContext(await params);
-    const pathname = getPagePathFromParams(await params);
+    const pagePath = getPagePathFromParams(await params);
 
     return serveAskMarkdown(context, {
         question: rawRequest.nextUrl.searchParams.get('ask') ?? '',
-        pagePath: pathname, // not really correct
+        pagePath,
     });
 }
