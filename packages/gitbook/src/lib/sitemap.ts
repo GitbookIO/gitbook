e44d235bbf7a3f8a0a509df640d3699d62942049
Rodrigo Castro <18582098+rodrcastro@users.noreply.github.com>
2026-04-28 10:32:22 +0100

    Fix hidden pages in sitemap exports

--- packages/gitbook/src/lib/sitemap.ts
+++ packages/gitbook/src/lib/sitemap.ts
@@ -9,13 +9,17 @@ export type FlatPageEntry = { page: RevisionPageDocument; depth: number };
  */
 function flattenPages(
     rootPages: RevisionPage[],
-    filter: (page: RevisionPageDocument | RevisionPageGroup) => boolean
+    filter: (
+        page: RevisionPageDocument | RevisionPageGroup,
+        ancestors: Array<RevisionPageDocument | RevisionPageGroup>
+    ) => boolean
 ): FlatPageEntry[] {
     const flattenPage = (
         page: RevisionPageDocument | RevisionPageGroup,
-        depth: number
+        depth: number,
+        ancestors: Array<RevisionPageDocument | RevisionPageGroup>
     ): FlatPageEntry[] => {
-        const allowed = filter(page);
+        const allowed = filter(page, ancestors);
         if (!allowed) {
             return [];
         }
@@ -23,13 +27,19 @@ function flattenPages(
         return [
             ...(page.type === 'document' ? [{ page, depth }] : []),
             ...page.pages.flatMap((child) =>
-                child.type === 'document' ? flattenPage(child, depth + 1) : []
+                child.type !== 'link' && child.type !== 'computed'
+                    ? flattenPage(
+                          child as RevisionPageDocument | RevisionPageGroup,
+                          depth + 1,
+                          [...ancestors, page]
+                      )
+                    : []
             ),
         ];
     };
 
     return rootPages.flatMap((page) =>
-        page.type === 'group' || page.type === 'document' ? flattenPage(page, 0) : []
+        page.type === 'group' || page.type === 'document' ? flattenPage(page, 0, []) : []
     );
 }
 
@@ -37,5 +47,5 @@ function flattenPages(
  * Get all indexable pages from a revision in a flat list.
  */
 export function getIndexablePages(rootPages: RevisionPage[]) {
-    return flattenPages(rootPages, (page) => !page.hidden && isPageIndexable([], page));
+    return flattenPages(rootPages, (page, ancestors) => isPageIndexable(ancestors, page));
 }
