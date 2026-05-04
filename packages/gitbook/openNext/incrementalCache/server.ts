56aeb926a7844e0d4845be48b923e55aa11b29de
Nicolas Dorseuil <nicolas@gitbook.io>
2026-05-04 13:08:59 +0200

    Reduce cache TTL for regional cache to aid debugging

--- packages/gitbook/openNext/incrementalCache/server.ts
+++ packages/gitbook/openNext/incrementalCache/server.ts
@@ -6,7 +6,8 @@ export default withRegionalCache(new GitbookIncrementalCache(), {
     // Because of a race condition, the middleware may have populated the cache entry before `cache.match` had time to run on the server.
     // TODO: We should bypass the incremental cache entirely when the interceptor has caught the request. Should be done in OpenNext.
     bypassTagCacheOnCacheHit: false,
-    defaultLongLivedTtlSec: 60 * 60 * 24 /* 24 hours */,
+    //TODO: remove, reducing cache ttl of regional cache to help debugging
+    defaultLongLivedTtlSec: 5 * 60 /* 5 minutes */,
     // We don't want to update the cache entry on every cache hit
     shouldLazilyUpdateOnCacheHit: false,
 });
