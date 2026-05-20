bc6c722407d75f088a84d412893efcd191d32f18
Nicolas Dorseuil <nicolas@gitbook.io>
2026-05-20 09:57:58 +0200

    remove remote only from specific route

--- packages/gitbook/src/lib/data/api.ts
+++ packages/gitbook/src/lib/data/api.ts
@@ -222,7 +222,7 @@ function cacheLifeFromResponse(
 }
 
 const getUserById = cache(async (input: DataFetcherInput, params: { userId: string }) => {
-    'use cache';
+    'use cache: remote';
     return wrapCacheDataFetcherError(async () => {
         return trace(`getUserById(${params.userId})`, async () => {
             const api = apiClient(input);
@@ -238,7 +238,7 @@ const getUserById = cache(async (input: DataFetcherInput, params: { userId: stri
 
 const getSpace = cache(
     async (input: DataFetcherInput, params: { spaceId: string; shareKey: string | undefined }) => {
-        'use cache';
+        'use cache: remote';
         cacheTag(
             getCacheTag({
                 tag: 'space',
@@ -268,7 +268,7 @@ const getSpace = cache(
 
 const getChangeRequest = cache(
     async (input: DataFetcherInput, params: { spaceId: string; changeRequestId: string }) => {
-        'use cache';
+        'use cache: remote';
         cacheTag(
             getCacheTag({
                 tag: 'change-request',
@@ -298,6 +298,7 @@ const getChangeRequest = cache(
     }
 );
 
+// We don't use remote cache on vercel because of the 2Mb limit on cache size that makes some route crash
 const getRevision = cache(
     async (input: DataFetcherInput, params: { spaceId: string; revisionId: string }) => {
         'use cache';
@@ -329,7 +330,7 @@ const getRevisionPageMarkdown = cache(
         // used only to bust the cache
         _functionName: string
     ) => {
-        'use cache';
+        'use cache: remote';
         return wrapCacheDataFetcherError(async () => {
             return trace(
                 `getRevisionPageMarkdown(${params.spaceId}, ${params.revisionId}, ${params.pageId})`,
@@ -371,7 +372,7 @@ const getRevisionPageDocument = cache(
         // used only to bust the cache
         _functionName: string
     ) => {
-        'use cache';
+        'use cache: remote';
         return wrapCacheDataFetcherError(async () => {
             return trace(
                 `getRevisionPageDocument(${params.spaceId}, ${params.revisionId}, ${params.pageId})`,
@@ -405,7 +406,7 @@ const getRevisionReusableContentDocument = cache(
         input: DataFetcherInput,
         params: { spaceId: string; revisionId: string; reusableContentId: string }
     ) => {
-        'use cache';
+        'use cache: remote';
         return wrapCacheDataFetcherError(async () => {
             return trace(
                 `getRevisionReusableContentDocument(${params.spaceId}, ${params.revisionId}, ${params.reusableContentId})`,
@@ -438,7 +439,7 @@ const getRevisionPageByPath = cache(
         input: DataFetcherInput,
         params: { spaceId: string; revisionId: string; path: string }
     ) => {
-        'use cache';
+        'use cache: remote';
         return wrapCacheDataFetcherError(async () => {
             return trace(
                 `getRevisionPageByPath(${params.spaceId}, ${params.revisionId}, ${params.path})`,
@@ -465,7 +466,7 @@ const getRevisionPageByPath = cache(
 
 const getDocument = cache(
     async (input: DataFetcherInput, params: { spaceId: string; documentId: string }) => {
-        'use cache';
+        'use cache: remote';
         return wrapCacheDataFetcherError(async () => {
             return trace(`getDocument(${params.spaceId}, ${params.documentId})`, async () => {
                 const api = apiClient(input);
@@ -495,7 +496,7 @@ const getComputedDocument = cache(
             seed: string;
         }
     ) => {
-        'use cache';
+        'use cache: remote';
         cacheTag(
             ...getComputedContentSourceCacheTags(
                 {
@@ -531,6 +532,7 @@ const getComputedDocument = cache(
     }
 );
 
+// We don't use remote cache on vercel because of the 2Mb limit on cache size that makes some route crash
 const getLatestOpenAPISpecVersionContent = cache(
     async (input: DataFetcherInput, params: { organizationId: string; slug: string }) => {
         'use cache';
@@ -569,7 +571,7 @@ const getPublishedContentSite = cache(
         params: { organizationId: string; siteId: string; siteShareKey: string | undefined },
         _apiVersion: string
     ) => {
-        'use cache';
+        'use cache: remote';
         cacheTag(
             getCacheTag({
                 tag: 'site',
@@ -611,7 +613,7 @@ const getSiteRedirectBySource = cache(
             source: string;
         }
     ) => {
-        'use cache';
+        'use cache: remote';
         cacheTag(
             getCacheTag({
                 tag: 'site',
@@ -646,7 +648,7 @@ const getSiteRedirectBySource = cache(
 
 const getEmbedByUrl = cache(
     async (input: DataFetcherInput, params: { spaceId: string; url: string }) => {
-        'use cache';
+        'use cache: remote';
         cacheTag(
             getCacheTag({
                 tag: 'space',
@@ -674,6 +676,7 @@ const getEmbedByUrl = cache(
     }
 );
 
+// We don't use remote cache for this one because of the number of potential cache entry causing 429 on vercel runtime cache when revalidating
 const searchSiteContent = cache(
     async (
         input: DataFetcherInput,
@@ -719,7 +722,7 @@ const renderIntegrationUi = cache(
         input: DataFetcherInput,
         params: { integrationName: string; request: RenderIntegrationUI }
     ) => {
-        'use cache';
+        'use cache: remote';
         cacheTag(
             getCacheTag({
                 tag: 'integration',
@@ -753,7 +756,7 @@ const listRevisionPageMetaLinks = cache(
         input: DataFetcherInput,
         params: { spaceId: string; revisionId: string; pageId: string }
     ) => {
-        'use cache';
+        'use cache: remote';
         return wrapCacheDataFetcherError(async () => {
             return trace(
                 `listRevisionPageMetaLinks(${params.spaceId}, ${params.revisionId}, ${params.pageId})`,
