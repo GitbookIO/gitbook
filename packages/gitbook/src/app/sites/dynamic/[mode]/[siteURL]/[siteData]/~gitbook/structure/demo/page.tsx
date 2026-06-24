10a70951820d84ebd739f71b24f1ef986582d085
Nicolas Dorseuil <nicolas@gitbook.io>
2026-06-24 19:56:36 +0200

    Add metadata export to Page component for SEO configuration

--- packages/gitbook/src/app/sites/dynamic/[mode]/[siteURL]/[siteData]/~gitbook/structure/demo/page.tsx
+++ packages/gitbook/src/app/sites/dynamic/[mode]/[siteURL]/[siteData]/~gitbook/structure/demo/page.tsx
@@ -1,10 +1,15 @@
 import { type RouteLayoutParams, getDynamicSiteContext } from '@/app/utils';
 import { StructurePreview } from '@/components/StructurePreview';
 import { GITBOOK_APP_URL } from '@/lib/env';
+import type { Metadata } from 'next';
 import { getStructurePreviewSnapshot } from '../snapshot';
 
 type PageProps = {
     params: Promise<RouteLayoutParams>;
+};
+
+export const metadata: Metadata = {
+    robots: { index: false, follow: false },
 };
 
 export default async function Page(props: PageProps) {
